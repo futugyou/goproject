04ed55eea4a03738ea15b25245f4368590709051
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2020-10-05 08:44:33 +0000

    add common response
    add common response

--- bolg-service/internal/routers/api/v1/tag.go
+++ bolg-service/internal/routers/api/v1/tag.go
@@ -3,7 +3,10 @@ package v1
 import (
 	"log"
 
+	"github.com/goproject/blog-service/pkg/errcode"
+
 	"github.com/gin-gonic/gin"
+	"github.com/goproject/blog-service/pkg/app"
 )
 
 type Tag struct{}
@@ -12,8 +15,11 @@ func NewTage() Tag {
 	return Tag{}
 }
 
-func (a Tag) Get(c *gin.Context)    {}
-func (a Tag) List(c *gin.Context)   { log.Print("this is tags list") }
+func (a Tag) Get(c *gin.Context) {}
+func (a Tag) List(c *gin.Context) {
+	app.NewResponse(c).ToErrorResponse(errcode.ServiceError)
+	log.Print("this is tags list")
+}
 func (a Tag) Create(c *gin.Context) {}
 func (a Tag) Update(c *gin.Context) {}
 func (a Tag) Delete(c *gin.Context) {}
---
+++ bolg-service/pkg/app/paging.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/goproject/blog-service/global"
+	"github.com/goproject/blog-service/pkg/convert"
+)
+
+func GetPage(c *gin.Context) int {
+	page := convert.StrTo(c.Query("page_size")).MustInt()
+	if page <= 0 {
+		return 1
+	}
+	return page
+}
+
+func GetPageSize(c *gin.Context) int {
+	pagesize := convert.StrTo(c.Query("page_size")).MustInt()
+	if pagesize < 0 {
+		return global.AppSetting.DefaultPageSize
+	}
+	if pagesize > global.AppSetting.MaxPageSize {
+		return global.AppSetting.MaxPageSize
+	}
+	return pagesize
+}
+
+func GetPageOffset(page, pagesize int) int {
+	result := 0
+	if page > 0 {
+		result = (page - 1) * pagesize
+	}
+	return result
+}
