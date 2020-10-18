f1d18d4c569db30bfe0e96302f1100d6503d67ba
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2020-10-18 14:31:10 +0000

    add grpc gateway
    add grpc gateway

--- tag-service/go.mod
+++ tag-service/go.mod
@@ -5,8 +5,11 @@ go 1.15
 require (
 	github.com/fullstorydev/grpcurl v1.7.0 // indirect
 	github.com/golang/protobuf v1.4.3
+	github.com/grpc-ecosystem/grpc-gateway v1.15.2
 	github.com/soheilhy/cmux v0.1.4
-	golang.org/x/net v0.0.0-20201016165138-7b1cca2348c0 // indirect
+	golang.org/x/net v0.0.0-20201016165138-7b1cca2348c0
+	google.golang.org/genproto v0.0.0-20201015140912-32ed001d685c
 	google.golang.org/grpc v1.33.0
 	google.golang.org/protobuf v1.25.0
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- tag-service/main.go
+++ tag-service/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"net"
 	"net/http"
+	"strings"
 
 	pb "github.com/goproject/tag-service/proto"
 	"github.com/goproject/tag-service/server"
-	"github.com/soheilhy/cmux"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 // protoc --go_out=plugins=grpc:. ./proto/*.proto
 // grpcurl -plaintext  localhost:8001 proto.TagService.GetTagList
+// protoc -IC:\Users\TerraformRs\Documents\GitHub\protobuf-3.13.0\include -I. -IC:\Users\TerraformRs\go -IC:\Users\TerraformRs\go\pkg\mod\github.com\grpc-ecosystem\grpc-gateway@v1.15.2\third_party\googleapis --go_out=plugins=grpc:.  --grpc-gateway_out=logtostderr=true:. ./proto/*.proto
 
 var (
 	port     string = "8001"
@@ -29,6 +34,50 @@ func init() {
 	flag.Parse()
 }
 
+func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+	return h2c.NewHandler(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+					grpcServer.ServeHTTP(w, r)
+				} else {
+					otherHandler.ServeHTTP(w, r)
+				}
+			}),
+		&http2.Server{},
+	)
+}
+
+func RunServer(port string) error {
+	httpMux := runHttpServer()
+	gatewayMux := runGrpcGatewayServer()
+	grpcS := runGrpcServer()
+	httpMux.Handle("/", gatewayMux)
+	return http.ListenAndServe(":"+port, grpcHandlerFunc(grpcS, httpMux))
+}
+
+func runHttpServer() *http.ServeMux {
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(`pong`))
+	})
+	return serverMux
+}
+
+func runGrpcServer() *grpc.Server {
+	s := grpc.NewServer()
+	pb.RegisterTagServiceServer(s, server.NewTagServer())
+	reflection.Register(s)
+	return s
+}
+
+func runGrpcGatewayServer() *runtime.ServeMux {
+	endpoint := "0.0.0.0:" + port
+	gwmux := runtime.NewServeMux()
+	dopts := []grpc.DialOption{grpc.WithInsecure()}
+	_ = pb.RegisterTagServiceHandlerFromEndpoint(context.Background(), gwmux, endpoint, dopts)
+	return gwmux
+}
 func RunTCPServer(port string) (net.Listener, error) {
 	return net.Listen("tcp", ":"+port)
 }
@@ -53,24 +102,29 @@ func RunGrpcServer() *grpc.Server {
 }
 
 func main() {
-	l, err := RunTCPServer(port)
+	err := RunServer(port)
 	if err != nil {
 		log.Fatalf("run tcp server err: %v", err)
 	}
-	m := cmux.New(l)
-	grpcL := m.MatchWithWriters(
-		cmux.HTTP2MatchHeaderFieldPrefixSendSettings(
-			"content-type",
-			"application/grpc"))
-	httpL := m.Match(cmux.HTTP1Fast())
 
-	grpcS := RunGrpcServer()
-	httpS := RunHttpServer(port)
+	// l, err := RunTCPServer(port)
+	// if err != nil {
+	// 	log.Fatalf("run tcp server err: %v", err)
+	// }
+	// m := cmux.New(l)
+	// grpcL := m.MatchWithWriters(
+	// 	cmux.HTTP2MatchHeaderFieldPrefixSendSettings(
+	// 		"content-type",
+	// 		"application/grpc"))
+	// httpL := m.Match(cmux.HTTP1Fast())
 
-	go grpcS.Serve(grpcL)
-	go httpS.Serve(httpL)
-	err = m.Serve()
-	if err != nil {
-		log.Fatalf("run serve err: %v", err)
-	}
+	// grpcS := RunGrpcServer()
+	// httpS := RunHttpServer(port)
+
+	// go grpcS.Serve(grpcL)
+	// go httpS.Serve(httpL)
+	// err = m.Serve()
+	// if err != nil {
+	// 	log.Fatalf("run serve err: %v", err)
+	// }
 }
