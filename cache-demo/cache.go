84154e3761037ca440f7703b1fff699a7a4f75f3
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2020-10-26 17:47:57 +0000

    add sync cache
    add sync cache

--- cache-demo/cache.go
+++ cache-demo/cache.go
@@ -1,9 +1,63 @@
 package cache
 
+import (
+	"log"
+	"sync"
+)
+
 type Cache interface {
 	Set(key string, value interface{})
 	Get(key string) interface{}
 	Del(key string)
 	DelOldest()
 	Len() int
+}
+
+const DefaultMaxBytes = 1 << 29
+
+type safeCache struct {
+	m          sync.RWMutex
+	cache      Cache
+	nhit, nget int
+}
+
+func newSafeCache(cache Cache) *safeCache {
+	return &safeCache{
+		cache: cache,
+	}
+}
+
+func (sc *safeCache) set(key string, value interface{}) {
+	sc.m.Lock()
+	defer sc.m.Unlock()
+	sc.cache.Set(key, value)
+}
+
+func (sc *safeCache) get(key string) interface{} {
+	sc.m.Lock()
+	defer sc.m.Unlock()
+	sc.nget++
+	if sc.cache == nil {
+		return nil
+	}
+
+	v := sc.cache.Get(key)
+	if v != nil {
+		log.Println("[Cache] hit")
+		sc.nhit++
+	}
+	return v
+}
+
+func (sc *safeCache) stat() *Stat {
+	sc.m.Lock()
+	defer sc.m.Unlock()
+	return &Stat{
+		NHit: sc.nhit,
+		NGet: sc.nget,
+	}
+}
+
+type Stat struct {
+	NHit, NGet int
 }
---
+++ cache-demo/tour_cache_test.go
@@ -0,0 +1,47 @@
+package cache_test
+
+import (
+	"log"
+	"sync"
+	"testing"
+
+	"github.com/goproject/cache-demo"
+	"github.com/goproject/cache-demo/lru"
+	"github.com/matryer/is"
+)
+
+func TestTourCacheGet(t *testing.T) {
+	db := map[string]string{
+		"key1": "val1",
+		"key2": "val2",
+		"key3": "val3",
+		"key4": "val4",
+	}
+	getter := cache.GetFunc(func(key string) interface{} {
+		log.Println("[From DB] find key", key)
+
+		if val, ok := db[key]; ok {
+			return val
+		}
+		return nil
+	})
+
+	tourCache := cache.NewTourCache(getter, lru.New(0, nil))
+	is := is.New(t)
+	var wg sync.WaitGroup
+	for k, v := range db {
+		wg.Add(1)
+		go func(k, v string) {
+			defer wg.Done()
+			is.Equal(tourCache.Get(k), v)
+			is.Equal(tourCache.Get(k), v)
+		}(k, v)
+	}
+	wg.Wait()
+
+	is.Equal(tourCache.Get("unknown"), nil)
+	is.Equal(tourCache.Get("unknown"), nil)
+
+	is.Equal(tourCache.Stat().NGet, 10)
+	is.Equal(tourCache.Stat().NHit, 4)
+}
