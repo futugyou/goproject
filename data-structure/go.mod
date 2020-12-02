610e807c9d8e4179a4d79d3d8b73531334d0daf6
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2020-12-02 14:20:19 +0000

    add trre and hash
    add trre and hash

---
+++ data-structure/go.mod
@@ -0,0 +1,3 @@
+module github.com/go-project/data-structure
+
+go 1.15
---
+++ data-structure/hashTable.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+)
+
+const SIZE = 15
+
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+type HashTable struct {
+	Table map[int]*Node
+	Size  int
+}
+
+func hashFunction(i, size int) int {
+	return (i % size)
+}
+
+func insert(hash *HashTable, value int) int {
+	index := hashFunction(value, hash.Size)
+	element := Node{Value: value, Next: hash.Table[index]}
+	hash.Table[index] = &element
+	// element := hash.Table[index]
+	// if element == nil {
+	// 	element = &Node{Value: value}
+	// 	hash.Table[index] = element
+	// } else {
+	// 	for element.Next != nil {
+	// 		element = element.Next
+	// 	}
+	// 	element.Next = &Node{Value: value}
+	// }
+	return index
+}
+
+func traverse(hash *HashTable) {
+	for k := range hash.Table {
+		if hash.Table[k] != nil {
+			t := hash.Table[k]
+			for t != nil {
+				fmt.Printf("%d -> ", t.Value)
+				t = t.Next
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func lookup(hash *HashTable, value int) bool {
+	index := hashFunction(value, hash.Size)
+	if hash.Table[index] != nil {
+		t := hash.Table[index]
+		for t != nil {
+			if t.Value == value {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func main() {
+	table := make(map[int]*Node, SIZE)
+	hash := &HashTable{Table: table, Size: SIZE}
+	fmt.Println("count is ", hash.Size)
+	for i := 0; i < 120; i++ {
+		insert(hash, i)
+	}
+	traverse(hash)
+}
