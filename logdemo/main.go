6fc162128280492e1682aa53433e025d022d0dfd
futugyou <17871902+futugyou@users.noreply.github.com>
2021-09-22 15:56:44 +0800

    update golang custom log
    update golang custom log

--- logdemo/main.go
+++ logdemo/main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
+var (
+	Trace   *log.Logger
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+)
+
 func main() {
 	log.Println("1234")
-	log.Fatalln("1234")
-	log.Panicln("1234")
+	Trace.Println("1234")
+	Info.Println("1234")
+	Warning.Println("1234")
+	Error.Println("1234")
 }
 
 func init() {
-	log.SetPrefix("TODO: ")
-	f, err := os.OpenFile("./log.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	file, err := os.OpenFile("./errors.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	log.SetOutput(f)
-	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Llongfile)
+	Trace = log.New(ioutil.Discard, "TRACE: ", log.Ldate|log.Ltime|log.Llongfile)
+	Info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Llongfile)
+	Warning = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Llongfile)
+	Error = log.New(io.MultiWriter(file, os.Stderr), "ERROR: ", log.Ldate|log.Ltime|log.Llongfile)
 }
