75a9f3e0f6a5ece6b1bcfbcbf959e9f98b000a7f
futugyou <17871902+futugyou@users.noreply.github.com>
2022-02-11 00:59:59 +0800

    add ethereum wallet
    add ethereum wallet

---
+++ ethereum-demo/client/wallet_generate.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"golang.org/x/crypto/sha3"
+)
+
+func main() {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	fmt.Println(hexutil.Encode(privateKeyBytes)[2:])
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("canont assert type publickey")
+	}
+
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	fmt.Println(hexutil.Encode(publicKeyBytes)[4:])
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	fmt.Println(address)
+
+	hash := sha3.NewLegacyKeccak256()
+	hash.Write(privateKeyBytes[1:])
+	fmt.Println(hexutil.Encode(hash.Sum(nil)[12:]))
+}
