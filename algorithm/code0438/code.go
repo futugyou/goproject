7aacf7c1061c4309743208351fed3d5e29041c3a
futugyou <17871902+futugyou@users.noreply.github.com>
2022-03-22 14:42:35 +0800

    update labuladong 438
    update labuladong 438

---
+++ algorithm/code0438/code.go
@@ -0,0 +1,57 @@
+package code0438
+
+import (
+	"fmt"
+)
+
+func Exection() {
+	s := "cbaebabacd"
+	t := "abc"
+	exection(s, t)
+}
+
+func exection(s, t string) {
+	need := make(map[rune]int)
+	for _, v := range t {
+		need[v]++
+	}
+	r := []rune(s)
+	left := 0
+	right := 0
+	valid := 0
+	result := make([]int, 0)
+	dic := make(map[rune]int)
+	for {
+		if right >= len(r) {
+			break
+		}
+		c := r[right]
+		right++
+
+		if need[c] > 0 {
+			dic[c]++
+			if need[c] == dic[c] {
+				valid++
+			}
+		}
+
+		for {
+			if right-left < len(need) {
+				break
+			}
+			if valid == len(need) {
+				result = append(result, left)
+			}
+
+			d := r[left]
+			left++
+			if need[d] > 0 {
+				if need[d] == dic[d] {
+					valid--
+				}
+				dic[d]--
+			}
+		}
+	}
+	fmt.Println(result)
+}
