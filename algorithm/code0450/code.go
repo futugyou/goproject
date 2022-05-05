64c4e911e7b6e3033cc880ca7dfebfadb5164668
futugyou <17871902+futugyou@users.noreply.github.com>
2022-05-05 14:48:06 +0800

    update labuladong 450
    update labuladong 450

---
+++ algorithm/code0450/code.go
@@ -0,0 +1,41 @@
+package code0450
+
+import "github.com/futugyousuzu/goproject/algorithm/common"
+
+func Exection() {
+	node := common.NewBSTTreeNode()
+	key := 5
+	r := exection(&node, key)
+	r.Display()
+}
+
+func exection(root *common.TreeNode, key int) *common.TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == key {
+		if root.Left == nil {
+			return root.Right
+		}
+		if root.Right == nil {
+			return root.Left
+		}
+		t := getMax(root.Left)
+		root.Val = t.Val
+		root.Left = exection(root.Left, t.Val)
+	} else if root.Val < key {
+		root.Right = exection(root.Right, key)
+	} else {
+		root.Left = exection(root.Left, key)
+	}
+	return root
+}
+
+func getMax(root *common.TreeNode) *common.TreeNode {
+	for {
+		if root.Right == nil {
+			return root
+		}
+		root = root.Right
+	}
+}
