f075fc74cf0e83d32cd4ac85e2a4a54b32320cdc
futugyou <17871902+futugyou@users.noreply.github.com>
2022-06-22 00:06:33 +0800

    update labuladong 121
    update labuladong 121

--- algorithm/main.go
+++ algorithm/main.go
@@ -1,6 +1,6 @@
 package main
 
-import code "github.com/futugyousuzu/goproject/algorithm/code0072"
+import code "github.com/futugyousuzu/goproject/algorithm/code0121"
 
 func main() {
 	// arr := []int{9, 3, 4, 5, 7, 1, 33, 69, 94, 84, 67}
