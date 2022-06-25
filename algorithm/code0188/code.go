61d8e612cef727098a6709a8e0aaa12cd10d26dd
futugyou <17871902+futugyou@users.noreply.github.com>
2022-06-25 21:16:45 +0800

    update labuladong 188
    update labuladong 188

---
+++ algorithm/code0188/code.go
@@ -0,0 +1,43 @@
+package code0188
+
+import (
+	"fmt"
+
+	"github.com/futugyousuzu/goproject/algorithm/common"
+)
+
+func Exection() {
+	prices := []int{2, 4, 1}
+	k := 2
+	r := exection(prices, k)
+	fmt.Println(r)
+}
+
+func exection(prices []int, k int) int {
+	n := len(prices)
+	dp := make([][][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([][]int, k)
+		for j := 0; j < k; j++ {
+			dp[i][j] = make([]int, 2)
+		}
+	}
+	dp[0][0][0] = -prices[0]
+	dp[0][0][1] = 0
+	dp[0][1][0] = -prices[0]
+	dp[0][1][1] = 0
+
+	for i := 1; i < n; i++ {
+		for j := 0; j < 2; j++ {
+			if j == 0 {
+				dp[i][j][0] = common.Max(dp[i-1][j][0], -prices[i])
+				dp[i][j][1] = common.Max(dp[i-1][j][1], dp[i-1][j][0]+prices[i])
+				continue
+			}
+			dp[i][j][0] = common.Max(dp[i-1][j][0], dp[i-1][j-1][1]-prices[i])
+			dp[i][j][1] = common.Max(dp[i-1][j][1], dp[i-1][j][0]+prices[i])
+		}
+	}
+	fmt.Println(dp)
+	return dp[n-1][1][1]
+}
