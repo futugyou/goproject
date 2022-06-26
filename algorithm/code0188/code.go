4e21d57a7c613b6a9ff53e0b39868439f6ddd9ee
futugyou <17871902+futugyou@users.noreply.github.com>
2022-06-26 10:43:12 +0800

    fix bug
    fix bug

--- algorithm/code0188/code.go
+++ algorithm/code0188/code.go
@@ -28,7 +28,7 @@ func exection(prices []int, k int) int {
 	dp[0][1][1] = 0
 
 	for i := 1; i < n; i++ {
-		for j := 0; j < 2; j++ {
+		for j := 0; j < k; j++ {
 			if j == 0 {
 				dp[i][j][0] = common.Max(dp[i-1][j][0], -prices[i])
 				dp[i][j][1] = common.Max(dp[i-1][j][1], dp[i-1][j][0]+prices[i])
