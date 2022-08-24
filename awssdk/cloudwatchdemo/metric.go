689aaca443c181791af5f8febbdd7befc75865da
futugyou <17871902+futugyou@users.noreply.github.com>
2022-08-24 15:47:49 +0800

    AWS-6 update aws cloud watch
    AWS-6 update aws cloud watch

--- awssdk/cloudwatchdemo/metric.go
+++ awssdk/cloudwatchdemo/metric.go
@@ -48,3 +48,89 @@ func GetMetricData() {
 	}
 	fmt.Println(result)
 }
+
+func GetDashboard() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{Region: aws.String(endpoints.ApSoutheast1RegionID)},
+	}))
+	svc := cloudwatch.New(sess)
+
+	input := &cloudwatch.GetDashboardInput{
+		DashboardName: aws.String("CloudWatch-Default"),
+	}
+	result, err := svc.GetDashboard(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case cloudwatch.ErrCodeInternalServiceFault:
+				fmt.Println(cloudwatch.ErrCodeInternalServiceFault, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+	}
+	fmt.Println(result)
+}
+
+func ListMetrics() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{Region: aws.String(endpoints.ApSoutheast1RegionID)},
+	}))
+	svc := cloudwatch.New(sess)
+
+	input := &cloudwatch.ListMetricsInput{}
+	result, err := svc.ListMetrics(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case cloudwatch.ErrCodeInternalServiceFault:
+				fmt.Println(cloudwatch.ErrCodeInternalServiceFault, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+	}
+	fmt.Println(result)
+}
+
+func GetMetricStatistics() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{Region: aws.String(endpoints.ApSoutheast1RegionID)},
+	}))
+	svc := cloudwatch.New(sess)
+	d := 6 * 60 * time.Minute
+	statistics := make([]*string, 0)
+	statistics = append(statistics, aws.String("Sum"))
+	input := &cloudwatch.GetMetricStatisticsInput{
+		StartTime:  aws.Time(time.Now().UTC().Add(-d)),
+		EndTime:    aws.Time(time.Now().UTC()),
+		Period:     aws.Int64(300),
+		MetricName: aws.String("ClusterName,ServiceName"),
+		Namespace:  aws.String("ECS"),
+		Statistics: statistics,
+	}
+	result, err := svc.GetMetricStatistics(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case cloudwatch.ErrCodeInternalServiceFault:
+				fmt.Println(cloudwatch.ErrCodeInternalServiceFault, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+	}
+	fmt.Println(result)
+}
--- awssdk/main.go
+++ awssdk/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/futugyousuzu/goproject/awsgolang/cloudwatchdemo"
-	"github.com/futugyousuzu/goproject/awsgolang/servicediscoverydemo"
+	// "github.com/futugyousuzu/goproject/awsgolang/servicediscoverydemo"
 )
 
 func main() {
-	servicediscoverydemo.CreateNamespace()
-	fmt.Println("this is separator!")
-	cloudwatchdemo.GetMetricData()
+	// servicediscoverydemo.CreateNamespace()
+	// cloudwatchdemo.GetMetricData()
+	// cloudwatchdemo.GetDashboard()
+	// cloudwatchdemo.ListMetrics()
+	// cloudwatchdemo.GetMetricStatistics()
+	cloudwatchdemo.DescribeExportTasks()
 }
