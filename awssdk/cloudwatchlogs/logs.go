db44646b3bf63bae0844abbcf0ce468307b6421f
futugyou <17871902+futugyou@users.noreply.github.com>
2023-01-06 22:46:15 +0800

    compose all log
    compose all log

--- awssdk/cloudwatchlogs/logs.go
+++ awssdk/cloudwatchlogs/logs.go
@@ -33,8 +33,9 @@ func DescribeExportTasks() {
 }
 
 func DescribeLogGroups() {
+	// d := 6 * 60 * time.Minute
 	input := &cloudwatchlogs.DescribeLogGroupsInput{
-		Limit: aws.Int32(50),
+		Limit: aws.Int32(10),
 	}
 	result, err := svc.DescribeLogGroups(awsenv.EmptyContext, input)
 	if err != nil {
@@ -43,37 +44,66 @@ func DescribeLogGroups() {
 	}
 
 	for _, group := range result.LogGroups {
-		fmt.Println("LogGroupName:", *group.LogGroupName, "\tArn:", *group.Arn, "\tRetentionInDays:", group.RetentionInDays)
+		fmt.Println("LogGroupName:", *group.LogGroupName)
+		input := &cloudwatchlogs.DescribeLogStreamsInput{
+			Limit:        aws.Int32(10),
+			LogGroupName: group.LogGroupName,
+		}
+		result, err := svc.DescribeLogStreams(awsenv.EmptyContext, input)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		for _, stream := range result.LogStreams {
+			fmt.Println("\tLogStreamName:", *stream.LogStreamName)
+			input := &cloudwatchlogs.GetLogEventsInput{
+				Limit: aws.Int32(10),
+				// StartTime:     aws.Int64(time.Now().UTC().Add(-d).Unix()),
+				// EndTime:       aws.Int64(time.Now().UTC().Unix()),
+				LogGroupName:  group.LogGroupName,
+				LogStreamName: stream.LogStreamName,
+			}
+			result, err := svc.GetLogEvents(awsenv.EmptyContext, input)
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
+
+			for _, event := range result.Events {
+				fmt.Println("\t\tMessage:", *event.Message)
+			}
+		}
 	}
 }
 
-func GetLogEvents() {
-	d := 6 * 60 * time.Minute
-	input := &cloudwatchlogs.GetLogEventsInput{
-		Limit:         aws.Int32(50),
-		StartTime:     aws.Int64(time.Now().UTC().Add(-d).Unix()),
-		EndTime:       aws.Int64(time.Now().UTC().Unix()),
-		LogGroupName:  aws.String("/eks/openTelemetry"),
-		LogStreamName: aws.String("/eks/openTelemetry/7c04a4d0ccd74c9cbfc7a5e6e7d76501"),
-	}
-	result, err := svc.GetLogEvents(awsenv.EmptyContext, input)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println(result)
-}
+// func DescribeLogStreams() {
+// 	input := &cloudwatchlogs.DescribeLogStreamsInput{
+// 		Limit:        aws.Int32(50),
+// 		LogGroupName: aws.String("/eks/openTelemetry"),
+// 	}
+// 	result, err := svc.DescribeLogStreams(awsenv.EmptyContext, input)
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	fmt.Println(result)
+// }
 
-func DescribeLogStreams() {
-	input := &cloudwatchlogs.DescribeLogStreamsInput{
-		Limit:        aws.Int32(50),
-		LogGroupName: aws.String("/eks/openTelemetry"),
-	}
-	result, err := svc.DescribeLogStreams(awsenv.EmptyContext, input)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println(result)
-}
+// func GetLogEvents() {
+// 	d := 6 * 60 * time.Minute
+// 	input := &cloudwatchlogs.GetLogEventsInput{
+// 		Limit:         aws.Int32(50),
+// 		StartTime:     aws.Int64(time.Now().UTC().Add(-d).Unix()),
+// 		EndTime:       aws.Int64(time.Now().UTC().Unix()),
+// 		LogGroupName:  aws.String("/eks/openTelemetry"),
+// 		LogStreamName: aws.String("/eks/openTelemetry/7c04a4d0ccd74c9cbfc7a5e6e7d76501"),
+// 	}
+// 	result, err := svc.GetLogEvents(awsenv.EmptyContext, input)
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	fmt.Println(result)
+// }
 
 func GetLogGroupFields() {
 	d := 60 * time.Minute
