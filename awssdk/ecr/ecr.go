e6e63507a013d2558349b441887a7b3e1e5659bc
futugyou <17871902+futugyou@users.noreply.github.com>
2023-01-07 19:06:40 +0800

    update aws ecr add BatchGetImage and PutImage
    update aws ecr add BatchGetImage and PutImage

--- awssdk/ecr/ecr.go
+++ awssdk/ecr/ecr.go
@@ -1,10 +1,12 @@
 package ecr
 
 import (
+	"crypto/sha1"
 	"fmt"
 	"sort"
 
 	"github.com/aws/aws-sdk-go-v2/service/ecr"
+	"github.com/aws/aws-sdk-go-v2/service/ecr/types"
 	"github.com/futugyousuzu/goproject/awsgolang/awsenv"
 )
 
@@ -23,7 +25,7 @@ func DescribeRepositories() {
 		fmt.Println(err)
 		return
 	}
-
+	h := sha1.New()
 	for _, repository := range output.Repositories {
 		fmt.Println("RepositoryName:", *repository.RepositoryName, "\tRepositoryUri:", *repository.RepositoryUri)
 
@@ -42,12 +44,47 @@ func DescribeRepositories() {
 		}
 
 		sort.Slice(output.ImageDetails, func(i, j int) bool {
-			return output.ImageDetails[i].ImagePushedAt.Before(*output.ImageDetails[j].ImagePushedAt)
+			return output.ImageDetails[i].ImagePushedAt.After(*output.ImageDetails[j].ImagePushedAt)
 		})
 
-		for i := 0; i < len(output.ImageDetails); i++ {
+		for i := 0; i < len(output.ImageDetails) && i < 5; i++ {
 			fmt.Println("\tImageDigest:", *output.ImageDetails[i].ImageDigest, output.ImageDetails[i].ImagePushedAt)
 		}
+
+		imageIdentifier := types.ImageIdentifier{ImageDigest: output.ImageDetails[0].ImageDigest}
+		batchinput := &ecr.BatchGetImageInput{
+			ImageIds:       []types.ImageIdentifier{imageIdentifier},
+			RepositoryName: repository.RepositoryName,
+		}
+
+		batchoutput, err := svc.BatchGetImage(awsenv.EmptyContext, batchinput)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		for _, image := range batchoutput.Images {
+
+			h.Reset()
+			h.Write([]byte(*image.ImageId.ImageTag))
+			tag := fmt.Sprintf("%x", h.Sum(nil))
+
+			input := &ecr.PutImageInput{
+				ImageManifest:  image.ImageManifest,
+				RepositoryName: image.RepositoryName,
+				ImageTag:       &tag,
+			}
+
+			output, err := svc.PutImage(awsenv.EmptyContext, input)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			fmt.Println("\t\tImageTag:", *output.Image.ImageId.ImageTag)
+			break
+		}
+
 		fmt.Println()
 	}
 }
