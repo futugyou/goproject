6b76f7962a84c6e79af269b7cac6547e8db91486
futugyou <17871902+futugyou@users.noreply.github.com>
2023-01-07 16:49:23 +0800

    update ecr DescribeRepositories  with DescribeImages
    update ecr DescribeRepositories  with DescribeImages

--- awssdk/ecr/ecr.go
+++ awssdk/ecr/ecr.go
@@ -2,6 +2,7 @@ package ecr
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/aws/aws-sdk-go-v2/service/ecr"
 	"github.com/futugyousuzu/goproject/awsgolang/awsenv"
@@ -13,7 +14,6 @@ var (
 
 func init() {
 	svc = ecr.NewFromConfig(awsenv.Cfg)
-
 }
 
 func DescribeRepositories() {
@@ -26,5 +26,28 @@ func DescribeRepositories() {
 
 	for _, repository := range output.Repositories {
 		fmt.Println("RepositoryName:", *repository.RepositoryName, "\tRepositoryUri:", *repository.RepositoryUri)
+
+		// DescribeImages
+		input := &ecr.DescribeImagesInput{
+			RepositoryName: repository.RepositoryName,
+		}
+		output, err := svc.DescribeImages(awsenv.EmptyContext, input)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if len(output.ImageDetails) == 0 {
+			continue
+		}
+
+		sort.Slice(output.ImageDetails, func(i, j int) bool {
+			return output.ImageDetails[i].ImagePushedAt.Before(*output.ImageDetails[j].ImagePushedAt)
+		})
+
+		for i := 0; i < len(output.ImageDetails); i++ {
+			fmt.Println("\tImageDigest:", *output.ImageDetails[i].ImageDigest, output.ImageDetails[i].ImagePushedAt)
+		}
+		fmt.Println()
 	}
 }
