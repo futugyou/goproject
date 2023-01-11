a2fed1f8ec9ea5b6eccfd78e7211fa45e08f93a8
futugyou <17871902+futugyou@users.noreply.github.com>
2023-01-11 19:53:06 +0800

    CNP-42 finish other resource
    CNP-42 finish other resource

--- container/cgroups/subsystem/cpu.go
+++ container/cgroups/subsystem/cpu.go
@@ -1,1 +1,59 @@
 package subsystem
+
+import (
+	"os"
+	"path"
+	"strconv"
+
+	"github.com/sirupsen/logrus"
+)
+
+type CpuSubSystem struct {
+	apply bool
+}
+
+func (*CpuSubSystem) Name() string {
+	return "cpu"
+}
+
+func (c *CpuSubSystem) Set(cgroupPath string, res *ResourceConfig) error {
+	subsystemCgroupPath, err := GetCgroupPath(c.Name(), cgroupPath, true)
+	if err != nil {
+		logrus.Errorf("get %s path, err: %v", cgroupPath, err)
+		return err
+	}
+	if res.CpuShare != "" {
+		c.apply = true
+		err = os.WriteFile(path.Join(subsystemCgroupPath, "cpu.shares"), []byte(res.CpuShare), 0644)
+		if err != nil {
+			logrus.Errorf("failed to write file cpu.shares, err: %+v", err)
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *CpuSubSystem) Remove(cgroupPath string) error {
+	subsystemCgroupPath, err := GetCgroupPath(c.Name(), cgroupPath, false)
+	if err != nil {
+		return err
+	}
+	return os.RemoveAll(subsystemCgroupPath)
+}
+
+func (c *CpuSubSystem) Apply(cgroupPath string, pid int) error {
+	if c.apply {
+		subsystemCgroupPath, err := GetCgroupPath(c.Name(), cgroupPath, false)
+		if err != nil {
+			return err
+		}
+
+		tasksPath := path.Join(subsystemCgroupPath, "tasks")
+		err = os.WriteFile(tasksPath, []byte(strconv.Itoa(pid)), os.ModePerm)
+		if err != nil {
+			logrus.Errorf("write pid to tasks, path: %s, pid: %d, err: %v", tasksPath, pid, err)
+			return err
+		}
+	}
+	return nil
+}
--- container/cgroups/subsystem/util_test.go
+++ container/cgroups/subsystem/util_test.go
@@ -1,1 +1,13 @@
 package subsystem
+
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestGetCgroupPath(t *testing.T) {
+	logrus.Infof(findCgroupMountPoint("memory"))
+	logrus.Infof(findCgroupMountPoint("cpu"))
+	logrus.Infof(findCgroupMountPoint("cpuset"))
+}
