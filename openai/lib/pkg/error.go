96ebc69449dbbb27a301922e547a1edf6916ede0
futugyou <17871902+futugyou@users.noreply.github.com>
2023-02-26 16:39:50 +0800

    openai, remove omitempty from openaierror
    openai, remove omitempty from openaierror

--- openai/lib/pkg/error.go
+++ openai/lib/pkg/error.go
@@ -1,8 +1,8 @@
 package pkg
 
 type OpenaiError struct {
-	ErrorMessage string `json:"message,omitempty"`
-	ErrorType    string `json:"type,omitempty"`
-	Param        string `json:"param,omitempty"`
-	ErrorCode    string `json:"code,omitempty"`
+	ErrorMessage string `json:"message"`
+	ErrorType    string `json:"type"`
+	Param        string `json:"param"`
+	ErrorCode    string `json:"code"`
 }
