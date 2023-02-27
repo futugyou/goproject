03e08c6c52cdb3c357a6ae073facb1a3d69a0091
futugyou <17871902+futugyou@users.noreply.github.com>
2023-02-27 19:41:15 +0800

    openai, add multipart/form-data type for http client
    openai, add multipart/form-data type for http client

--- openai/lib/pkg/openaiclient.go
+++ openai/lib/pkg/openaiclient.go
@@ -6,7 +6,12 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
 )
 
 const baseUrl string = "https://api.openai.com/v1/"
@@ -55,6 +60,83 @@ func (c *openaiClient) doRequest(path, method string, request, response interfac
 
 	req, _ := http.NewRequest(method, path, body)
 	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", "Bearer", c.apikey))
+
+	if len(c.organization) > 0 {
+		req.Header.Set("organization", c.organization)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	defer resp.Body.Close()
+
+	all, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	json.Unmarshal(all, response)
+}
+
+func (c *openaiClient) PostWithFile(path string, request, response interface{}) {
+	path = c.baseurl + path
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	reType := reflect.TypeOf(request)
+	if reType.Kind() != reflect.Ptr || reType.Elem().Kind() != reflect.Struct {
+		fmt.Println("request must ptr")
+		return
+	}
+
+	v := reflect.ValueOf(request).Elem()
+	for i := 0; i < v.NumField(); i++ {
+		value := v.Field(i)
+		if value.IsZero() {
+			continue
+		}
+
+		structField := v.Type().Field(i)
+		fieldName := structField.Name
+		tag := structField.Tag.Get("json")
+		if len(tag) > 0 {
+			if strings.Contains(tag, ",") {
+				fieldName = strings.Split(tag, ",")[0]
+			} else {
+				fieldName = tag
+			}
+		}
+
+		switch v := value.Interface().(type) {
+		case float32:
+			writer.WriteField(fieldName, fmt.Sprintf("%f", v))
+		case float64:
+			writer.WriteField(fieldName, fmt.Sprintf("%f", v))
+		case int:
+			writer.WriteField(fieldName, strconv.Itoa(v))
+		case int32:
+			writer.WriteField(fieldName, strconv.FormatInt(int64(v), 10))
+		case int64:
+			writer.WriteField(fieldName, strconv.FormatInt(v, 10))
+		case string:
+			writer.WriteField(fieldName, v)
+		case *os.File:
+			wimage, _ := writer.CreateFormFile(fieldName, v.Name())
+			io.Copy(wimage, v)
+		}
+	}
+
+	writer.Close()
+
+	req, _ := http.NewRequest("POST", path, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
 	req.Header.Set("Authorization", fmt.Sprintf("%s %s", "Bearer", c.apikey))
 
 	if len(c.organization) > 0 {
