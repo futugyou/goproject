a7b17f617961697e83d6489026116ba861d7413e
futugyou <17871902+futugyou@users.noreply.github.com>
2023-03-02 13:36:36 +0800

    openai, add audio translation lib
    openai, add audio translation lib

--- openai/lib/audio.go
+++ openai/lib/audio.go
@@ -3,6 +3,7 @@ package lib
 import "os"
 
 const audioTranscriptionPath string = "audio/transcriptions"
+const audioTranslationPath string = "audio/translations"
 
 type CreateAudioTranscriptionRequest struct {
 	File           *os.File `json:"file"`
@@ -18,8 +19,27 @@ type CreateAudioTranscriptionResponse struct {
 	Text  string       `json:"text,omitempty"`
 }
 
+type CreateAudioTranslationRequest struct {
+	File           *os.File `json:"file"`
+	Model          string   `json:"model"`
+	Prompt         string   `json:"prompt,omitempty"`
+	ResponseFormat string   `json:"response_format,omitempty"` //  json, text, srt, verbose_json, or vtt.
+	Temperature    float32  `json:"temperature,omitempty"`
+}
+
+type CreateAudioTranslationResponse struct {
+	Error *OpenaiError `json:"error,omitempty"`
+	Text  string       `json:"text,omitempty"`
+}
+
 func (client *openaiClient) CreateAudioTranscription(request CreateAudioTranscriptionRequest) *CreateAudioTranscriptionResponse {
 	result := &CreateAudioTranscriptionResponse{}
 	client.PostWithFile(audioTranscriptionPath, &request, result)
+	return result
+}
+
+func (client *openaiClient) CreateAudioTranslation(request CreateAudioTranslationRequest) *CreateAudioTranslationResponse {
+	result := &CreateAudioTranslationResponse{}
+	client.PostWithFile(audioTranslationPath, &request, result)
 	return result
 }
