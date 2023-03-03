3d508099afc034c5f5edb7160370152e36695ba4
futugyou <17871902+futugyou@users.noreply.github.com>
2023-03-03 13:13:39 +0800

    openai, update audio validate
    openai, update audio validate

--- openai/lib/audio.go
+++ openai/lib/audio.go
@@ -11,8 +11,9 @@ import (
 const audioTranscriptionPath string = "audio/transcriptions"
 const audioTranslationPath string = "audio/translations"
 
-var responseFormatType = []string{"json", "text", "srt", "verbose_json", "vtt"}
-var audioType = []string{"mp3", "mp4", "mpeg", "mpga", "m4a", "wav", "webm"}
+var supportededResponseFormatType = []string{"json", "text", "srt", "verbose_json", "vtt"}
+var supportedAudioType = []string{"mp3", "mp4", "mpeg", "mpga", "m4a", "wav", "webm"}
+var supportedAudioModel = []string{"whisper-1"}
 
 var responseFormatTypeError = func(message string) *OpenaiError {
 	return &OpenaiError{
@@ -27,6 +28,14 @@ var audioTypeError = func(message string) *OpenaiError {
 		ErrorMessage: "audio type only mp3, mp4, mpeg, mpga, m4a, wav, or webm",
 		ErrorType:    "invalid parameters",
 		Param:        fmt.Sprintf("current audio type is: %s", message),
+	}
+}
+
+var audioModelError = func(message string) *OpenaiError {
+	return &OpenaiError{
+		ErrorMessage: "Only whisper-1 is currently available.",
+		ErrorType:    "invalid parameters",
+		Param:        fmt.Sprintf("current model is: %s", message),
 	}
 }
 
@@ -60,20 +69,22 @@ type CreateAudioTranslationResponse struct {
 func (client *openaiClient) CreateAudioTranscription(request CreateAudioTranscriptionRequest) *CreateAudioTranscriptionResponse {
 	result := &CreateAudioTranscriptionResponse{}
 
-	if len(request.ResponseFormat) > 0 && !slices.Contains(responseFormatType, request.ResponseFormat) {
-		result.Error = responseFormatTypeError(request.ResponseFormat)
+	err := validateAudioModel(request.ResponseFormat)
+	if err != nil {
+		result.Error = err
 		return result
 	}
 
-	segmentations := strings.Split(request.File.Name(), ".")
-	if len(segmentations) <= 1 {
-		result.Error = audioTypeError("no file extension")
+	err = validateAudioResponseFormat(request.ResponseFormat)
+	if err != nil {
+		result.Error = err
 		return result
 	}
 
-	suffix := strings.Split(request.File.Name(), ".")[len(segmentations)-1]
-	if !slices.Contains(audioType, suffix) {
-		result.Error = responseFormatTypeError(suffix)
+	err = validateAudioFile(*request.File)
+	if err != nil {
+		result.Error = err
+		return result
 	}
 
 	client.PostWithFile(audioTranscriptionPath, &request, result)
@@ -82,6 +93,55 @@ func (client *openaiClient) CreateAudioTranscription(request CreateAudioTranscri
 
 func (client *openaiClient) CreateAudioTranslation(request CreateAudioTranslationRequest) *CreateAudioTranslationResponse {
 	result := &CreateAudioTranslationResponse{}
+
+	err := validateAudioModel(request.ResponseFormat)
+	if err != nil {
+		result.Error = err
+		return result
+	}
+
+	err = validateAudioResponseFormat(request.ResponseFormat)
+	if err != nil {
+		result.Error = err
+		return result
+	}
+
+	err = validateAudioFile(*request.File)
+	if err != nil {
+		result.Error = err
+		return result
+	}
+
 	client.PostWithFile(audioTranslationPath, &request, result)
 	return result
+}
+
+func validateAudioResponseFormat(responseFormat string) *OpenaiError {
+	if len(responseFormat) > 0 && !slices.Contains(supportededResponseFormatType, responseFormat) {
+		return responseFormatTypeError(responseFormat)
+	}
+
+	return nil
+}
+
+func validateAudioModel(model string) *OpenaiError {
+	if len(model) == 0 || !slices.Contains(supportedAudioModel, model) {
+		return audioModelError(model)
+	}
+
+	return nil
+}
+
+func validateAudioFile(file os.File) *OpenaiError {
+	segmentations := strings.Split(file.Name(), ".")
+	if len(segmentations) <= 1 {
+		return audioTypeError("no file extension")
+	}
+
+	suffix := strings.ToLower(strings.Split(file.Name(), ".")[len(segmentations)-1])
+	if !slices.Contains(supportedAudioType, suffix) {
+		return responseFormatTypeError(suffix)
+	}
+
+	return nil
 }
