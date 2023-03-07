46af02ef1d5b5dc0ce253c506d68827d2566ae93
futugyou <17871902+futugyou@users.noreply.github.com>
2023-03-07 13:56:49 +0800

    beego, add validate for penalty
    beego, add validate for penalty

--- openai/models/q&a.go
+++ openai/models/q&a.go
@@ -8,10 +8,10 @@ type QuestionAnswer struct {
 	Prompt           string   `json:"prompt"`
 	MaxTokens        int32    `json:"max_tokens"`
 	Temperature      float32  `json:"temperature" valid:"ValidateTemperature"`
-	TopP             float32  `json:"top_p"`
-	FrequencyPenalty float32  `json:"frequency_penalty"`
-	PresencePenalty  float32  `json:"presence_penalty"`
-	BestOf           int      `json:"best_of" valid:"Range(1, 20)"`
+	TopP             float32  `json:"top_p" valid:"ValidateTemperature"`
+	FrequencyPenalty float32  `json:"frequency_penalty" valid:"ValidatePenalty"`
+	PresencePenalty  float32  `json:"presence_penalty" valid:"ValidatePenalty"`
+	BestOf           int32    `json:"best_of" valid:"Range(1, 20)"`
 	Echo             bool     `json:"echo"`
 	Logprobs         int      `json:"logprobs"`
 	Stop             []string `json:"stop"`
@@ -20,6 +20,7 @@ type QuestionAnswer struct {
 
 func init() {
 	validation.AddCustomFunc("ValidateTemperature", validateTemperature)
+	validation.AddCustomFunc("ValidatePenalty", validatePenalty)
 }
 
 var validateTemperature validation.CustomFunc = func(v *validation.Validation, obj interface{}, key string) {
@@ -31,5 +32,17 @@ var validateTemperature validation.CustomFunc = func(v *validation.Validation, o
 
 	if temperature < 0.0 || temperature > 1.0 {
 		v.AddError(key, "must in 0~1")
+	}
+}
+
+var validatePenalty validation.CustomFunc = func(v *validation.Validation, obj interface{}, key string) {
+	temperature, ok := obj.(float32)
+
+	if !ok {
+		return
+	}
+
+	if temperature < 0.0 || temperature > 2.0 {
+		v.AddError(key, "must in 0~2")
 	}
 }
