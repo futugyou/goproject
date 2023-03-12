5af20de41e9f5e9186e3dc07f3d9c709bba71d5e
futugyou <17871902+futugyou@users.noreply.github.com>
2023-03-12 22:09:13 +0800

    openai, add bytes to unicode
    openai, add bytes to unicode

--- openai/tokenizer/encoder.go
+++ openai/tokenizer/encoder.go
@@ -5,10 +5,15 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"golang.org/x/exp/slices"
 )
 
 var encoder map[string]int
 var vocab map[*vacabItem]int = make(map[*vacabItem]int)
+var bpeCache map[string]string = make(map[string]string)
+
+const encodingRegex string = `'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+`
 
 type vacabItem struct {
 	x string
@@ -16,6 +21,7 @@ type vacabItem struct {
 }
 
 func init() {
+	initializeBytesToUnicodeCache()
 	createEncoder()
 	createVocab()
 }
@@ -59,4 +65,39 @@ func filter(raws []string, filter func(string) bool) (ret []string) {
 	}
 
 	return
+}
+
+func initializeBytesToUnicodeCache() []string {
+	result := make([]string, 0)
+	list, list2 := []rune{}, []rune{}
+
+	for i := '!'; i < '~'+1; i++ {
+		list = append(list, i)
+		list2 = append(list2, i)
+	}
+
+	for i := '¡'; i < '¬'+1; i++ {
+		list = append(list, i)
+		list2 = append(list2, i)
+	}
+
+	for i := '®'; i < 'ÿ'+1; i++ {
+		list = append(list, i)
+		list2 = append(list2, i)
+	}
+
+	n := 1
+	for i := 0; i < 256; i++ {
+		if !slices.Contains(list, rune(i)) {
+			list = append(list, rune(i))
+			list2 = append(list2, rune(256+n))
+			n = n + 1
+		}
+	}
+
+	for i := 0; i < len(list2); i++ {
+		result = append(result, string(rune(list2[i])))
+	}
+
+	return result
 }
