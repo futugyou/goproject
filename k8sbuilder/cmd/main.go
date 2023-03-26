944dbe8c8ab88209f1c82af9638286bb2e688d8e
futugyou <17871902+futugyou@users.noreply.github.com>
2023-03-26 18:04:07 +0800

    kubebuilder, update webhook
    kubebuilder, update webhook

--- k8sbuilder/cmd/main.go
+++ k8sbuilder/cmd/main.go
@@ -106,9 +106,12 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "CronJob")
 		os.Exit(1)
 	}
-	if err = (&batchv1.CronJob{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "CronJob")
-		os.Exit(1)
+
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&batchv1.CronJob{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "CronJob")
+			os.Exit(1)
+		}
 	}
 	//+kubebuilder:scaffold:builder
 
