ddadd526a1f88e3a86f6a9eb3bcac8ac36d66270
futugyou <17871902+futugyou@users.noreply.github.com>
2023-03-29 15:54:49 +0800

    kubebuilder, add new kind
    kubebuilder, add new kind

--- k8sbuilder/cmd/main.go
+++ k8sbuilder/cmd/main.go
@@ -141,6 +141,13 @@ func main() {
 			os.Exit(1)
 		}
 	}
+	if err = (&webappcontroller.WelcomeReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Welcome")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
