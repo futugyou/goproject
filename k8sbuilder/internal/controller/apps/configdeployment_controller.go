06ec69f0ff1182c9fee1e238f4c0e13f4d22ef12
futugyou <17871902+futugyou@users.noreply.github.com>
2023-03-29 17:23:24 +0800

    kubebuilder, update config deployment reconciler
    kubebuilder, update config deployment reconciler

--- k8sbuilder/internal/controller/apps/configdeployment_controller.go
+++ k8sbuilder/internal/controller/apps/configdeployment_controller.go
@@ -19,12 +19,27 @@ package apps
 import (
 	"context"
 
+	"fmt"
+
+	kapps "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields" // Required for Watching
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types" // Required for Watching
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder" // Required for Watching
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler" // Required for Watching
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate" // Required for Watching
+	"sigs.k8s.io/controller-runtime/pkg/reconcile" // Required for Watching
+	"sigs.k8s.io/controller-runtime/pkg/source"    // Required for Watching
 
 	appsv1 "github.com/futugyousuzu/k8sbuilder/api/apps/v1"
+)
+
+const (
+	configMapField = ".spec.configMap"
 )
 
 // ConfigDeploymentReconciler reconciles a ConfigDeployment object
@@ -37,6 +52,10 @@ type ConfigDeploymentReconciler struct {
 //+kubebuilder:rbac:groups=apps.vishel.io,resources=configdeployments/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=apps.vishel.io,resources=configdeployments/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get
+//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
@@ -47,16 +66,76 @@ type ConfigDeploymentReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *ConfigDeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
+	log.WithValues("configDeployment", req.NamespacedName)
 
-	// TODO(user): your logic here
+	var configDeployment appsv1.ConfigDeployment
+	if err := r.Get(ctx, req.NamespacedName, &configDeployment); err != nil {
+		log.Error(err, "unable to fetch ConfigDeployment")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	var configMapVersion string
+	if configDeployment.Spec.ConfigMap != "" {
+		configMapName := configDeployment.Spec.ConfigMap
+		foundConfigMap := &corev1.ConfigMap{}
+		err := r.Get(ctx, types.NamespacedName{Name: configMapName, Namespace: configDeployment.Namespace}, foundConfigMap)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		configMapVersion = foundConfigMap.ResourceVersion
+	}
+
+	fmt.Println(configMapVersion)
+	// Logic here to add the configMapVersion as an annotation on your Deployment Pods.
 
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ConfigDeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	if err := mgr.GetFieldIndexer().IndexField(context.Background(), &appsv1.ConfigDeployment{}, configMapField, func(rawObj client.Object) []string {
+		// Extract the ConfigMap name from the ConfigDeployment Spec, if one is provided
+		configDeployment := rawObj.(*appsv1.ConfigDeployment)
+		if configDeployment.Spec.ConfigMap == "" {
+			return nil
+		}
+		return []string{configDeployment.Spec.ConfigMap}
+	}); err != nil {
+		return err
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&appsv1.ConfigDeployment{}).
+		Owns(&kapps.Deployment{}).
+		Watches(
+			&source.Kind{Type: &corev1.ConfigMap{}},
+			handler.EnqueueRequestsFromMapFunc(r.findObjectsForConfigMap),
+			builder.WithPredicates(predicate.ResourceVersionChangedPredicate{}),
+		).
 		Complete(r)
+}
+
+func (r *ConfigDeploymentReconciler) findObjectsForConfigMap(configMap client.Object) []reconcile.Request {
+	attachedConfigDeployments := &appsv1.ConfigDeploymentList{}
+	listOps := &client.ListOptions{
+		FieldSelector: fields.OneTermEqualSelector(configMapField, configMap.GetName()),
+		Namespace:     configMap.GetNamespace(),
+	}
+	err := r.List(context.TODO(), attachedConfigDeployments, listOps)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+
+	requests := make([]reconcile.Request, len(attachedConfigDeployments.Items))
+	for i, item := range attachedConfigDeployments.Items {
+		requests[i] = reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Name:      item.GetName(),
+				Namespace: item.GetNamespace(),
+			},
+		}
+	}
+	return requests
 }
