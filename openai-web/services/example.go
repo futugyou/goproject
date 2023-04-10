f1b0af23289f58e17aaeea086830b1fb562689b0
futugyou <17871902+futugyou@users.noreply.github.com>
2023-04-10 17:29:38 +0800

    beego, add cache for example service
    beego, add cache for example service

--- openai-web/services/example.go
+++ openai-web/services/example.go
@@ -2,7 +2,10 @@ package services
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+
+	"github.com/beego/beego/v2/core/logs"
 )
 
 type ExampleModel struct {
@@ -25,11 +28,49 @@ type ExampleModel struct {
 type ExampleService struct {
 }
 
-func (s *ExampleService) GetExampleSettings() []ExampleModel {
+const GetAllExamplesKey string = "GetAllExamplesKey"
 
+func (s *ExampleService) GetExampleSettings() []ExampleModel {
 	result := make([]ExampleModel, 0)
-	if settings, err := os.ReadFile("./examples/examples.json"); err == nil {
-		json.Unmarshal(settings, &result)
+	// get data from redis,
+	// it is not necessary at the moment, but examples.json data will migrate to db in the future
+	rmap, _ := Rbd.HGetAll(ctx, GetAllExamplesKey).Result()
+
+	if len(rmap) > 0 {
+		for _, r := range rmap {
+			example := ExampleModel{}
+			json.Unmarshal([]byte(r), &example)
+			result = append(result, example)
+		}
+
+		return result
+	}
+
+	// get data from file
+	var examples []byte
+	var err error
+
+	if examples, err = os.ReadFile("./examples/examples.json"); err != nil {
+		logs.Error(err)
+		return result
+	}
+
+	if err = json.Unmarshal(examples, &result); err != nil {
+		logs.Error(err)
+		return result
+	}
+
+	examplesCache := make(map[string]interface{})
+	for _, example := range result {
+		examplestring, _ := json.Marshal(example)
+		examplesCache[example.Key] = examplestring
+	}
+
+	count, err := Rbd.HSet(ctx, GetAllExamplesKey, examplesCache).Result()
+	if err != nil {
+		logs.Error(err)
+	} else {
+		logs.Info(fmt.Sprintf("example data count: %d", count))
 	}
 
 	return result
