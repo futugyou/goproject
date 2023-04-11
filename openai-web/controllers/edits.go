dd2df138bbce4c159225d0718338c29ffff17972
futugyou <17871902+futugyou@users.noreply.github.com>
2023-04-11 21:37:55 +0800

    beego, add edit controller
    beego, add edit controller

---
+++ openai-web/controllers/edits.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"encoding/json"
+
+	"github.com/beego/beego/v2/server/web"
+
+	"github.com/futugyousuzu/go-openai-web/services"
+)
+
+// Operations about edit
+type EditController struct {
+	web.Controller
+}
+
+// @Title Create edit
+// @Description create edit
+// @Param	body		body 	services.CreateEditsRequest	true		"body for create edit content"
+// @Success 200 {test} 	string
+// @router / [post]
+func (c *EditController) CreateEdit() {
+	var r services.CreateEditsRequest
+	json.Unmarshal(c.Ctx.Input.RequestBody, &r)
+
+	completionService := services.EditService{}
+	result := completionService.CreateEdit(r)
+	c.Ctx.JSONResp(result)
+}
