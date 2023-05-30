c153b535594f1685c664c6e8b73a7ed5ab579693
futugyou <17871902+futugyou@users.noreply.github.com>
2023-05-30 14:56:18 +0800

    add jwks store
    add jwks store

---
+++ identity-center/server/jwks.go
@@ -0,0 +1,111 @@
+package server
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/lestrrat-go/jwx/v2/jwk"
+)
+
+type JwksStore interface {
+	GetJwksList(ctx context.Context) (string, error)
+	CreateJwks(ctx context.Context, signed_key string) error
+}
+
+type JwkModel struct {
+	ID      string `bson:"_id"`
+	Payload string `bson:"payload"`
+}
+
+type MongoJwksStore struct {
+	DBName         string
+	CollectionName string
+	client         *mongo.Client
+}
+
+func NewJwksStore() *MongoJwksStore {
+	db := os.Getenv("db_name")
+	u_name := "oauth2_jwks"
+	url := os.Getenv("mongodb_url")
+	client, _ := mongo.Connect(context.TODO(), options.Client().ApplyURI(url))
+
+	return &MongoJwksStore{
+		DBName:         db,
+		CollectionName: u_name,
+		client:         client,
+	}
+}
+
+func (u *MongoJwksStore) GetJwksList(ctx context.Context) (string, error) {
+	coll := u.client.Database(u.DBName).Collection(u.CollectionName)
+
+	result := make([]JwkModel, 0)
+	filter := bson.D{}
+	cursor, err := coll.Find(context.TODO(), filter)
+	if err != nil {
+		panic(err)
+	}
+	// end find
+
+	if err = cursor.All(context.TODO(), &result); err != nil {
+		panic(err)
+	}
+
+	for _, data := range result {
+		cursor.Decode(&data)
+	}
+
+	s := make([]string, len(result))
+	for _, v := range result {
+		s = append(s, v.Payload)
+	}
+
+	return "{ \"keys\": [ " + strings.Join(s, ",") + " ] }", err
+}
+
+func (u *MongoJwksStore) CreateJwks(ctx context.Context, signed_key string) error {
+	coll := u.client.Database(u.DBName).Collection(u.CollectionName)
+	var jwkModel JwkModel
+	err := coll.FindOne(ctx, bson.D{{Key: "_id", Value: signed_key}}).Decode(&jwkModel)
+	if err == nil {
+		return nil
+	}
+
+	raw, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return errors.New(fmt.Sprintf("failed to generate new rsa private key: %s\n", err))
+	}
+
+	key, err := jwk.FromRaw(raw)
+	if err != nil {
+		return errors.New(fmt.Sprintf("failed to create RSA key: %s\n", err))
+	}
+	if _, ok := key.(jwk.RSAPrivateKey); !ok {
+		return errors.New(fmt.Sprintf("expected jwk.RSAPrivateKey, got %T\n", err))
+	}
+
+	key.Set(jwk.KeyIDKey, signed_key)
+
+	buf, err := json.MarshalIndent(key, "", "  ")
+	if err != nil {
+		return errors.New(fmt.Sprintf("failed to marshal key into JSON: %s\n", err))
+	}
+
+	jwkModel = JwkModel{
+		ID:      signed_key,
+		Payload: string(buf),
+	}
+
+	_, err = coll.InsertOne(ctx, jwkModel)
+	return err
+}
