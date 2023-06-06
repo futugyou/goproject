65fcd2ef28d8176287592cec2497d4f35d6c350b
futugyou <17871902+futugyou@users.noreply.github.com>
2023-06-06 18:45:17 +0800

    add vercel example
    add vercel example

---
+++ openai-web/api/examples.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/futugyousuzu/go-openai-web/services"
+)
+
+func Examples(w http.ResponseWriter, r *http.Request) {
+	typestring := r.URL.Query().Get("type")
+
+	exampleService := services.ExampleService{}
+	var result []services.ExampleModel
+	if typestring == "custom" {
+		result = exampleService.GetCustomExamples()
+	} else {
+		result = exampleService.GetSystemExamples()
+	}
+
+	body, _ := json.Marshal(result)
+	w.Write(body)
+	w.WriteHeader(200)
+
+}
