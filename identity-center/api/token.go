338a89f02f6f0e7fef6c324f88c53f334d4149f3
futugyou <17871902+futugyou@users.noreply.github.com>
2023-06-11 12:52:24 +0800

    update cros again and again

--- identity-center/api/token.go
+++ identity-center/api/token.go
@@ -3,10 +3,23 @@ package api
 import (
 	"net/http"
 
-	"github.com/futugyousuzu/identity/apiraw"
-	"github.com/futugyousuzu/identity/middleware"
+	"github.com/futugyousuzu/identity/server"
 )
 
 func Token(w http.ResponseWriter, r *http.Request) {
-	middleware.Cors(apiraw.Token)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS, HEAD")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Origin, Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-Token, x-requested-with")
+	w.Header().Set("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Token, Content-Length, Access-Control-Allow-Headers, Content-Type, Authorization")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	err := server.OAuthServer.HandleTokenRequest(w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
