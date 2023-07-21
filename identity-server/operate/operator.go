39f4310082665cb7132ea333dfa275a9304e90e0
futugyou <17871902+futugyou@users.noreply.github.com>
2023-07-21 21:31:13 +0800

    update operator

--- identity-server/operate/operator.go
+++ identity-server/operate/operator.go
@@ -2,40 +2,45 @@ package operate
 
 import (
 	base "github.com/futugyousuzu/identity-server/store/mongo"
-	jwkstore "github.com/futugyousuzu/identity-server/store/mongo/token"
-	userstore "github.com/futugyousuzu/identity-server/store/mongo/user"
-	jwksRepository "github.com/futugyousuzu/identity-server/token"
-	userRepository "github.com/futugyousuzu/identity-server/user"
+	jwkRepoImpl "github.com/futugyousuzu/identity-server/store/mongo/token"
+	userRepoImpl "github.com/futugyousuzu/identity-server/store/mongo/user"
+
+	jwksInterface "github.com/futugyousuzu/identity-server/token"
+	userInterface "github.com/futugyousuzu/identity-server/user"
 )
 
 type Operator struct {
-	UserRepository      userRepository.IUserRepository
-	UserLoginRepository userRepository.IUserLoginRepository
-	JwksRepository      jwksRepository.IJwksRepository
+	UserRepository      userInterface.IUserRepository
+	UserLoginRepository userInterface.IUserLoginRepository
+	JwksRepository      jwksInterface.IJwksRepository
+}
+
+func NewOperator() *Operator {
+	return &Operator{}
 }
 
 func DefaultOperator() *Operator {
+	o := NewOperator()
 	config := base.DBConfig{}
-	jwt := jwkstore.NewJwksStore(config)
-	user := userstore.NewUserStore(config)
-	userlogin := userstore.NewUserloginStore(config)
-	o := &Operator{
-		UserRepository:      user,
-		UserLoginRepository: userlogin,
-		JwksRepository:      jwt,
-	}
+	jwt := jwkRepoImpl.NewJwksStore(config)
+	user := userRepoImpl.NewUserStore(config)
+	userlogin := userRepoImpl.NewUserloginStore(config)
+
+	o.SetUserRepository(user)
+	o.SetUserLoginRepository(userlogin)
+	o.SetJwksRepository(jwt)
 
 	return o
 }
 
-func (o *Operator) SetUserRepository(repo userRepository.IUserRepository) {
+func (o *Operator) SetUserRepository(repo userInterface.IUserRepository) {
 	o.UserRepository = repo
 }
 
-func (o *Operator) SetUserLoginRepository(repo userRepository.IUserLoginRepository) {
+func (o *Operator) SetUserLoginRepository(repo userInterface.IUserLoginRepository) {
 	o.UserLoginRepository = repo
 }
 
-func (o *Operator) SetJwksRepository(repo jwksRepository.IJwksRepository) {
+func (o *Operator) SetJwksRepository(repo jwksInterface.IJwksRepository) {
 	o.JwksRepository = repo
 }
