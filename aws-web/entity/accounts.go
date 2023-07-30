37a346e5c9b515e42684b8f8ad098d982b1dacf8
futugyou <17871902+futugyou@users.noreply.github.com>
2023-07-30 20:55:05 +0800

    add account entity

---
+++ aws-web/entity/accounts.go
@@ -0,0 +1,14 @@
+package entity
+
+type AccountEntity struct {
+	Id              string `bson:"_id"`
+	Alias           string `bson:"alias"`
+	AccessKeyId     string `bson:"accessKeyId"`
+	SecretAccessKey string `bson:"secretAccessKey"`
+	Region          string `bson:"region"`
+	CreatedAt       int    `bson:"createdAt"`
+}
+
+func (AccountEntity) GetType() string {
+	return "accounts"
+}
