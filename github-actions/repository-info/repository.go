0867efdb4dfd326c7fd842cae5bfc94cffcc9e80
futugyou <17871902+futugyou@users.noreply.github.com>
2023-08-05 21:47:30 +0800

    get git repository

---
+++ github-actions/repository-info/repository.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"github.com/google/go-github/v53/github"
+)
+
+var repos = make([]*github.Repository, 0)
+
+func GetRepository() {
+	var err error
+	if reponame == nil {
+		opts := &github.RepositoryListOptions{
+			Sort: "pushed",
+			ListOptions: github.ListOptions{
+				PerPage: 1000,
+			},
+		}
+
+		repos, _, err = client.Repositories.List(ctx, "", opts)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		repo, _, err := client.Repositories.Get(ctx, "", *reponame)
+		if err != nil {
+			log.Println(err)
+		}
+
+		repos = append(repos, repo)
+	}
+}
