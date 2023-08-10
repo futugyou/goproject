7fadcacc4a37fe533b4f3f1085047c7264b6c4b8
futugyou <17871902+futugyou@users.noreply.github.com>
2023-08-10 15:31:57 +0800

    add account-id to Access-Control-Allow-Headers
    add account-id to Access-Control-Allow-Headers

--- aws-web/vercel/cros.go
+++ aws-web/vercel/cros.go
@@ -7,7 +7,7 @@ import (
 func CrosForVercel(w http.ResponseWriter, r *http.Request) bool {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS, HEAD")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Origin, Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-Token, x-requested-with")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Origin, Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-Token, x-requested-with, account-id")
 	w.Header().Set("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Token, Content-Length, Access-Control-Allow-Headers, Content-Type, Authorization")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 
