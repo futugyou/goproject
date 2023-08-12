fd8f50033d174b95a3e8f051775effdf7f5af471
futugyou <17871902+futugyou@users.noreply.github.com>
2023-08-12 14:22:47 +0800

    add ssm entity

---
+++ aws-web/entity/parameters.go
@@ -0,0 +1,29 @@
+package entity
+
+type ParameterEntity struct {
+	Id        string `bson:"_id,omitempty"`
+	AccountId string `bson:"account_id"`
+	Region    string `bson:"region"`
+	Key       string `bson:"key"`
+	Value     string `bson:"value"`
+	Version   string `bson:"version"`
+	OperateAt int64  `bson:"operate_at"`
+}
+
+func (ParameterEntity) GetType() string {
+	return "parameters"
+}
+
+type ParameterLogEntity struct {
+	Id        string `bson:"_id,omitempty"`
+	AccountId string `bson:"account_id"`
+	Region    string `bson:"region"`
+	Key       string `bson:"key"`
+	Value     string `bson:"value"`
+	Version   string `bson:"version"`
+	OperateAt int64  `bson:"operate_at"`
+}
+
+func (ParameterLogEntity) GetType() string {
+	return "parameter_logs"
+}
