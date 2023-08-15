bd1cf81da47c2f3cd2f1567f439ab9723d3e75ac
futugyou <17871902+futugyou@users.noreply.github.com>
2023-08-15 19:11:03 +0800

    update bson to json

--- aws-web/viewmodel/parameter.go
+++ aws-web/viewmodel/parameter.go
@@ -3,12 +3,12 @@ package viewmodel
 import "time"
 
 type ParameterViewModel struct {
-	Id        string    `bson:"_id,omitempty"`
-	AccountId string    `bson:"account_id"`
-	Region    string    `bson:"region"`
-	Key       string    `bson:"key"`
-	Value     string    `bson:"value,omitempty"`
-	Version   string    `bson:"version"`
-	NeedSync  bool      `bson:"need_sync,omitempty"`
-	OperateAt time.Time `bson:"operate_at"`
+	Id        string    `json:"id,omitempty"`
+	AccountId string    `json:"accountId"`
+	Region    string    `json:"region"`
+	Key       string    `json:"key"`
+	Value     string    `json:"value,omitempty"`
+	Version   string    `json:"version"`
+	NeedSync  bool      `json:"need_sync,omitempty"`
+	OperateAt time.Time `json:"operateAt"`
 }
