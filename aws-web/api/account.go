3ef498862d8032a9093c48d6f7aeec3d71033b39
futugyou <17871902+futugyou@users.noreply.github.com>
2023-08-28 16:55:14 +0800

    reduce the number of vercel apis

---
+++ aws-web/api/account.go
@@ -0,0 +1,127 @@
+package api
+
+import (
+	"strconv"
+
+	_ "github.com/joho/godotenv/autoload"
+
+	"encoding/json"
+	"net/http"
+
+	"github.com/futugyousuzu/goproject/awsgolang/core"
+	"github.com/futugyousuzu/goproject/awsgolang/services"
+	verceltool "github.com/futugyousuzu/goproject/awsgolang/vercel"
+	model "github.com/futugyousuzu/goproject/awsgolang/viewmodel"
+)
+
+func Account(w http.ResponseWriter, r *http.Request) {
+	if verceltool.CrosForVercel(w, r) {
+		return
+	}
+
+	if !verceltool.AuthForVercel(w, r) {
+		return
+	}
+
+	operatetype := r.URL.Query().Get("type")
+	switch operatetype {
+	case "getall":
+		getAllAccount(w, r)
+	case "get":
+		getAccount(w, r)
+	case "create":
+		createAccount(w, r)
+	case "delete":
+		deleteAccount(w, r)
+	case "update":
+		updateAccount(w, r)
+	}
+}
+
+func createAccount(w http.ResponseWriter, r *http.Request) {
+	var account model.UserAccount
+
+	err := json.NewDecoder(r.Body).Decode(&account)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	accountService := services.NewAccountService()
+
+	err = accountService.CreateAccount(account)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		w.WriteHeader(500)
+		return
+	}
+
+	w.WriteHeader(200)
+}
+
+func deleteAccount(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+
+	accountService := services.NewAccountService()
+
+	err := accountService.DeleteAccount(id)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(200)
+}
+
+func getAccount(w http.ResponseWriter, r *http.Request) {
+	accountService := services.NewAccountService()
+
+	id := r.URL.Query().Get("id")
+
+	account := accountService.GetAccountByID(id)
+
+	body, _ := json.Marshal(account)
+	w.Write(body)
+	w.WriteHeader(200)
+}
+
+func getAllAccount(w http.ResponseWriter, r *http.Request) {
+	accountService := services.NewAccountService()
+	var accounts []model.UserAccount
+
+	pageString := r.URL.Query().Get("page")
+	limitString := r.URL.Query().Get("limit")
+	page, _ := strconv.ParseInt(pageString, 10, 64)
+	limit, _ := strconv.ParseInt(limitString, 10, 64)
+
+	if page != 0 && limit != 0 {
+		paging := core.Paging{Page: page, Limit: limit}
+		accounts = accountService.GetAccountsByPaging(paging)
+	} else {
+		accounts = accountService.GetAllAccounts()
+	}
+
+	body, _ := json.Marshal(accounts)
+	w.Write(body)
+	w.WriteHeader(200)
+}
+
+func updateAccount(w http.ResponseWriter, r *http.Request) {
+	var account model.UserAccount
+
+	err := json.NewDecoder(r.Body).Decode(&account)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	accountService := services.NewAccountService()
+
+	err = accountService.UpdateAccount(account)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(200)
+}
---
+++ aws-web/api/parameter.go
@@ -0,0 +1,121 @@
+package api
+
+import (
+	"strconv"
+
+	_ "github.com/joho/godotenv/autoload"
+
+	"encoding/json"
+	"net/http"
+
+	"github.com/futugyousuzu/goproject/awsgolang/core"
+	"github.com/futugyousuzu/goproject/awsgolang/services"
+	verceltool "github.com/futugyousuzu/goproject/awsgolang/vercel"
+	model "github.com/futugyousuzu/goproject/awsgolang/viewmodel"
+)
+
+func Parameter(w http.ResponseWriter, r *http.Request) {
+	if verceltool.CrosForVercel(w, r) {
+		return
+	}
+
+	if !verceltool.AuthForVercel(w, r) {
+		return
+	}
+
+	operatetype := r.URL.Query().Get("type")
+	switch operatetype {
+	case "compare":
+		compareParameter(w, r)
+	case "get":
+		getParameter(w, r)
+	case "getall":
+		getallParameter(w, r)
+	case "sync":
+		syncParameter(w, r)
+	}
+}
+
+func compareParameter(w http.ResponseWriter, r *http.Request) {
+	parameterService := services.NewParameterService()
+
+	sourceid := r.URL.Query().Get("sourceid")
+	destid := r.URL.Query().Get("destid")
+
+	if len(sourceid) == 0 || len(destid) == 0 {
+		w.Write([]byte("sourceid or destid can not be empty"))
+		w.WriteHeader(400)
+		return
+	}
+
+	parameters := parameterService.CompareParameterByIDs(sourceid, destid)
+
+	body, _ := json.Marshal(parameters)
+	w.Write(body)
+	w.WriteHeader(200)
+}
+
+func getParameter(w http.ResponseWriter, r *http.Request) {
+	parameterService := services.NewParameterService()
+	id := r.URL.Query().Get("id")
+	parameter := parameterService.GetParameterByID(id)
+
+	body, _ := json.Marshal(parameter)
+	w.Write(body)
+	w.WriteHeader(200)
+}
+
+func getallParameter(w http.ResponseWriter, r *http.Request) {
+	parameterService := services.NewParameterService()
+	var parameters []model.ParameterViewModel
+
+	pageString := r.URL.Query().Get("page")
+	limitString := r.URL.Query().Get("limit")
+	alias := r.URL.Query().Get("alias")
+	region := r.URL.Query().Get("region")
+	key := r.URL.Query().Get("key")
+
+	page, _ := strconv.ParseInt(pageString, 10, 64)
+	limit, _ := strconv.ParseInt(limitString, 10, 64)
+	if page == 0 {
+		page = 1
+	}
+
+	if limit == 0 {
+		limit = 10
+	}
+
+	filer := model.ParameterFilter{
+		AccountAlias: alias,
+		Region:       region,
+		Key:          key,
+	}
+
+	paging := core.Paging{Page: page, Limit: limit}
+	parameters = parameterService.GetParametersByCondition(paging, filer)
+
+	body, _ := json.Marshal(parameters)
+	w.Write(body)
+	w.WriteHeader(200)
+}
+
+func syncParameter(w http.ResponseWriter, r *http.Request) {
+	parameterService := services.NewParameterService()
+
+	var sync model.SyncModel
+
+	err := json.NewDecoder(r.Body).Decode(&sync)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = parameterService.SyncParameterByID(sync.Id)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		w.WriteHeader(500)
+		return
+	}
+
+	w.WriteHeader(200)
+}
