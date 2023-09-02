5c585bda4d3042aa770eb381b14c26a4efd7b564
futugyou <17871902+futugyou@users.noreply.github.com>
2023-09-02 16:54:45 +0800

    add key/value service

---
+++ aws-web/services/keyvalue.go
@@ -0,0 +1,70 @@
+package services
+
+import (
+	"context"
+	"os"
+
+	"github.com/futugyousuzu/goproject/awsgolang/entity"
+	"github.com/futugyousuzu/goproject/awsgolang/repository"
+	"github.com/futugyousuzu/goproject/awsgolang/repository/mongorepo"
+	model "github.com/futugyousuzu/goproject/awsgolang/viewmodel"
+)
+
+type KeyValueService struct {
+	repository repository.IKeyValueRepository
+}
+
+func NewKeyValueService() *KeyValueService {
+	config := mongorepo.DBConfig{
+		DBName:        os.Getenv("db_name"),
+		ConnectString: os.Getenv("mongodb_url"),
+	}
+
+	return &KeyValueService{
+		repository: mongorepo.NewKeyValueRepository(config),
+	}
+}
+
+func (a *KeyValueService) GetAllKeyValues() []model.KeyValue {
+	KeyValues := make([]model.KeyValue, 0)
+	entities, err := a.repository.GetAll(context.Background())
+	if err != nil {
+		return KeyValues
+	}
+
+	for _, entity := range entities {
+		KeyValues = append(KeyValues, model.KeyValue{
+			Key:   entity.Key,
+			Value: entity.Value,
+		})
+	}
+
+	return KeyValues
+}
+
+func (a *KeyValueService) GetValueByKey(key string) *model.KeyValue {
+	keyvalue, err := a.repository.GetValueByKey(context.Background(), key)
+	if err != nil {
+		return nil
+	}
+
+	return &model.KeyValue{
+		Key:   keyvalue.Key,
+		Value: keyvalue.Value,
+	}
+}
+
+func (a *KeyValueService) CreateKeyValue(key string, value string) {
+	keyvalue, err := a.repository.GetValueByKey(context.Background(), key)
+	if err != nil {
+		keyvalue = &entity.KeyValueEntity{
+			Key:   key,
+			Value: value,
+		}
+		a.repository.Insert(context.Background(), *keyvalue)
+		return
+	} else {
+		keyvalue.Value = value
+		a.repository.Update(context.Background(), *keyvalue, keyvalue.Id)
+	}
+}
---
+++ aws-web/viewmodel/keyvalue.go
@@ -0,0 +1,6 @@
+package viewmodel
+
+type KeyValue struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
