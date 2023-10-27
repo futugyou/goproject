fb5e322d21ebf9e2fe42c1b3935fe936eb1cfe9b
futugyou <17871902+futugyou@users.noreply.github.com>
2023-10-27 16:46:09 +0800

    add target gourp and listener

--- aws-web/sdk/loadbalancing/loadbalancing.go
+++ aws-web/sdk/loadbalancing/loadbalancing.go
@@ -3,8 +3,10 @@ package loadbalancing
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2"
+	"github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2/types"
 	"github.com/futugyousuzu/goproject/awsgolang/awsenv"
 )
 
@@ -28,4 +30,45 @@ func DescribeTargetGroups() {
 	for _, target := range result.TargetGroups {
 		fmt.Println(*target.TargetGroupArn, *target.TargetGroupName, *target.VpcId)
 	}
+}
+
+func DescribeDescribeListeners() {
+	input := &elasticloadbalancingv2.DescribeListenersInput{}
+
+	result, err := svc.DescribeListeners(context.TODO(), input)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, target := range result.Listeners {
+		fmt.Println(*target.ListenerArn, *target.LoadBalancerArn)
+		for _, act := range target.DefaultActions {
+			fmt.Println(act.TargetGroupArn)
+		}
+	}
+}
+
+func GetTargetGroups() []types.TargetGroup {
+	input := &elasticloadbalancingv2.DescribeTargetGroupsInput{}
+
+	result, err := svc.DescribeTargetGroups(context.TODO(), input)
+	if err != nil {
+		log.Println(err)
+		return []types.TargetGroup{}
+	}
+
+	return result.TargetGroups
+}
+
+func GetLoadbalanceListeners() []types.Listener {
+	input := &elasticloadbalancingv2.DescribeListenersInput{}
+
+	result, err := svc.DescribeListeners(context.TODO(), input)
+	if err != nil {
+		log.Println(err)
+		return []types.Listener{}
+	}
+
+	return result.Listeners
 }
