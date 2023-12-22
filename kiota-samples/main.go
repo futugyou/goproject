b1f761a51bfb6b912a52f2c036b44f388ce29ca1
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2023-12-22 12:41:37 +0000

    azure ad example

--- kiota-samples/main.go
+++ kiota-samples/main.go
@@ -1,76 +1,130 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
+	"context"
+	"fmt"
+	"log"
 
-    "kiota-samples/client"
-    "kiota-samples/client/models"
+	"kiota-samples/client"
+	"kiota-samples/client/models"
 
-    auth "github.com/microsoft/kiota-abstractions-go/authentication"
-    http "github.com/microsoft/kiota-http-go"
+	client2 "kiota-samples/client2"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+
+	auth "github.com/microsoft/kiota-abstractions-go/authentication"
+	azure "github.com/microsoft/kiota-authentication-azure-go"
+	http "github.com/microsoft/kiota-http-go"
 )
 
 func main() {
-    // API requires no authentication, so use the anonymous
-    // authentication provider
-    authProvider := auth.AnonymousAuthenticationProvider{}
+	client_1()
+	client_2()
+}
 
-    // Create request adapter using the net/http-based implementation
-    adapter, err := http.NewNetHttpRequestAdapter(&authProvider)
-    if err != nil {
-        log.Fatalf("Error creating request adapter: %v\n", err)
-    }
+func client_1() {
+	// API requires no authentication, so use the anonymous
+	// authentication provider
+	authProvider := auth.AnonymousAuthenticationProvider{}
 
-    // Create the API client
-    client := client.NewPostsClient(adapter)
+	// Create request adapter using the net/http-based implementation
+	adapter, err := http.NewNetHttpRequestAdapter(&authProvider)
+	if err != nil {
+		log.Fatalf("Error creating request adapter: %v\n", err)
+	}
 
-    // GET /posts
-    allPosts, err := client.Posts().Get(context.Background(), nil)
-    if err != nil {
-        log.Fatalf("Error getting posts: %v\n", err)
-    }
-    fmt.Printf("Retrieved %d posts.\n", len(allPosts))
+	// Create the API client
+	client := client.NewPostsClient(adapter)
 
-    // GET /posts/{id}
-    specificPostId := int32(5)
-    specificPost, err := client.Posts().ByPostIdInteger(specificPostId).Get(context.Background(), nil)
-    if err != nil {
-        log.Fatalf("Error getting post by ID: %v\n", err)
-    }
-    fmt.Printf("Retrieved post - ID: %d, Title: %s, Body: %s\n", *specificPost.GetId(), *specificPost.GetTitle(), *specificPost.GetBody())
+	// GET /posts
+	allPosts, err := client.Posts().Get(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("Error getting posts: %v\n", err)
+	}
+	fmt.Printf("Retrieved %d posts.\n", len(allPosts))
 
-    // POST /posts
-    newPost := models.NewPost()
-    userId := int32(42)
-    newPost.SetUserId(&userId)
-    title := "Testing Kiota-generated API client"
-    newPost.SetTitle(&title)
-    body := "Hello world!"
-    newPost.SetBody(&body)
+	// GET /posts/{id}
+	specificPostId := int32(5)
+	specificPost, err := client.Posts().ByPostIdInteger(specificPostId).Get(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("Error getting post by ID: %v\n", err)
+	}
+	fmt.Printf("Retrieved post - ID: %d, Title: %s, Body: %s\n", *specificPost.GetId(), *specificPost.GetTitle(), *specificPost.GetBody())
 
-    createdPost, err := client.Posts().Post(context.Background(), newPost, nil)
-    if err != nil {
-        log.Fatalf("Error creating post: %v\n", err)
-    }
-    fmt.Printf("Created new post with ID: %d\n", *createdPost.GetId())
+	// POST /posts
+	newPost := models.NewPost()
+	userId := int32(42)
+	newPost.SetUserId(&userId)
+	title := "Testing Kiota-generated API client"
+	newPost.SetTitle(&title)
+	body := "Hello world!"
+	newPost.SetBody(&body)
 
-    // PATCH /posts/{id}
-    update := models.NewPost()
-    newTitle := "Updated title"
-    update.SetTitle(&newTitle)
+	createdPost, err := client.Posts().Post(context.Background(), newPost, nil)
+	if err != nil {
+		log.Fatalf("Error creating post: %v\n", err)
+	}
+	fmt.Printf("Created new post with ID: %d\n", *createdPost.GetId())
 
-    updatedPost, err := client.Posts().ByPostIdInteger(specificPostId).Patch(context.Background(), update, nil)
-    if err != nil {
-        log.Fatalf("Error updating post: %v\n", err)
-    }
-    fmt.Printf("Updated post - ID: %d, Title: %s, Body: %s\n", *updatedPost.GetId(), *updatedPost.GetTitle(), *updatedPost.GetBody())
+	// PATCH /posts/{id}
+	update := models.NewPost()
+	newTitle := "Updated title"
+	update.SetTitle(&newTitle)
 
-    // DELETE /posts/{id}
-    _, err = client.Posts().ByPostIdInteger(specificPostId).Delete(context.Background(), nil)
-    if err != nil {
-        log.Fatalf("Error deleting post: %v\n", err)
-    }
-    fmt.Printf("Deleted post\n")
+	updatedPost, err := client.Posts().ByPostIdInteger(specificPostId).Patch(context.Background(), update, nil)
+	if err != nil {
+		log.Fatalf("Error updating post: %v\n", err)
+	}
+	fmt.Printf("Updated post - ID: %d, Title: %s, Body: %s\n", *updatedPost.GetId(), *updatedPost.GetTitle(), *updatedPost.GetBody())
+
+	// DELETE /posts/{id}
+	_, err = client.Posts().ByPostIdInteger(specificPostId).Delete(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("Error deleting post: %v\n", err)
+	}
+	fmt.Printf("Deleted post\n")
+}
+
+func client_2() {
+	clientId := ""
+
+	// The auth provider will only authorize requests to
+	// the allowed hosts, in this case Microsoft Graph
+	allowedHosts := []string{"graph.microsoft.com"}
+	graphScopes := []string{"User.Read"}
+
+	credential, err := azidentity.NewDeviceCodeCredential(&azidentity.DeviceCodeCredentialOptions{
+		ClientID: clientId,
+		UserPrompt: func(ctx context.Context, dcm azidentity.DeviceCodeMessage) error {
+			fmt.Println(dcm.Message)
+			return nil
+		},
+	})
+
+	if err != nil {
+		fmt.Printf("Error creating credential: %v\n", err)
+	}
+
+	authProvider, err := azure.NewAzureIdentityAuthenticationProviderWithScopesAndValidHosts(
+		credential, graphScopes, allowedHosts)
+
+	if err != nil {
+		fmt.Printf("Error creating auth provider: %v\n", err)
+	}
+
+	adapter, err := http.NewNetHttpRequestAdapter(authProvider)
+
+	if err != nil {
+		fmt.Printf("Error creating request adapter: %v\n", err)
+	}
+
+	client := client2.NewGraphApiClient(adapter)
+
+	me, err := client.Me().Get(context.Background(), nil)
+
+	if err != nil {
+		fmt.Printf("Error getting user: %v\n", err)
+	}
+
+	fmt.Printf("Hello %s, your ID is %s\n", *me.GetDisplayName(), *me.GetId())
 }
--- kiota-samples/readme.md
+++ kiota-samples/readme.md
@@ -7,4 +7,5 @@ export PATH="$PATH:/home/codespace/.dotnet/tools"
 
 ```cmd
 kiota generate -l go -c PostsClient -n kiota-samples/client -d ./posts-api.yml -o ./client
+kiota generate -l go -c GraphApiClient -n kiota-samples/client2 -d ./get-me.yml -o ./client2
 ```
