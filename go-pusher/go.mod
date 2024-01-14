99e54b6c36c88f948fe97ad4da98a7053c982feb
futugyou <17871902+futugyou@users.noreply.github.com>
2024-01-14 18:49:08 +0800

    pub/sub with pusher
    pub/sub with pusher

---
+++ go-pusher/go.mod
@@ -0,0 +1,9 @@
+module go-pusher
+
+go 1.20
+
+require (
+	github.com/pusher/pusher-http-go/v5 v5.1.1 // indirect
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
+	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
+)
---
+++ go-pusher/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/pusher/pusher-http-go/v5"
+)
+
+func main() {
+	mux := &MyMux{}
+	http.ListenAndServe(":9090", mux)
+}
+
+type MyMux struct{}
+
+func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/api/push" {
+		sayhelloName(w, r)
+		return
+	}
+	http.NotFound(w, r)
+}
+
+type Data struct {
+	X0    interface{} `json:"x0"`
+	X1    interface{} `json:"x1"`
+	Y0    interface{} `json:"y0"`
+	Y1    interface{} `json:"y1"`
+	Color string      `json:"color"`
+}
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS, HEAD")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Origin, Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-Token, x-requested-with, account-id")
+	w.Header().Set("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Token, Content-Length, Access-Control-Allow-Headers, Content-Type, Authorization")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+	}
+
+	pusherClient := pusher.Client{
+		AppID:   "",
+		Key:     "",
+		Secret:  "",
+		Cluster: "ap3",
+		Secure:  true,
+	}
+
+	var d Data
+	err := json.NewDecoder(r.Body).Decode(&d)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	data := map[string]interface{}{
+		"x0":    d.X0,
+		"x1":    d.X1,
+		"y0":    d.Y0,
+		"y1":    d.Y1,
+		"color": d.Color,
+	}
+
+	err = pusherClient.Trigger("my-channel", "my-event", data)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
