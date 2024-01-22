daf752149fd3eb82f976c022dd71edf32743b42b
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-01-22 06:44:41 +0000

    beams push to user

--- go-pusher/main.go
+++ go-pusher/main.go
@@ -49,6 +49,10 @@ func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		beamspush(w, r)
 		return
 	}
+	if r.URL.Path == "/api/beams/pushuser" {
+		beamspushuser(w, r)
+		return
+	}
 	http.NotFound(w, r)
 }
 
@@ -306,16 +310,70 @@ func beamspush(w http.ResponseWriter, r *http.Request) {
 	}
 
 	publishRequest := map[string]interface{}{
+		"apns": map[string]interface{}{
+			"aps": map[string]interface{}{
+				"alert": map[string]interface{}{
+					"title": "Hello",
+					"body":  "Hello, world",
+				},
+			},
+		},
+		"fcm": map[string]interface{}{
+			"notification": map[string]interface{}{
+				"title": "Hello",
+				"body":  "Hello, world",
+			},
+		},
 		"web": map[string]interface{}{
 			"notification": map[string]interface{}{
-				"title":     "Hello",
-				"body":      "Hello, world",
-				"deep_link": "https://www.pusher.com",
+				"title": "Hello",
+				"body":  "Hello, world",
 			},
 		},
 	}
 
 	pubId, err := beamsClient.PublishToInterests([]string{"hello"}, publishRequest)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Publish Id:", pubId)
+}
+
+func beamspushuser(w http.ResponseWriter, r *http.Request) {
+	cors(w, r)
+
+	beamsClient, err := pushnotifications.New(config.INSTANCE_ID, config.SECRET_KEY)
+	if err != nil {
+		fmt.Println("Could not create Beams Client:", err.Error())
+		return
+	}
+
+	publishRequest := map[string]interface{}{
+		"apns": map[string]interface{}{
+			"aps": map[string]interface{}{
+				"alert": map[string]interface{}{
+					"title": "Hello",
+					"body":  "Hello, world",
+				},
+			},
+		},
+		"fcm": map[string]interface{}{
+			"notification": map[string]interface{}{
+				"title": "Hello",
+				"body":  "Hello, world",
+			},
+		},
+		"web": map[string]interface{}{
+			"notification": map[string]interface{}{
+				"title": "Hello",
+				"body":  "Hello, world",
+			},
+		},
+	}
+
+	pubId, err := beamsClient.PublishToUsers([]string{"user-001", "user-002"}, publishRequest)
 	if err != nil {
 		fmt.Println(err)
 		return
