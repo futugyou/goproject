888ffc993994c83956efa419043cb1cbdc237835
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-01-27 10:11:19 +0000

    init go pulsar client

---
+++ go-pulsar/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/apache/pulsar-client-go/pulsar"
+)
+
+func main() {
+	client, err := pulsar.NewClient(pulsar.ClientOptions{
+		URL:               "pulsar://localhost:6650",
+		OperationTimeout:  30 * time.Second,
+		ConnectionTimeout: 30 * time.Second,
+	})
+	if err != nil {
+		log.Fatalf("Could not instantiate Pulsar client: %v", err)
+	}
+
+	defer client.Close()
+}
