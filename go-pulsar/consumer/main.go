5f0982a17efb35c9f21f9542c6a25aaeb9d59ddf
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-01-27 10:29:37 +0000

    pulsar consumer

---
+++ go-pulsar/consumer/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/apache/pulsar-client-go/pulsar"
+)
+
+func main() {
+	client, err := pulsar.NewClient(pulsar.ClientOptions{
+		URL:               "pulsar://localhost:6650",
+		OperationTimeout:  30 * time.Second,
+		ConnectionTimeout: 30 * time.Second,
+	})
+	if err != nil {
+		log.Fatalf("Could not instantiate Pulsar client: %v", err)
+	}
+
+	defer client.Close()
+
+	createConsumer(client)
+}
+
+func createConsumer(client pulsar.Client) {
+	consumer, err := client.Subscribe(pulsar.ConsumerOptions{
+		Topic:            "topic-1",
+		SubscriptionName: "my-sub",
+		Type:             pulsar.Shared,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer consumer.Close()
+
+	for i := 0; i < 10; i++ {
+		// may block here
+		msg, err := consumer.Receive(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Received message msgId: %#v -- content: '%s'\n",
+			msg.ID(), string(msg.Payload()))
+
+		consumer.Ack(msg)
+	}
+
+	if err := consumer.Unsubscribe(); err != nil {
+		log.Fatal(err)
+	}
+}
