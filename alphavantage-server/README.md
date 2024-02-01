1348b86f22debda6ab0ed79eccad468798b48a5e
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-02-01 11:16:36 +0000

    create alphavantage server project

---
+++ alphavantage-server/README.md
@@ -0,0 +1,5 @@
+```golang
+go mod edit -replace github.com/futugyousuzu/alphavantage=../alphavantage
+```
+
+### add use and replace in go.work
---
+++ alphavantage-server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+
+	"github.com/futugyousuzu/alphavantage"
+)
+
+func main() {
+	apikey := os.Getenv("ALPHAVANTAGE_API_KEY")
+	fmt.Println(apikey)
+	s := alphavantage.NewTimeSeriesClient(apikey)
+	p := alphavantage.TimeSeriesParameter{
+		Function: "TIME_SERIES_WEEKLY",
+		Symbol:   "IBM",
+		Interval: "15min",
+	}
+	result, err := s.ReadTimeSeries(p)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	for _, v := range result {
+		fmt.Println(v.DividendAmount, v.SplitCoefficient)
+	}
+}
