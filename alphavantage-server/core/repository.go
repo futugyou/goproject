b53051dbd3a7af1c7caff878cb9ceaccb14e1adb
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-02-12 09:04:45 +0000

    use mongodb to implement universal repository

--- alphavantage-server/core/repository.go
+++ alphavantage-server/core/repository.go
@@ -2,10 +2,112 @@ package core
 
 import (
 	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type IRepository[E IEntity, K any] interface {
 	GetAll(ctx context.Context) ([]E, error)
 	Paging(ctx context.Context, page Paging) ([]E, error)
 	InsertMany(ctx context.Context, items []E) error
+}
+
+type DBConfig struct {
+	DBName        string
+	ConnectString string
+}
+
+type MongoRepository[E IEntity, K any] struct {
+	DBName string
+	Client *mongo.Client
+}
+
+func NewMongoRepository[E IEntity, K any](config DBConfig) *MongoRepository[E, K] {
+	client, _ := mongo.Connect(context.TODO(), options.Client().ApplyURI(config.ConnectString))
+	return &MongoRepository[E, K]{
+		DBName: config.DBName,
+		Client: client,
+	}
+}
+
+func (s *MongoRepository[E, K]) GetAll(ctx context.Context) ([]E, error) {
+	result := make([]E, 0)
+	entity := new(E)
+	c := s.Client.Database(s.DBName).Collection((*entity).GetType())
+
+	filter := bson.D{}
+	cursor, err := c.Find(ctx, filter)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	if err = cursor.All(ctx, &result); err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	for _, data := range result {
+		cursor.Decode(&data)
+	}
+
+	return result, nil
+}
+
+func (s *MongoRepository[E, K]) InsertMany(ctx context.Context, items []E) error {
+	if len(items) == 0 {
+		return nil
+	}
+
+	item := items[0]
+	c := s.Client.Database(s.DBName).Collection(item.GetType())
+	entitys := make([]interface{}, len(items))
+	for i := 0; i < len(items); i++ {
+		entitys[i] = items[i]
+	}
+
+	result, err := c.InsertMany(ctx, entitys)
+	if err != nil {
+		return err
+	}
+
+	log.Println("Inserted Count: ", len(result.InsertedIDs))
+	return nil
+}
+
+func (s *MongoRepository[E, K]) Paging(ctx context.Context, page Paging) ([]*E, error) {
+	result := make([]*E, 0)
+	entity := new(E)
+	c := s.Client.Database(s.DBName).Collection((*entity).GetType())
+
+	filter := bson.D{}
+	var skip int64 = (page.Page - 1) * page.Limit
+	op := options.Find().SetLimit(page.Limit).SetSkip(skip)
+	if len(page.SortField) > 0 {
+		if page.Direct == ASC {
+			op.SetSort(bson.D{{Key: page.SortField, Value: 1}})
+		} else {
+			op.SetSort(bson.D{{Key: page.SortField, Value: -1}})
+		}
+	}
+
+	cursor, err := c.Find(ctx, filter, op)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	if err = cursor.All(ctx, &result); err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	for _, data := range result {
+		cursor.Decode(&data)
+	}
+
+	return result, nil
 }
