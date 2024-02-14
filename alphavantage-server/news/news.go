2287e41fdf4ca226a119770b5944ea4d4dc97db3
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-02-14 09:17:24 +0000

    add new sync method

---
+++ alphavantage-server/news/news.go
@@ -0,0 +1,104 @@
+package news
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/futugyou/alphavantage"
+	"github.com/futugyou/alphavantage-server/core"
+	"github.com/futugyou/alphavantage-server/stock"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func SyncNewsSentimentData() {
+	log.Println("news sentiment data sync start.")
+	// get stock symbol data from db
+	list, err := stock.StockSymbolDatas()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if len(list) == 0 {
+		return
+	}
+
+	// create news list
+	data := make([]NewsEntity, 0)
+	apikey := os.Getenv("ALPHAVANTAGE_API_KEY")
+	client := alphavantage.NewIntelligenceClient(apikey)
+	for i := 0; i < len(list); i++ {
+		symbol := list[i].Symbol
+		log.Printf("start to get %s data \n", symbol)
+		t := time.Now()
+		y, m, d := t.Date()
+		p := alphavantage.SentimentParameter{
+			Tickers:  symbol,
+			TimeFrom: time.Date(y, m, d-1, 0, 0, 0, 1, t.Location()).Format("20060102T1504"),
+			TimeTo:   time.Date(y, m, d, 0, 0, 0, -1, t.Location()).Format("20060102T1504"),
+		}
+		s, err := client.NewsSentiment(p)
+		if err != nil || s == nil {
+			log.Println(err)
+			continue
+		}
+		log.Printf("%s feed count is %d \n", symbol, len(s.Feed))
+		for ii := 0; ii < len(s.Feed); ii++ {
+			timePublished, err := time.Parse("20060102T150405", s.Feed[ii].TimePublished)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			e := NewsEntity{
+				Id:                    s.Feed[ii].Title + s.Feed[ii].TimePublished,
+				Title:                 s.Feed[ii].Title,
+				URL:                   s.Feed[ii].URL,
+				TimePublished:         timePublished,
+				Authors:               s.Feed[ii].Authors,
+				Summary:               s.Feed[ii].Summary,
+				BannerImage:           s.Feed[ii].BannerImage,
+				Source:                s.Feed[ii].Source,
+				CategoryWithinSource:  s.Feed[ii].CategoryWithinSource,
+				SourceDomain:          s.Feed[ii].SourceDomain,
+				Topics:                getTopics(s.Feed[ii].Topics),
+				OverallSentimentScore: s.Feed[ii].OverallSentimentScore,
+				OverallSentimentLabel: s.Feed[ii].OverallSentimentLabel,
+				TickerSentiment:       getTickerSentiment(s.Feed[ii].TickerSentiment),
+			}
+			data = append(data, e)
+		}
+	}
+
+	// insert data
+	config := core.DBConfig{
+		DBName:        os.Getenv("db_name"),
+		ConnectString: os.Getenv("mongodb_url"),
+	}
+
+	repository := NewNewsRepository(config)
+	repository.InsertMany(context.Background(), data, NewsFilter)
+	log.Println("news sentiment data sync finish")
+}
+
+func getTickerSentiment(tickerSentiment []alphavantage.TickerSentiment) []string {
+	r := make([]string, 0)
+	for _, v := range tickerSentiment {
+		r = append(r, v.Ticker)
+	}
+	return r
+}
+
+func getTopics(topic []alphavantage.Topic) []string {
+	r := make([]string, 0)
+	for _, v := range topic {
+		r = append(r, v.Topic)
+	}
+	return r
+}
+
+func NewsFilter(e NewsEntity) primitive.D {
+	return bson.D{{Key: "title", Value: e.Title}}
+}
