ee9e748475eeb8d9f046ce05ea7f519d24e59860
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-02-22 12:26:14 +0000

    handle array types

--- tour/internal/mongo2struct/conf.go
+++ tour/internal/mongo2struct/conf.go
@@ -15,6 +15,17 @@ type MongoDBConfig struct {
 	ConnectString string
 }
 
+type Struct struct {
+	Name string
+	Item []StructItem
+}
+
+type StructItem struct {
+	Name string
+	Type string
+	Tag  string
+}
+
 func (m MongoDBConfig) Check() error {
 	if len(m.DBName) == 0 {
 		return fmt.Errorf("mongodb name can not be nil")
@@ -33,8 +44,78 @@ func (m *MongoDBConfig) Generator() {
 	}
 	db := client.Database(m.DBName)
 	filter := bson.D{}
-	tables, _ := db.ListCollectionSpecifications(context.Background(), filter)
-	for _, v := range tables {
-		fmt.Println(v.Name)
+	tables, err := db.ListCollectionSpecifications(context.Background(), filter)
+	if err != nil {
+		log.Println(err)
+		return
 	}
+
+	for _, c := range tables {
+		generatorTabel(db, c.Name)
+	}
+}
+
+func generatorTabel(db *mongo.Database, collectionName string) {
+	c := db.Collection(collectionName)
+	result := c.FindOne(context.Background(), bson.D{})
+	b, err := result.Raw()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	e, err := b.Elements()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	s := Struct{Name: collectionName, Item: make([]StructItem, 0)}
+
+	for _, v := range e {
+		itemType := convertBsontypeTogotype(v.Value())
+		s.Item = append(s.Item, StructItem{Name: v.Key(), Type: itemType, Tag: fmt.Sprintf("`bson:\"%s\"`", v.Key())})
+	}
+	fmt.Println(s)
+}
+
+func convertBsontypeTogotype(value bson.RawValue) string {
+	switch value.Type {
+	case bson.TypeDouble:
+		return "float64"
+	case bson.TypeString:
+		return "string"
+	case bson.TypeEmbeddedDocument:
+	case bson.TypeArray:
+		e, err := value.Array().Elements()
+		if err != nil || len(e) == 0 {
+			return "[]interface{}"
+		}
+		return "[]" + convertBsontypeTogotype(e[0].Value())
+	case bson.TypeBinary:
+	case bson.TypeUndefined:
+	case bson.TypeObjectID:
+		fmt.Println(5)
+	case bson.TypeBoolean:
+		return "bool"
+	case bson.TypeDateTime:
+		return "time.Time"
+	case bson.TypeNull:
+	case bson.TypeRegex:
+	case bson.TypeDBPointer:
+	case bson.TypeJavaScript:
+	case bson.TypeSymbol:
+	case bson.TypeCodeWithScope:
+	case bson.TypeInt32:
+		return "int32"
+	case bson.TypeTimestamp:
+		return "int64"
+	case bson.TypeInt64:
+		return "int64"
+	case bson.TypeDecimal128:
+		return "float64"
+	case bson.TypeMinKey:
+	case bson.TypeMaxKey:
+	}
+
+	return "interface{}"
 }
