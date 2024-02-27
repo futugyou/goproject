b63c9c1c32e59a6f8aa426784b68bccdbfd01de6
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-02-27 11:17:48 +0000

    assembly repository generate

--- InfrTtemplate/main.go
+++ InfrTtemplate/main.go
@@ -1,7 +1,29 @@
 package main
 
+import (
+	_ "github.com/joho/godotenv/autoload"
+	// "context"
+	// "fmt"
+	// "os"
+	// "github.com/futugyou/InfrTtemplate/core"
+	// "github.com/futugyou/InfrTtemplate/mongorepo"
+)
+
 //go:generate go install github.com/joho/godotenv/cmd/godotenv@latest
 //go:generate godotenv -f ./.env go run ../tour/main.go  mongo g -p github.com/futugyou/InfrTtemplate
 func main() {
+	// config := mongorepo.DBConfig{
+	// 	DBName:        os.Getenv("db_name"),
+	// 	ConnectString: os.Getenv("mongodb_url"),
+	// }
 
+	// repository := mongorepo.NewBaseDatasRepository(config)
+	// list, err := repository.Paging(context.Background(), core.Paging{Page: 1, Limit: 100}, []core.DataFilterItem{})
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// for _, v := range list {
+	// 	fmt.Println(v.Symbol)
+	// }
 }
--- tour/internal/mongo2struct/manager.go
+++ tour/internal/mongo2struct/manager.go
@@ -29,9 +29,21 @@ func NewManager(db *mongo.Database, entityFolder string, repoFolder string, pkgN
 }
 
 func (m *Manager) Generator() {
-	m.generatorCore()
-	m.generatorEntity()
-	m.generatorRepository()
+	if err := m.generatorCore(); err != nil {
+		log.Println(err)
+		return
+	}
+
+	list, err := m.getEntityStructList()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if err := m.generatorEntity(list); err != nil {
+		log.Println(err)
+		return
+	}
+	m.generatorRepository(list)
 	m.formatCode()
 }
 
@@ -42,23 +54,20 @@ func (m *Manager) generatorCore() error {
 	}{
 		BasePackageName: m.BasePackageName,
 	}
-	m.Template.GenerateBaseRepoImpl(obj)
-	return nil
+
+	return m.Template.GenerateBaseRepoImpl(obj)
 }
 
-func (m *Manager) generatorEntity() error {
+func (m *Manager) getEntityStructList() ([]EntityStruct, error) {
 	tables, err := m.DB.ListCollectionSpecifications(context.Background(), bson.D{})
 	if err != nil {
-		log.Println(err)
-		return err
+		return []EntityStruct{}, err
 	}
 
-	entityList, err := m.createEntityList(tables)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
+	return m.createEntityList(tables)
+}
 
+func (m *Manager) generatorEntity(entityList []EntityStruct) error {
 	var wg sync.WaitGroup
 	for _, entity := range entityList {
 		wg.Add(1)
@@ -117,7 +126,26 @@ func (m *Manager) createRawElements(name string) ([]bson.RawElement, error) {
 	return b.Elements()
 }
 
-func (m *Manager) generatorRepository() {
+func (m *Manager) generatorRepository(eList []EntityStruct) {
+	list := make([]RepositoryStruct, 0)
+	for _, v := range eList {
+		list = append(list, RepositoryStruct{
+			BasePackageName: m.BasePackageName,
+			FileName:        v.FileName,
+			RepoName:        v.StructName,
+		})
+	}
+	var wg sync.WaitGroup
+	for _, entity := range list {
+		wg.Add(1)
+		go func(entity RepositoryStruct, wg *sync.WaitGroup) {
+			defer wg.Done()
+			m.Template.GenerateRepository(entity)
+		}(entity, &wg)
+	}
+
+	wg.Wait()
+
 }
 
 func (m *Manager) formatCode() {
