bdbdb13a188cf20adddf8259cb38b76cf6ce46c7
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-02-29 11:44:48 +0000

    analyse struct information from folder

---
+++ tour/util/struct.go
@@ -0,0 +1,61 @@
+package util
+
+import (
+	"bytes"
+	"go/ast"
+	"go/parser"
+	"go/printer"
+	"go/token"
+	"log"
+)
+
+func GetStructsFromFolder(filePath string) (structs []StructInfo, err error) {
+	fset := token.NewFileSet()
+	packages, err := parser.ParseDir(fset, filePath, nil, 0)
+	for _, pack := range packages {
+		for _, file := range pack.Files {
+			ast.Inspect(file, func(n ast.Node) bool {
+				switch x := n.(type) {
+				case *ast.TypeSpec:
+					fls := make([]FieldInfo, 0)
+					v := x.Type.(*ast.StructType)
+					for _, field := range v.Fields.List {
+						for _, name := range field.Names {
+							// get field.Type as string
+							var typeNameBuf bytes.Buffer
+							err := printer.Fprint(&typeNameBuf, fset, field.Type)
+							if err != nil {
+								log.Fatalf("failed printing %s", err)
+								continue
+							}
+							fls = append(fls, FieldInfo{
+								Name:     name.Name,
+								TypeName: typeNameBuf.String(),
+							})
+						}
+
+					}
+					structs = append(structs, StructInfo{
+						PackageName: pack.Name,
+						StructName:  x.Name.Name,
+						FieldInfos:  fls,
+					})
+				}
+
+				return true
+			})
+		}
+	}
+
+	return
+}
+
+type StructInfo struct {
+	PackageName string
+	StructName  string
+	FieldInfos  []FieldInfo
+}
+type FieldInfo struct {
+	Name     string
+	TypeName string
+}
