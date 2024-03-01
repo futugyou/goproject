ecffd42fddd1b7f92fecb8d4c5a8282fc0343554
futugyou <17871902+futugyou@users.noreply.github.com>
2024-03-01 17:44:09 +0800

    use typename as feild name when field is anonymous

--- tour/util/struct.go
+++ tour/util/struct.go
@@ -21,21 +21,47 @@ func GetStructsFromFolder(filePath string) (structs []StructInfo, err error) {
 					fls := make([]FieldInfo, 0)
 					v := x.Type.(*ast.StructType)
 					for _, field := range v.Fields.List {
-						for _, name := range field.Names {
-							// get field.Type as string
-							var typeNameBuf bytes.Buffer
-							err := printer.Fprint(&typeNameBuf, fset, field.Type)
-							if err != nil {
-								log.Fatalf("failed printing %s", err)
-								continue
-							}
-							fls = append(fls, FieldInfo{
-								Name:     name.Name,
-								TypeName: typeNameBuf.String(),
-								Tag:      field.Tag.Value,
-							})
+						// get field.Type as string
+						var typeNameBuf bytes.Buffer
+						err := printer.Fprint(&typeNameBuf, fset, field.Type)
+						if err != nil {
+							log.Fatalf("failed printing %s", err)
+							continue
 						}
 
+						fieldName := ""
+						fieldTypeName := typeNameBuf.String()
+						fieldTag := ""
+						if field.Tag != nil {
+							fieldTag = field.Tag.Value
+						}
+
+						// if nestedStruct, ok := field.Type.(*ast.StructType); ok {
+						// 	for _, nestedField := range nestedStruct.Fields.List {
+						// 		nestedFieldName := nestedField.Names[0].Name
+						// 		var typeNameBuf2 bytes.Buffer
+						// 		printer.Fprint(&typeNameBuf2, fset, nestedField.Type)
+						// 		fmt.Printf("  1%s: %s\n", nestedFieldName, typeNameBuf2.String())
+						// 	}
+						// }
+
+						// if arrayType, ok := field.Type.(*ast.ArrayType); ok {
+						// 	var typeNameBuf2 bytes.Buffer
+						// 	printer.Fprint(&typeNameBuf2, fset, arrayType.Elt)
+						// 	fmt.Printf("  2 : %s\n", typeNameBuf2.String())
+
+						// }
+
+						if len(field.Names) == 0 {
+							fieldName = fieldTypeName
+						} else {
+							fieldName = field.Names[0].Name
+						}
+						fls = append(fls, FieldInfo{
+							Name:     fieldName,
+							TypeName: fieldTypeName,
+							Tag:      fieldTag,
+						})
 					}
 					structs = append(structs, StructInfo{
 						PackageName: pack.Name,
