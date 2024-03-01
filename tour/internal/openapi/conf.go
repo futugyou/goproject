00aa07a84e7578485f28375bc57c62feac644c80
futugyou <17871902+futugyou@users.noreply.github.com>
2024-03-01 21:49:26 +0800

    api detail config

--- tour/internal/openapi/conf.go
+++ tour/internal/openapi/conf.go
@@ -1,23 +1,34 @@
 package openapi
 
 type OpenAPIConfig struct {
-	Title       string
-	Description string
-	Version     string
-	ModelFolder string
-	OutputPath  string
-	OutputType  string
+	SpceVersion  string
+	Title        string
+	Description  string
+	APIVersion   string
+	APIRoutePath string
+	ModelFolder  string
+	OutputPath   string
+	OutputType   string
+	JsonConfig   []OperationConfig
+}
+
+type OperationConfig struct {
+	Method      string `json:"method"`
+	Path        string `json:"path"`
+	Request     string `json:"request"`
+	Response    string `json:"response"`
+	Description string `json:"description"`
 }
 
 func (m *OpenAPIConfig) Check() error {
 	if len(m.Title) == 0 {
-		m.Title = ""
+		m.Title = "this is openapi tile"
 	}
 	if len(m.Description) == 0 {
-		m.Description = ""
+		m.Description = "this is openapi description"
 	}
-	if len(m.Version) == 0 {
-		m.Version = ""
+	if len(m.APIVersion) == 0 {
+		m.APIVersion = "0.0.0"
 	}
 	if len(m.ModelFolder) == 0 {
 		m.ModelFolder = "./"
@@ -27,6 +38,9 @@ func (m *OpenAPIConfig) Check() error {
 	}
 	if len(m.OutputType) == 0 {
 		m.OutputType = "json"
+	}
+	if len(m.SpceVersion) == 0 {
+		m.SpceVersion = "3.0.3"
 	}
 	return nil
 }
