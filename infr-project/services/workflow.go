d1929ae6df9a8139bddad1787e737ab28c55e41a
futugyou <17871902+futugyou@users.noreply.github.com>
2024-05-04 23:20:16 +0800

    get workflow by repo

--- infr-project/services/workflow.go
+++ infr-project/services/workflow.go
@@ -1,6 +1,11 @@
 package services
 
-import "github.com/google/go-github/v61/github"
+import (
+	"context"
+	"log"
+
+	"github.com/google/go-github/v61/github"
+)
 
 type WorkflowService struct {
 	client *github.Client
@@ -11,4 +16,21 @@ func NewWorkflowService(token string) *WorkflowService {
 	return &WorkflowService{
 		client: client,
 	}
+}
+
+func (s *WorkflowService) Workflow(owner string, repo string) {
+	opts := &github.ListOptions{
+		Page:    1,
+		PerPage: 100,
+	}
+
+	wfs, _, err := s.client.Actions.ListWorkflows(context.Background(), owner, repo, opts)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	for _, wf := range wfs.Workflows {
+		log.Println(wf.GetName())
+	}
+	log.Println(wfs.GetTotalCount())
 }
