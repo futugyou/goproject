4d55f1a337efb66b2fda58eb1e15ee5084e899db
futugyou <17871902+futugyou@users.noreply.github.com>
2024-05-04 23:20:26 +0800

    update gin route

---
+++ infr-project/gin-routes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+
+	"github.com/futugyou/infr-project/services"
+	"github.com/gin-gonic/gin"
+)
+
+func NewGinRoute() *gin.Engine {
+	router := gin.Default()
+
+	v1 := router.Group("/api/v1")
+	{
+		v1.POST("/ping", pingEndpoint)
+		v1.GET("/workflow", workflowEndpoint)
+	}
+	return router
+}
+
+func pingEndpoint(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func workflowEndpoint(c *gin.Context) {
+	owner := c.Query("owner")
+	repo := c.Query("repo")
+
+	f := services.NewWorkflowService(os.Getenv("GITHUB_TOKEN"))
+	f.Workflow(owner, repo)
+}
--- infr-project/main.go
+++ infr-project/main.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	"os"
-
-	"github.com/futugyou/infr-project/services"
 	_ "github.com/joho/godotenv/autoload"
-
-	"github.com/gin-gonic/gin"
 )
 
 //go:generate go install github.com/joho/godotenv/cmd/godotenv@latest
 //go:generate godotenv -f ./.env go run ../tour/main.go mongo generate
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		_ = services.NewWorkflowService(os.Getenv("GITHUB_TOKEN"))
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r := NewGinRoute()
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
