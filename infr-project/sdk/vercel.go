9276d9589870a8a1011094bcc3ffac2d40844a0d
futugyou <17871902+futugyou@users.noreply.github.com>
2024-05-05 23:36:01 +0800

    vercel project env

--- infr-project/sdk/vercel.go
+++ infr-project/sdk/vercel.go
@@ -44,3 +44,26 @@ func (v *VercelClient) GetProjects() string {
 	}
 	return string(all)
 }
+
+func (v *VercelClient) GetProjectEnv(project string) string {
+	path := v.baseurl + "/v9/projects/" + project + "/env"
+	var body io.Reader
+	req, _ := http.NewRequest("GET", path, body)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", "Bearer", v.token))
+	resp, err := v.http.Do(req)
+
+	if err != nil {
+		log.Println(err.Error())
+		return "[]"
+	}
+
+	defer resp.Body.Close()
+	all, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Println(err.Error())
+		return "[]"
+	}
+	return string(all)
+}
