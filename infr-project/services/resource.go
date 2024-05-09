728267ca1d549c5be828034adde99c757e70dce2
futugyou <17871902+futugyou@users.noreply.github.com>
2024-05-09 19:25:59 +0800

    update resource

--- infr-project/services/resource.go
+++ infr-project/services/resource.go
@@ -1,6 +1,10 @@
 package services
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Resource struct {
 	Id       string       `json:"id"`
@@ -27,3 +31,36 @@ const DrawIO resourceType = "DrawIO"
 const Markdown resourceType = "Markdown"
 const Excalidraw resourceType = "Excalidraw"
 const Plate resourceType = "Plate"
+
+func NewResource(name string, resourceType ResourceType, data string) *Resource {
+	return &Resource{
+		Id:       uuid.New().String(),
+		Name:     name,
+		Type:     resourceType,
+		Version:  0,
+		Data:     data,
+		CreateAt: time.Now().UTC(),
+	}
+}
+
+func (r *Resource) ChangeName(name string) *Resource {
+	r.Version = r.Version + 1
+	r.CreateAt = time.Now().UTC()
+	r.Name = name
+	return r
+}
+
+func (r *Resource) ChangeType(resourceType ResourceType, data string) *Resource {
+	r.Version = r.Version + 1
+	r.CreateAt = time.Now().UTC()
+	r.Type = resourceType
+	r.Data = data
+	return r
+}
+
+func (r *Resource) ChangeData(data string) *Resource {
+	r.Version = r.Version + 1
+	r.CreateAt = time.Now().UTC()
+	r.Data = data
+	return r
+}
