933bab921d16a00e837fe1f1b4f2d7c1a0fb8cbb
futugyou <17871902+futugyou@users.noreply.github.com>
2024-05-10 23:13:46 +0800

    update resource event sourcing

--- infr-project/event_sourcing/sourcing.go
+++ infr-project/event_sourcing/sourcing.go
@@ -12,7 +12,7 @@ type IEventSourcer[E IEvent, R IAggregate] interface {
 	Add(event E) error
 	Save(events []E) error
 	Load(id string) ([]E, error)
-	Apply(event E) R
+	Apply(aggregate R, event E) R
 	GetAlltVersions() []R
 	GetSpecificVersion(version int) R
 }
--- infr-project/services/resource_event.go
+++ infr-project/services/resource_event.go
@@ -64,15 +64,59 @@ func (res *ResourceEventSourcer) Load(id string) ([]IResourceEvent, error) {
 	return nil, nil
 }
 
-func (res *ResourceEventSourcer) Apply(event IResourceEvent) Resource {
-	resource := Resource{Id: res.ResourceId}
+func (res *ResourceEventSourcer) Apply(aggregate Resource, event IResourceEvent) Resource {
+	resource := aggregate
+
+	switch e := event.(type) {
+	case ResourceCreatedEvent:
+		resource = Resource{Id: e.Id, Name: e.Name, Type: e.Type, Data: e.Data, Version: 1, CreatedAt: e.CreatedAt}
+	case ResourceUpdatedEvent:
+		resource.Name = e.Name
+		resource.Type = e.Type
+		resource.Data = e.Data
+		resource.Version = e.Version
+		resource.CreatedAt = e.UpdatedAt
+	case ResourceDeletedEvent:
+		// TODO: how to handle delete
+	}
+
 	return resource
 }
 
 func (res *ResourceEventSourcer) GetResourceVersions() []Resource {
+	if len(res.allVersions) > 0 {
+		return res.allVersions
+	}
+
+	if len(res.events) == 0 {
+		if _, err := res.Load(res.ResourceId); err != nil {
+			return res.allVersions
+		}
+	}
+
+	resource := Resource{}
+
+	for i := 0; i < len(res.events); i++ {
+		prevVersion := resource.Version
+		resource = res.Apply(resource, res.events[i])
+
+		if resource.Version != prevVersion {
+			res.allVersions = append(res.allVersions, resource)
+		}
+	}
+
 	return res.allVersions
 }
 
 func (res *ResourceEventSourcer) GetResourceVersion(version int) Resource {
+	if len(res.allVersions) == 0 {
+		res.GetResourceVersions()
+	}
+
+	for i := 0; i < len(res.allVersions); i++ {
+		if res.allVersions[i].Version == version {
+			return res.allVersions[i]
+		}
+	}
 	return Resource{}
 }
