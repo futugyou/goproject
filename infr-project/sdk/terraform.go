cf45fe8f6ff6c5ba8b82c9daffdb86b6930d7af6
futugyou <17871902+futugyou@users.noreply.github.com>
2024-05-23 21:14:57 +0800

    create configuration versions

--- infr-project/sdk/terraform.go
+++ infr-project/sdk/terraform.go
@@ -31,24 +31,38 @@ func NewTerraformClient(token string) (*TerraformClient, error) {
 	}, nil
 }
 
-func (s *TerraformClient) CheckWorkspace(name string) (string, error) {
+func (s *TerraformClient) CheckWorkspace(name string) (*tfe.Workspace, error) {
 	ctx := context.Background()
 
 	w, err := s.client.Workspaces.Read(ctx, organization, name)
 	if err != nil && err.Error() != "resource not found" {
-		return "", err
+		return nil, err
 	}
 
 	if w != nil {
-		return w.Name, nil
+		return w, nil
 	}
 
 	w, err = s.client.Workspaces.Create(ctx, organization, tfe.WorkspaceCreateOptions{
 		Name: tfe.String(name),
 	})
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return w.Name, nil
+	return w, nil
+}
+
+func (s *TerraformClient) CreateConfigurationVersions(workspaceID string, path string) (*tfe.ConfigurationVersion, error) {
+	ctx := context.Background()
+
+	cv, err := s.client.ConfigurationVersions.Create(ctx, workspaceID, tfe.ConfigurationVersionCreateOptions{
+		AutoQueueRuns: tfe.Bool(false),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cv, s.client.ConfigurationVersions.Upload(ctx, cv.UploadURL, path)
 }
