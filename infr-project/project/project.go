ee47b46dc10bc18fd4da2436fea8ed1667b6744b
futugyou <17871902+futugyou@users.noreply.github.com>
2024-06-12 14:10:26 +0800

    update project aggregate

--- infr-project/project/project.go
+++ infr-project/project/project.go
@@ -7,33 +7,47 @@ import (
 
 	"github.com/futugyou/infr-project/core"
 	"github.com/futugyou/infr-project/domain"
-	"github.com/futugyou/infr-project/platform"
-	"github.com/futugyou/infr-project/resource"
 )
 
 type Project struct {
 	domain.Aggregate `json:"-"`
-	Name             string              `json:"name"`
-	Description      string              `json:"description"`
-	StartDate        time.Time           `json:"start_date"`
-	EndDate          *time.Time          `json:"end_date"`
-	Platforms        []platform.Platform `json:"platforms"`
-	Resources        []resource.Resource `json:"resources"`
+	Name             string            `json:"name"`
+	Description      string            `json:"description"`
+	StartDate        *time.Time        `json:"start_date"`
+	EndDate          *time.Time        `json:"end_date"`
+	Platforms        []ProjectPlatform `json:"platforms"`
+	Designs          []ProjectDesign   `json:"designs"`
 }
 
 func (r *Project) AggregateName() string {
 	return "projects"
 }
 
-func NewProject(name string, description string) *Project {
+type ProjectPlatform struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	// ref platform.Platform
+	PlatformId string `json:"platform_id"`
+}
+
+type ProjectDesign struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	// ref resource.Resource
+	Resources []string `json:"resources"`
+}
+
+func NewProject(name string, description string, start *time.Time, end *time.Time) *Project {
 	return &Project{
 		Aggregate: domain.Aggregate{
 			Id: uuid.New().String(),
 		},
 		Name:        name,
 		Description: description,
-		StartDate:   time.Now().UTC(),
-		Platforms:   []platform.Platform{},
+		StartDate:   start,
+		EndDate:     end,
+		Platforms:   []ProjectPlatform{},
+		Designs:     []ProjectDesign{},
 	}
 }
 
@@ -42,10 +56,25 @@ func (s *Project) ChangeName(name string) *Project {
 	return s
 }
 
-func (w *Project) UpdatePlatform(platform platform.Platform) *Project {
+func (s *Project) ChangeDescription(description string) *Project {
+	s.Description = description
+	return s
+}
+
+func (s *Project) ChangeStartDate(start time.Time) *Project {
+	s.StartDate = &start
+	return s
+}
+
+func (s *Project) ChangeEndDate(end *time.Time) *Project {
+	s.EndDate = end
+	return s
+}
+
+func (w *Project) UpdatePlatform(platform ProjectPlatform) *Project {
 	f := false
 	for i := 0; i < len(w.Platforms); i++ {
-		if w.Platforms[i].Id == platform.Id {
+		if w.Platforms[i].Name == platform.Name {
 			w.Platforms[i] = platform
 			f = true
 			break
@@ -58,10 +87,35 @@ func (w *Project) UpdatePlatform(platform platform.Platform) *Project {
 	return w
 }
 
-func (w *Project) RemovePlatform(id string) *Project {
+func (w *Project) RemovePlatform(name string) *Project {
 	for i := len(w.Platforms) - 1; i >= 0; i-- {
-		if w.Platforms[i].Id == id {
+		if w.Platforms[i].Name == name {
 			w.Platforms = append(w.Platforms[:i], w.Platforms[i+1:]...)
+		}
+	}
+	return w
+}
+
+func (w *Project) UpdateDesign(design ProjectDesign) *Project {
+	f := false
+	for i := 0; i < len(w.Platforms); i++ {
+		if w.Designs[i].Name == design.Name {
+			w.Designs[i] = design
+			f = true
+			break
+		}
+	}
+
+	if !f {
+		w.Designs = append(w.Designs, design)
+	}
+	return w
+}
+
+func (w *Project) RemoveDesign(name string) *Project {
+	for i := len(w.Designs) - 1; i >= 0; i-- {
+		if w.Designs[i].Name == name {
+			w.Designs = append(w.Designs[:i], w.Designs[i+1:]...)
 		}
 	}
 	return w
