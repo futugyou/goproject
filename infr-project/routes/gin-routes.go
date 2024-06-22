7112287dee3c45d603ced0f0c86db0597b07ea88
futugyou <17871902+futugyou@users.noreply.github.com>
2024-06-22 20:44:38 +0800

    split sdk test from base route

--- infr-project/routes/gin-routes.go
+++ infr-project/routes/gin-routes.go
@@ -1,17 +1,12 @@
 package routes
 
 import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/futugyou/infr-project/sdk"
-	"github.com/futugyou/infr-project/services"
-
-	docs "github.com/futugyou/infr-project/docs"
 	"github.com/gin-gonic/gin"
+
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+
+	docs "github.com/futugyou/infr-project/docs"
 )
 
 func NewGinRoute() *gin.Engine {
@@ -20,12 +15,8 @@ func NewGinRoute() *gin.Engine {
 	docs.SwaggerInfo.BasePath = "/api/v1"
 	v1 := router.Group("/api/v1")
 	{
-		v1.POST("/ping", pingEndpoint)
-		v1.GET("/workflow", workflowEndpoint)
-		v1.GET("/vercel", vercelProjectEndpoint)
-		v1.GET("/circleci", circleciPipeline)
-		v1.GET("/vault", vaultSecret)
-		v1.GET("/tf", terraformWS)
+		// sdk test routes
+		ConfigTestingRoutes(v1)
 		// resource routes
 		ConfigResourceRoutes(v1)
 		// platform routes
@@ -33,61 +24,9 @@ func NewGinRoute() *gin.Engine {
 		// project routes
 		ConfigProjectRoutes(v1)
 	}
+
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	return router
-}
-
-func pingEndpoint(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
-
-func workflowEndpoint(c *gin.Context) {
-	owner := c.Query("owner")
-	repo := c.Query("repo")
-
-	f := services.NewWorkflowService(os.Getenv("GITHUB_TOKEN"))
-	f.Workflows(owner, repo)
-}
-
-func vercelProjectEndpoint(c *gin.Context) {
-	f := sdk.NewVercelClient(os.Getenv("VERCEL_TOKEN"))
-	result := f.GetProjects()
-	c.JSON(200, result)
-}
-
-func circleciPipeline(c *gin.Context) {
-	f := sdk.NewCircleciClient(os.Getenv("CIRCLECI_TOKEN"))
-	result := f.Pipelines(os.Getenv("CIRCLECI_ORG_SLUG"))
-	c.JSON(200, result)
-}
-
-func vaultSecret(c *gin.Context) {
-	f := sdk.NewVaultClient()
-	result, err := f.GetAppSecret("VERCEL_TOKEN")
-	if err != nil {
-		log.Println(err.Error())
-		return
-	}
-	c.JSON(200, result)
-}
-
-func terraformWS(c *gin.Context) {
-	tfclient, _ := sdk.NewTerraformClient(os.Getenv("TFC_TOKEN"))
-	ws, _ := tfclient.CheckWorkspace("test")
-	_, err := tfclient.CreateConfigurationVersions(ws.ID, "./tmp")
-	if err != nil {
-		fmt.Println("cv", err.Error())
-	}
-	plan, err := tfclient.CreateRun(ws, true)
-	if err != nil {
-		fmt.Println("cr", err.Error())
-	}
-	err = tfclient.ApplyRun(plan.ID)
-	if err != nil {
-		fmt.Println("ar", err.Error())
-	}
 }
 
 func Cors() gin.HandlerFunc {
