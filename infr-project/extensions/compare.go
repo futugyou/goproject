96674329e6b85d3cf3bda4cb5ee1d5e939b6948c
futugyou <17871902+futugyou@users.noreply.github.com>
2024-06-24 17:54:12 +0800

    add map compare

--- infr-project/extensions/compare.go
+++ infr-project/extensions/compare.go
@@ -26,3 +26,18 @@ func StringArrayCompare(arr1, arr2 []string) bool {
 
 	return true
 }
+
+func MapsCompare(map1, map2 map[string]string) bool {
+	if len(map1) != len(map2) {
+		return false
+	}
+
+	for key, value1 := range map1 {
+		value2, ok := map2[key]
+		if !ok || value1 != value2 {
+			return false
+		}
+	}
+
+	return true
+}
