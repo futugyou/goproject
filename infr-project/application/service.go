83db05d727ba2f3fbe087e142cac7e2154665e06
futugyou <17871902+futugyou@users.noreply.github.com>
2024-06-25 17:53:35 +0800

    handle mutile doamin event

--- infr-project/application/service.go
+++ infr-project/application/service.go
@@ -127,6 +127,9 @@ func (es *ApplicationService[Event, EventSourcing]) RetrieveLatestVersion(id str
 	return es.domainService.RetrieveLatestVersion(*aggregate, events)
 }
 
+// this method can not handle mutile version change
+// eg. if version changed form 4 to 7, the 6 will not skipped,
+// beacuse needStoreSnapshot will only check lastest version(7)
 func (s *ApplicationService[Event, EventSourcing]) SaveSnapshotAndEvent(ctx context.Context, aggregate EventSourcing) error {
 	es := aggregate.DomainEvents()
 	events := make([]Event, 0)
@@ -141,6 +144,29 @@ func (s *ApplicationService[Event, EventSourcing]) SaveSnapshotAndEvent(ctx cont
 	if s.needStoreSnapshot(aggregate) {
 		if err := s.snapshotStore.SaveSnapshot(ctx, aggregate); err != nil {
 			return err
+		}
+	}
+
+	return s.eventStore.Save(ctx, events)
+}
+
+// this method can handle mutle version change
+func (s *ApplicationService[Event, EventSourcing]) SaveSnapshotAndEvent2(ctx context.Context, aggregates []EventSourcing) error {
+	es := aggregates[len(aggregates)-1].DomainEvents()
+	events := make([]Event, 0)
+	for i := 0; i < len(es); i++ {
+		ev, ok := es[i].(Event)
+		if !ok {
+			return fmt.Errorf("expected type Event but got %T", es[i])
+		}
+		events = append(events, ev)
+	}
+
+	for _, agg := range aggregates {
+		if s.needStoreSnapshot(agg) {
+			if err := s.snapshotStore.SaveSnapshot(ctx, agg); err != nil {
+				return err
+			}
 		}
 	}
 
