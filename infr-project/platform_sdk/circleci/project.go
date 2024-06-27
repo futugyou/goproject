563c0c52f0831cf09f089599767eb1695cc4ac5c
futugyou <17871902+futugyou@users.noreply.github.com>
2024-06-27 20:35:54 +0800

    get circleci project

--- infr-project/platform_sdk/circleci/project.go
+++ infr-project/platform_sdk/circleci/project.go
@@ -15,6 +15,19 @@ func (s *CircleciClient) CreateProject(org_slug string, name string) CreateProje
 	return result
 }
 
+func (s *CircleciClient) GetProject(org_slug string, name string) ProjectInfo {
+	path := "/project/" + org_slug + "/" + name
+
+	result := ProjectInfo{}
+	err := s.http.Get(path, &result)
+
+	if err != nil {
+		log.Println(err.Error())
+		return result
+	}
+	return result
+}
+
 type CreateProjectResponse struct {
 	Advanced Advanced `json:"advanced"`
 	Message  *string  `json:"message,omitempty"`
@@ -31,4 +44,21 @@ type Advanced struct {
 	SetupWorkflows             bool     `json:"setup_workflows"`
 	WriteSettingsRequiresAdmin bool     `json:"write_settings_requires_admin"`
 	PROnlyBranchOverrides      []string `json:"pr_only_branch_overrides"`
+}
+
+type ProjectInfo struct {
+	Slug             string  `json:"slug"`
+	Name             string  `json:"name"`
+	ID               string  `json:"id"`
+	OrganizationName string  `json:"organization_name"`
+	OrganizationSlug string  `json:"organization_slug"`
+	OrganizationID   string  `json:"organization_id"`
+	VcsInfo          VcsInfo `json:"vcs_info"`
+	Message          *string `json:"message,omitempty"`
+}
+
+type VcsInfo struct {
+	VcsURL        string `json:"vcs_url"`
+	Provider      string `json:"provider"`
+	DefaultBranch string `json:"default_branch"`
 }
