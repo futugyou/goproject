55fce9d430312f0e5d27b90115dcae44130e292a
futugyou <17871902+futugyou@users.noreply.github.com>
2024-06-27 19:59:11 +0800

    create webhook for circleci

--- infr-project/sdk/circleci.go
+++ infr-project/sdk/circleci.go
@@ -29,3 +29,40 @@ func (s *CircleciClient) Pipelines(org_slug string) string {
 	}
 	return result
 }
+
+func (s *CircleciClient) CreateWebhook(name string, url string, projectId string) string {
+	path := "/webhook"
+	request := CreateWebhookRequest{
+		Name:          name,
+		Events:        []string{"workflow-completed"},
+		Url:           url,
+		VerifyTLS:     false,
+		SigningSecret: "",
+		Scope: WebhookScope{
+			Id:   projectId,
+			Type: "project",
+		},
+	}
+	result := ""
+	err := s.http.Post(path, request, &result)
+
+	if err != nil {
+		log.Println(err.Error())
+		return result
+	}
+	return result
+}
+
+type CreateWebhookRequest struct {
+	Name          string       `json:"name"`
+	Events        []string     `json:"events"` // "workflow-completed" "job-completed"
+	Url           string       `json:"url"`
+	VerifyTLS     bool         `json:"verify-tls"`
+	SigningSecret string       `json:"signing-secret"`
+	Scope         WebhookScope `json:"scope"`
+}
+
+type WebhookScope struct {
+	Id   string `json:"id"`
+	Type string `json:"type"` //"project"
+}
