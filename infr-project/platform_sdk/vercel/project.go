31b5a4d776846a1488daeee8775ca26a4ed0d40a
futugyou <17871902+futugyou@users.noreply.github.com>
2024-06-28 22:11:14 +0800

    create vercel project

--- infr-project/platform_sdk/vercel/project.go
+++ infr-project/platform_sdk/vercel/project.go
@@ -25,3 +25,49 @@ func (v *VercelClient) GetProjectEnv(project string) string {
 	}
 	return result
 }
+
+func (v *VercelClient) CreateProject(name string, slug string, teamId string) string {
+	path := "/v10/projects?slug=" + slug + "&teamId=" + teamId
+	// TODO: struct
+	result := ""
+	info := ProjectInfo{
+		Name: name,
+	}
+	err := v.http.Post(path, info, &result)
+
+	if err != nil {
+		log.Println(err.Error())
+		return result
+	}
+	return result
+}
+
+type ProjectInfo struct {
+	Name                                 string                `json:"name"`
+	BuildCommand                         string                `json:"buildCommand,omitempty"`
+	CommandForIgnoringBuildStep          string                `json:"commandForIgnoringBuildStep,omitempty"`
+	DevCommand                           string                `json:"devCommand,omitempty"`
+	EnvironmentVariables                 []EnvironmentVariable `json:"environmentVariables,omitempty"`
+	Framework                            string                `json:"framework,omitempty"`
+	GitRepository                        GitRepository         `json:"gitRepository,omitempty"`
+	InstallCommand                       string                `json:"installCommand,omitempty"`
+	OutputDirectory                      string                `json:"outputDirectory,omitempty"`
+	PublicSource                         bool                  `json:"publicSource,omitempty"`
+	RootDirectory                        string                `json:"rootDirectory,omitempty"`
+	ServerlessFunctionRegion             string                `json:"serverlessFunctionRegion,omitempty"`
+	ServerlessFunctionZeroConfigFailover string                `json:"serverlessFunctionZeroConfigFailover,omitempty"`
+	SkipGitConnectDuringLink             bool                  `json:"skipGitConnectDuringLink,omitempty"`
+}
+
+type EnvironmentVariable struct {
+	Key       string `json:"key,omitempty"`
+	Target    string `json:"target,omitempty"`
+	GitBranch string `json:"gitBranch,omitempty"`
+	Type      string `json:"type,omitempty"`
+	Value     string `json:"value,omitempty"`
+}
+
+type GitRepository struct {
+	Repo string `json:"repo,omitempty"`
+	Type string `json:"type,omitempty"`
+}
