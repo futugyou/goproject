a81dc3ff6f474c9674798c1b8b7471eb7a125ea4
futugyou <17871902+futugyou@users.noreply.github.com>
2024-06-30 16:40:12 +0800

    update circleci insights api

--- infr-project/platform_sdk/circleci/insights.go
+++ infr-project/platform_sdk/circleci/insights.go
@@ -70,6 +70,144 @@ func (s *CircleciClient) GetFlakyTests(project_slug string) (*FlakyTestsResponse
 	return result, nil
 }
 
+func (s *CircleciClient) GetMetricsForProjectWorkflows(project_slug string) (*WorkflowMetricsResponse, error) {
+	path := "/insights/" + project_slug + "/workflows"
+	result := &WorkflowMetricsResponse{}
+	if err := s.http.Get(path, result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (s *CircleciClient) GetRecentRuns(project_slug string, workflow_name string) (*RecentRunsResponse, error) {
+	path := "/insights/" + project_slug + "/workflows/" + workflow_name
+	result := &RecentRunsResponse{}
+	if err := s.http.Get(path, result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (s *CircleciClient) GetMetricsForProjectWorkflowJobs(project_slug string, workflow_name string) (*MetricsProjectWorkflowJobsResponse, error) {
+	path := "/insights/" + project_slug + "/workflows/" + workflow_name + "/jobs"
+	result := &MetricsProjectWorkflowJobsResponse{}
+	if err := s.http.Get(path, result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (s *CircleciClient) GetMetricsTrendsForWorkflows(project_slug string, workflow_name string) (*MetricsTrendsForWorkflowsResponse, error) {
+	path := "/insights/" + project_slug + "/workflows/" + workflow_name + "/summary"
+
+	result := &MetricsTrendsForWorkflowsResponse{}
+	if err := s.http.Get(path, result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (s *CircleciClient) GetTestMetricsForProjectWorkflow(project_slug string, workflow_name string) (*TestMetrics, error) {
+	path := "/insights/" + project_slug + "/workflows/" + workflow_name + "/test-metrics"
+	result := &TestMetrics{}
+	if err := s.http.Get(path, result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+type TestMetrics struct {
+	AverageTestCount     int64     `json:"average_test_count"`
+	MostFailedTests      []Test    `json:"most_failed_tests"`
+	MostFailedTestsExtra int64     `json:"most_failed_tests_extra"`
+	SlowestTests         []Test    `json:"slowest_tests"`
+	SlowestTestsExtra    int64     `json:"slowest_tests_extra"`
+	TotalTestRuns        int64     `json:"total_test_runs"`
+	TestRuns             []TestRun `json:"test_runs"`
+	Message              *string   `json:"message,omitempty"`
+}
+
+type Test struct {
+	P95Duration int64  `json:"p95_duration"`
+	TotalRuns   int64  `json:"total_runs"`
+	Classname   string `json:"classname"`
+	FailedRuns  int64  `json:"failed_runs"`
+	Flaky       bool   `json:"flaky"`
+	Source      string `json:"source"`
+	File        string `json:"file"`
+	JobName     string `json:"job_name"`
+	TestName    string `json:"test_name"`
+}
+
+type TestRun struct {
+	PipelineNumber int64       `json:"pipeline_number"`
+	WorkflowID     interface{} `json:"workflow_id"`
+	SuccessRate    int64       `json:"success_rate"`
+	TestCounts     TestCounts  `json:"test_counts"`
+}
+
+type TestCounts struct {
+	Error   int64 `json:"error"`
+	Failure int64 `json:"failure"`
+	Skipped int64 `json:"skipped"`
+	Success int64 `json:"success"`
+	Total   int64 `json:"total"`
+}
+
+type MetricsTrendsForWorkflowsResponse struct {
+	Metrics       Metrics  `json:"metrics"`
+	Trends        Trends   `json:"trends"`
+	WorkflowNames []string `json:"workflow_names"`
+	Message       *string  `json:"message,omitempty"`
+}
+
+type Trends struct {
+	TotalRuns          int64 `json:"total_runs"`
+	FailedRuns         int64 `json:"failed_runs"`
+	SuccessRate        int64 `json:"success_rate"`
+	P95DurationSecs    int64 `json:"p95_duration_secs"`
+	MedianDurationSecs int64 `json:"median_duration_secs"`
+	TotalCreditsUsed   int64 `json:"total_credits_used"`
+	Mttr               int64 `json:"mttr"`
+	Throughput         int64 `json:"throughput"`
+}
+
+type MetricsProjectWorkflowJobsResponse struct {
+	Items         []WorkflowMetrics `json:"items"`
+	NextPageToken string            `json:"next_page_token"`
+}
+
+type RecentRunsResponse struct {
+	Items         []RunsInfo `json:"items"`
+	NextPageToken string     `json:"next_page_token"`
+	Message       *string    `json:"message,omitempty"`
+}
+
+type RunsInfo struct {
+	ID          string `json:"id"`
+	Branch      string `json:"branch"`
+	Duration    int64  `json:"duration"`
+	CreatedAt   string `json:"created_at"`
+	StoppedAt   string `json:"stopped_at"`
+	CreditsUsed int64  `json:"credits_used"`
+	Status      string `json:"status"`
+	IsApproval  bool   `json:"is_approval"`
+}
+
+type WorkflowMetricsResponse struct {
+	Items         []WorkflowMetrics `json:"items"`
+	NextPageToken string            `json:"next_page_token"`
+	Message       *string           `json:"message,omitempty"`
+}
+
+type WorkflowMetrics struct {
+	Name        string  `json:"name"`
+	Metrics     Metrics `json:"metrics"`
+	WindowStart string  `json:"window_start"`
+	WindowEnd   string  `json:"window_end"`
+	ProjectID   string  `json:"project_id"`
+}
+
 type FlakyTestsResponse struct {
 	FlakyTests      []FlakyTest `json:"flaky-tests"`
 	TotalFlakyTests int64       `json:"total-flaky-tests"`
@@ -77,18 +215,18 @@ type FlakyTestsResponse struct {
 }
 
 type FlakyTest struct {
-	TimeWasted        int64       `json:"time-wasted"`
-	WorkflowCreatedAt string      `json:"workflow-created-at"`
-	WorkflowID        interface{} `json:"workflow-id"`
-	Classname         string      `json:"classname"`
-	PipelineNumber    int64       `json:"pipeline-number"`
-	WorkflowName      string      `json:"workflow-name"`
-	TestName          string      `json:"test-name"`
-	JobName           string      `json:"job-name"`
-	JobNumber         int64       `json:"job-number"`
-	TimesFlaked       int64       `json:"times-flaked"`
-	Source            string      `json:"source"`
-	File              string      `json:"file"`
+	TimeWasted        int64  `json:"time-wasted"`
+	WorkflowCreatedAt string `json:"workflow-created-at"`
+	WorkflowID        string `json:"workflow-id"`
+	Classname         string `json:"classname"`
+	PipelineNumber    int64  `json:"pipeline-number"`
+	WorkflowName      string `json:"workflow-name"`
+	TestName          string `json:"test-name"`
+	JobName           string `json:"job-name"`
+	JobNumber         int64  `json:"job-number"`
+	TimesFlaked       int64  `json:"times-flaked"`
+	Source            string `json:"source"`
+	File              string `json:"file"`
 }
 
 type ProjectBranches struct {
@@ -138,14 +276,18 @@ type Metrics struct {
 	MedianCreditsUsed int64           `json:"median_credits_used"`
 	TotalCreditsUsed  int64           `json:"total_credits_used"`
 	DurationMetrics   DurationMetrics `json:"duration_metrics"`
+	Mttr              int64           `json:"mttr"`
+	SuccessRate       int64           `json:"success_rate"`
+	TotalRecoveries   int64           `json:"total_recoveries"`
 }
 
 type DurationMetrics struct {
-	Min    int64 `json:"min"`
-	Median int64 `json:"median"`
-	Max    int64 `json:"max"`
-	P95    int64 `json:"p95"`
-	Total  int64 `json:"total"`
+	Min               int64 `json:"min"`
+	Median            int64 `json:"median"`
+	Max               int64 `json:"max"`
+	P95               int64 `json:"p95"`
+	Total             int64 `json:"total"`
+	StandardDeviation int64 `json:"standard_deviation"`
 }
 
 type MetricsTrends struct {
