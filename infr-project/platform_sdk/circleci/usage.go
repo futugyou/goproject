dadd70d6acf764cf57f5d038d94541702a3baab1
futugyou <17871902+futugyou@users.noreply.github.com>
2024-07-03 18:08:45 +0800

    create a usage export

---
+++ infr-project/platform_sdk/circleci/usage.go
@@ -0,0 +1,28 @@
+package circleci
+
+func (s *CircleciClient) CreateUsageExport(org_id string, start string, end string, shared_org_ids []string) (*UsageExport, error) {
+	path := "/organizations/" + org_id + "/usage_export_job"
+	request := struct {
+		Start        string   `json:"start"`
+		End          string   `json:"end"`
+		SharedOrgIds []string `json:"shared_org_ids"`
+	}{
+		Start:        start,
+		End:          end,
+		SharedOrgIds: shared_org_ids,
+	}
+	result := &UsageExport{}
+	if err := s.http.Post(path, request, result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+type UsageExport struct {
+	UsageExportJobID string   `json:"usage_export_job_id"`
+	State            string   `json:"state"`
+	Start            string   `json:"start"`
+	End              string   `json:"end"`
+	DownloadUrls     []string `json:"download_urls"`
+	Message          *string  `json:"message,omitempty"`
+}
