89be5ed6817e50f4858ea01b731d01087adffd04
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-07-05 10:07:06 +0000

    query information about an artifact

--- infr-project/platform_sdk/vercel/artifacts.go
+++ infr-project/platform_sdk/vercel/artifacts.go
@@ -25,3 +25,36 @@ func (v *VercelClient) CheckArtifactExists(hash string, slug string, teamId stri
 	}
 	return &result, nil
 }
+
+func (v *VercelClient) QueryArtifact(slug string, teamId string, hashes []string) (*QueryArtifactResponse, error) {
+	path := "/v8/artifacts"
+	queryParams := url.Values{}
+	if len(slug) > 0 {
+		queryParams.Add("slug", slug)
+	}
+	if len(teamId) > 0 {
+		queryParams.Add("teamId", teamId)
+	}
+	if len(queryParams) > 0 {
+		path += "?" + queryParams.Encode()
+	}
+	result := &QueryArtifactResponse{}
+	request := struct {
+		Hashes []string `json:"hashes"`
+	}{
+		Hashes: hashes,
+	}
+	err := v.http.Post(path, request, result)
+
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+type QueryArtifactResponse struct {
+	Size           int          `json:"size"`
+	Tag            string       `json:"tag"`
+	TaskDurationMs int          `json:"taskDurationMs"`
+	Error          *VercelError `json:"error"`
+}
