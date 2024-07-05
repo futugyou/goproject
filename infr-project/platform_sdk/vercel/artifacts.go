6d55434e46016330645a972824b7e513e5026619
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-07-05 10:10:11 +0000

    download a cache artifact

--- infr-project/platform_sdk/vercel/artifacts.go
+++ infr-project/platform_sdk/vercel/artifacts.go
@@ -52,6 +52,27 @@ func (v *VercelClient) QueryArtifact(slug string, teamId string, hashes []string
 	return result, nil
 }
 
+func (v *VercelClient) DownloadArtifact(slug string, teamId string, hash string) (*string, error) {
+	path := fmt.Sprintf("/v8/artifacts/%s", hash)
+	queryParams := url.Values{}
+	if len(slug) > 0 {
+		queryParams.Add("slug", slug)
+	}
+	if len(teamId) > 0 {
+		queryParams.Add("teamId", teamId)
+	}
+	if len(queryParams) > 0 {
+		path += "?" + queryParams.Encode()
+	}
+	result := ""
+	err := v.http.Get(path, &result)
+
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
 type QueryArtifactResponse struct {
 	Size           int          `json:"size"`
 	Tag            string       `json:"tag"`
