026cbd3c9c7857fa4106334a6eb510ce653da630
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-07-06 11:17:36 +0000

    create cert

--- infr-project/platform_sdk/vercel/certs.go
+++ infr-project/platform_sdk/vercel/certs.go
@@ -2,12 +2,39 @@ package vercel
 
 import (
 	"fmt"
+	"net/url"
 )
 
 func (v *VercelClient) GetCertById(id string) (*CertInfo, error) {
 	path := fmt.Sprintf("/v7/certs/%s", id)
 	result := &CertInfo{}
 	err := v.http.Get(path, result)
+
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (v *VercelClient) CreateCert(slug string, teamId string, cns []string) (*CertInfo, error) {
+	path := "/v7/certs"
+	queryParams := url.Values{}
+	if len(slug) > 0 {
+		queryParams.Add("slug", slug)
+	}
+	if len(teamId) > 0 {
+		queryParams.Add("teamId", teamId)
+	}
+	if len(queryParams) > 0 {
+		path += "?" + queryParams.Encode()
+	}
+	request := struct {
+		Cns []string `json:"cns"`
+	}{
+		Cns: cns,
+	}
+	result := &CertInfo{}
+	err := v.http.Post(path, request, result)
 
 	if err != nil {
 		return nil, err
