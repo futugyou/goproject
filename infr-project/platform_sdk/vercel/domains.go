6fbaa6bdd50410c65fecb0441a5cce5456044255
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-07-07 12:49:48 +0000

    Purchase a domain

---
+++ infr-project/platform_sdk/vercel/domains.go
@@ -0,0 +1,51 @@
+package vercel
+
+import (
+	"net/url"
+)
+
+func (v *VercelClient) PurchaseDomain(slug string, teamId string, req PurchaseDomainRequest) (*PurchaseDomainResponse, error) {
+	path := "/v5/domains/buy"
+	queryParams := url.Values{}
+	if len(slug) > 0 {
+		queryParams.Add("slug", slug)
+	}
+	if len(teamId) > 0 {
+		queryParams.Add("teamId", teamId)
+	}
+	if len(queryParams) > 0 {
+		path += "?" + queryParams.Encode()
+	}
+	result := &PurchaseDomainResponse{}
+	err := v.http.Post(path, req, result)
+
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+type PurchaseDomainRequest struct {
+	Address1      string `json:"address1"`
+	City          string `json:"city"`
+	Country       string `json:"country"`
+	Email         string `json:"email"`
+	FirstName     string `json:"firstName"`
+	LastName      string `json:"lastName"`
+	Name          string `json:"name"`
+	Phone         string `json:"phone"`
+	PostalCode    string `json:"postalCode"`
+	State         string `json:"state"`
+	OrgName       string `json:"orgName"`
+	ExpectedPrice int    `json:"expectedPrice"`
+	Renew         bool   `json:"renew"`
+}
+
+type PurchaseDomainResponse struct {
+	Created  int          `json:"created"`
+	Ns       []string     `json:"ns"`
+	Pending  bool         `json:"pending"`
+	Uid      string       `json:"uid"`
+	Verified bool         `json:"verified"`
+	Error    *VercelError `json:"error,omitempty"`
+}
