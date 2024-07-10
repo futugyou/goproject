26bd11257b1ee4da461dc451b77aaab03a9bd979
futugyou <17871902+futugyou@users.noreply.github.com>
2024-07-11 02:39:25 +0800

    Create a new deployment

--- infr-project/platform_sdk/vercel/deployments.go
+++ infr-project/platform_sdk/vercel/deployments.go
@@ -26,77 +26,105 @@ func (v *VercelClient) CancelDeployment(id string, slug string, teamId string) (
 	return result, nil
 }
 
+func (v *VercelClient) CreateDeployment(forceNew string, skipAutoDetectionConfirmation string,
+	slug string, teamId string, req CreateDeploymentRequest) (*DeploymentInfo, error) {
+	path := "/v13/deployments"
+	queryParams := url.Values{}
+	if len(slug) > 0 {
+		queryParams.Add("slug", slug)
+	}
+	if len(teamId) > 0 {
+		queryParams.Add("teamId", teamId)
+	}
+	if len(forceNew) > 0 {
+		queryParams.Add("forceNew", forceNew)
+	}
+	if len(skipAutoDetectionConfirmation) > 0 {
+		queryParams.Add("skipAutoDetectionConfirmation", skipAutoDetectionConfirmation)
+	}
+	if len(queryParams) > 0 {
+		path += "?" + queryParams.Encode()
+	}
+	result := &DeploymentInfo{}
+	err := v.http.Post(path, req, result)
+
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
 type DeploymentInfo struct {
-	Id                            string        `json:"id,omitempty"`
-	Meta                          interface{}   `json:"meta,omitempty"`
-	Url                           string        `json:"url,omitempty"`
-	Alias                         []string      `json:"alias,omitempty"`
-	AliasAssigned                 bool          `json:"aliasAssigned,omitempty"`
-	AliasAssignedAt               string        `json:"aliasAssignedAt,omitempty"`
-	AliasError                    *VercelError  `json:"aliasError,omitempty"`
-	AliasFinal                    string        `json:"aliasFinal,omitempty"`
-	AliasWarning                  AliasWarning  `json:"aliasWarning,omitempty"`
-	AlwaysRefuseToBuild           bool          `json:"alwaysRefuseToBuild,omitempty"`
-	AutoAssignCustomDomains       bool          `json:"autoAssignCustomDomains,omitempty"`
-	AutomaticAliases              []string      `json:"automaticAliases,omitempty"`
-	BootedAt                      int           `json:"bootedAt,omitempty"`
-	Build                         Build         `json:"build,omitempty"`
-	BuildErrorAt                  int           `json:"buildErrorAt,omitempty"`
-	BuildSkipped                  bool          `json:"buildSkipped,omitempty"`
-	BuildingAt                    int           `json:"buildingAt,omitempty"`
-	Builds                        []string      `json:"builds,omitempty"`
-	CanceledAt                    int           `json:"canceledAt,omitempty"`
-	ChecksConclusion              string        `json:"checksConclusion,omitempty"`
-	ChecksState                   string        `json:"checksState,omitempty"`
-	ConnectBuildsEnabled          bool          `json:"connectBuildsEnabled,omitempty"`
-	ConnectConfigurationId        string        `json:"connectConfigurationId,omitempty"`
-	CreatedAt                     int           `json:"createdAt,omitempty"`
-	CreatedIn                     string        `json:"createdIn,omitempty"`
-	Creator                       CreatorInfo   `json:"creator,omitempty"`
-	Crons                         []Cron        `json:"crons,omitempty"`
-	CustomEnvironment             interface{}   `json:"customEnvironment,omitempty"`
-	DeletedAt                     int           `json:"deletedAt,omitempty"`
-	Env                           []string      `json:"env,omitempty"`
-	ErrorCode                     string        `json:"errorCode,omitempty"`
-	ErrorLink                     string        `json:"errorLink,omitempty"`
-	ErrorMessage                  string        `json:"errorMessage,omitempty"`
-	ErrorStep                     string        `json:"errorStep,omitempty"`
-	Flags                         interface{}   `json:"flags,omitempty"`
-	Functions                     string        `json:"functions,omitempty"`
-	GitRepo                       interface{}   `json:"gitRepo,omitempty"`
-	GitSource                     interface{}   `json:"gitSource,omitempty"`
-	InitReadyAt                   int           `json:"initReadyAt,omitempty"`
-	InspectorUrl                  string        `json:"inspectorUrl,omitempty"`
-	IsFirstBranchDeployment       bool          `json:"isFirstBranchDeployment,omitempty"`
-	IsInConcurrentBuildsQueue     bool          `json:"isInConcurrentBuildsQueue,omitempty"`
-	Lambdas                       []interface{} `json:"lambdas,omitempty"`
-	MonorepoManager               string        `json:"monorepoManager,omitempty"`
-	Name                          string        `json:"name,omitempty"`
-	OidcTokenClaims               interface{}   `json:"oidcTokenClaims,omitempty"`
-	OwnerId                       string        `json:"ownerId,omitempty"`
-	PassiveConnectConfigurationId string        `json:"passiveConnectConfigurationId,omitempty"`
-	PassiveRegions                []string      `json:"passiveRegions,omitempty"`
-	Plan                          string        `json:"plan,omitempty"`
-	PreviewCommentsEnabled        string        `json:"previewCommentsEnabled,omitempty"`
-	Project                       ProjectInfo   `json:"project,omitempty"`
-	ProjectId                     string        `json:"projectId,omitempty"`
-	ProjectSettings               interface{}   `json:"projectSettings,omitempty"`
-	Public                        bool          `json:"public,omitempty"`
-	Ready                         int           `json:"ready,omitempty"`
-	ReadyState                    string        `json:"readyState,omitempty"`
-	ReadyStateReason              string        `json:"readyStateReason,omitempty"`
-	ReadySubstate                 string        `json:"readySubstate,omitempty"`
-	Regions                       []string      `json:"regions,omitempty"`
-	Routes                        interface{}   `json:"routes,omitempty"`
-	Source                        string        `json:"source,omitempty"`
-	Target                        string        `json:"target,omitempty"`
-	Team                          Team          `json:"team,omitempty"`
-	TtyBuildLogs                  bool          `json:"ttyBuildLogs,omitempty"`
-	Type                          string        `json:"type,omitempty"`
-	UndeletedAt                   int           `json:"undeletedAt,omitempty"`
-	UserAliases                   []string      `json:"userAliases,omitempty"`
-	Version                       int           `json:"version,omitempty"`
-	Error                         *VercelError  `json:"error,omitempty"`
+	Id                            string          `json:"id,omitempty"`
+	Meta                          interface{}     `json:"meta,omitempty"`
+	Url                           string          `json:"url,omitempty"`
+	Alias                         []string        `json:"alias,omitempty"`
+	AliasAssigned                 bool            `json:"aliasAssigned,omitempty"`
+	AliasAssignedAt               string          `json:"aliasAssignedAt,omitempty"`
+	AliasError                    *VercelError    `json:"aliasError,omitempty"`
+	AliasFinal                    string          `json:"aliasFinal,omitempty"`
+	AliasWarning                  AliasWarning    `json:"aliasWarning,omitempty"`
+	AlwaysRefuseToBuild           bool            `json:"alwaysRefuseToBuild,omitempty"`
+	AutoAssignCustomDomains       bool            `json:"autoAssignCustomDomains,omitempty"`
+	AutomaticAliases              []string        `json:"automaticAliases,omitempty"`
+	BootedAt                      int             `json:"bootedAt,omitempty"`
+	Build                         Build           `json:"build,omitempty"`
+	BuildErrorAt                  int             `json:"buildErrorAt,omitempty"`
+	BuildSkipped                  bool            `json:"buildSkipped,omitempty"`
+	BuildingAt                    int             `json:"buildingAt,omitempty"`
+	Builds                        []string        `json:"builds,omitempty"`
+	CanceledAt                    int             `json:"canceledAt,omitempty"`
+	ChecksConclusion              string          `json:"checksConclusion,omitempty"`
+	ChecksState                   string          `json:"checksState,omitempty"`
+	ConnectBuildsEnabled          bool            `json:"connectBuildsEnabled,omitempty"`
+	ConnectConfigurationId        string          `json:"connectConfigurationId,omitempty"`
+	CreatedAt                     int             `json:"createdAt,omitempty"`
+	CreatedIn                     string          `json:"createdIn,omitempty"`
+	Creator                       CreatorInfo     `json:"creator,omitempty"`
+	Crons                         []Cron          `json:"crons,omitempty"`
+	CustomEnvironment             interface{}     `json:"customEnvironment,omitempty"`
+	DeletedAt                     int             `json:"deletedAt,omitempty"`
+	Env                           []string        `json:"env,omitempty"`
+	ErrorCode                     string          `json:"errorCode,omitempty"`
+	ErrorLink                     string          `json:"errorLink,omitempty"`
+	ErrorMessage                  string          `json:"errorMessage,omitempty"`
+	ErrorStep                     string          `json:"errorStep,omitempty"`
+	Flags                         interface{}     `json:"flags,omitempty"`
+	Functions                     string          `json:"functions,omitempty"`
+	GitRepo                       interface{}     `json:"gitRepo,omitempty"`
+	GitSource                     GitSource       `json:"gitSource,omitempty"`
+	InitReadyAt                   int             `json:"initReadyAt,omitempty"`
+	InspectorUrl                  string          `json:"inspectorUrl,omitempty"`
+	IsFirstBranchDeployment       bool            `json:"isFirstBranchDeployment,omitempty"`
+	IsInConcurrentBuildsQueue     bool            `json:"isInConcurrentBuildsQueue,omitempty"`
+	Lambdas                       []interface{}   `json:"lambdas,omitempty"`
+	MonorepoManager               string          `json:"monorepoManager,omitempty"`
+	Name                          string          `json:"name,omitempty"`
+	OidcTokenClaims               interface{}     `json:"oidcTokenClaims,omitempty"`
+	OwnerId                       string          `json:"ownerId,omitempty"`
+	PassiveConnectConfigurationId string          `json:"passiveConnectConfigurationId,omitempty"`
+	PassiveRegions                []string        `json:"passiveRegions,omitempty"`
+	Plan                          string          `json:"plan,omitempty"`
+	PreviewCommentsEnabled        string          `json:"previewCommentsEnabled,omitempty"`
+	Project                       ProjectInfo     `json:"project,omitempty"`
+	ProjectId                     string          `json:"projectId,omitempty"`
+	ProjectSettings               ProjectSettings `json:"projectSettings,omitempty"`
+	Public                        bool            `json:"public,omitempty"`
+	Ready                         int             `json:"ready,omitempty"`
+	ReadyState                    string          `json:"readyState,omitempty"`
+	ReadyStateReason              string          `json:"readyStateReason,omitempty"`
+	ReadySubstate                 string          `json:"readySubstate,omitempty"`
+	Regions                       []string        `json:"regions,omitempty"`
+	Routes                        interface{}     `json:"routes,omitempty"`
+	Source                        string          `json:"source,omitempty"`
+	Target                        string          `json:"target,omitempty"`
+	Team                          Team            `json:"team,omitempty"`
+	TtyBuildLogs                  bool            `json:"ttyBuildLogs,omitempty"`
+	Type                          string          `json:"type,omitempty"`
+	UndeletedAt                   int             `json:"undeletedAt,omitempty"`
+	UserAliases                   []string        `json:"userAliases,omitempty"`
+	Version                       int             `json:"version,omitempty"`
+	Error                         *VercelError    `json:"error,omitempty"`
 }
 
 type Build struct {
@@ -113,4 +141,59 @@ type Team struct {
 	Id     string `json:"id,omitempty"`
 	Name   string `json:"name,omitempty"`
 	Slug   string `json:"slug,omitempty"`
+}
+
+type CreateDeploymentRequest struct {
+	Name                      string          `json:"name,omitempty"`
+	CustomEnvironmentSlugOrID string          `json:"customEnvironmentSlugOrId,omitempty"`
+	DeploymentID              string          `json:"deploymentId,omitempty"`
+	Files                     []File          `json:"files,omitempty"`
+	GitMetadata               GitMetadata     `json:"gitMetadata,omitempty"`
+	GitSource                 GitSource       `json:"gitSource,omitempty"`
+	Meta                      string          `json:"meta,omitempty"`
+	MonorepoManager           string          `json:"monorepoManager,omitempty"`
+	Project                   string          `json:"project,omitempty"`
+	ProjectSettings           ProjectSettings `json:"projectSettings,omitempty"`
+	Target                    string          `json:"target,omitempty"`
+	WithLatestCommit          bool            `json:"withLatestCommit,omitempty"`
+}
+
+type File struct {
+	InlinedFile InlinedFile `json:"InlinedFile,omitempty"`
+}
+
+type InlinedFile struct {
+	Data     string `json:"data,omitempty"`
+	Encoding string `json:"encoding,omitempty"`
+	File     string `json:"file,omitempty"`
+}
+
+type GitMetadata struct {
+	RemoteURL        string `json:"remoteUrl,omitempty"`
+	CommitAuthorName string `json:"commitAuthorName,omitempty"`
+	CommitMessage    string `json:"commitMessage,omitempty"`
+	CommitRef        string `json:"commitRef,omitempty"`
+	CommitSHA        string `json:"commitSha,omitempty"`
+	Dirty            bool   `json:"dirty,omitempty"`
+}
+
+type GitSource struct {
+	Ref    string `json:"ref,omitempty"`
+	RepoID string `json:"repoId,omitempty"`
+	SHA    string `json:"sha,omitempty"`
+	Type   string `json:"type,omitempty"`
+}
+
+type ProjectSettings struct {
+	BuildCommand                    string `json:"buildCommand,omitempty"`
+	CommandForIgnoringBuildStep     string `json:"commandForIgnoringBuildStep,omitempty"`
+	DevCommand                      string `json:"devCommand,omitempty"`
+	Framework                       string `json:"framework,omitempty"`
+	InstallCommand                  string `json:"installCommand,omitempty"`
+	NodeVersion                     string `json:"nodeVersion,omitempty"`
+	OutputDirectory                 string `json:"outputDirectory,omitempty"`
+	RootDirectory                   string `json:"rootDirectory,omitempty"`
+	ServerlessFunctionRegion        string `json:"serverlessFunctionRegion,omitempty"`
+	SkipGitConnectDuringLink        bool   `json:"skipGitConnectDuringLink,omitempty"`
+	SourceFilesOutsideRootDirectory bool   `json:"sourceFilesOutsideRootDirectory,omitempty"`
 }
