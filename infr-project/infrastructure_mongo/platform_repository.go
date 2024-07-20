285f93eaf4d0a95a9fb83d17bd8a1241d3e0fe8a
futugyou <17871902+futugyou@users.noreply.github.com>
2024-07-21 01:02:00 +0800

    add async mothed for platform repository interface

--- infr-project/infrastructure_mongo/platform_repository.go
+++ infr-project/infrastructure_mongo/platform_repository.go
@@ -36,3 +36,35 @@ func (s *PlatformRepository) GetAllPlatform(ctx context.Context) ([]platform.Pla
 	condition := extensions.NewSearch(nil, nil, nil, nil)
 	return s.BaseRepository.GetWithCondition(ctx, condition)
 }
+
+func (s *PlatformRepository) GetPlatformByNameAsync(ctx context.Context, name string) (<-chan *platform.Platform, <-chan error) {
+	condition := extensions.NewSearch(nil, nil, nil, map[string]interface{}{"name": name})
+	resultChan := make(chan *platform.Platform, 1)
+	errorChan := make(chan error, 1)
+
+	go func() {
+		defer close(resultChan)
+		defer close(errorChan)
+
+		result, err := s.BaseRepository.GetWithConditionAsync(ctx, condition)
+		select {
+		case datas := <-result:
+			if len(datas) == 0 {
+				errorChan <- fmt.Errorf("%s with name %s", extensions.Data_Not_Found_Message, name)
+			} else {
+				resultChan <- (&datas[0])
+			}
+		case errM := <-err:
+			errorChan <- errM
+		case <-ctx.Done():
+			errorChan <- fmt.Errorf("GetPlatformByNameAsync timeout, name %s", name)
+		}
+	}()
+
+	return resultChan, errorChan
+}
+
+func (s *PlatformRepository) GetAllPlatformAsync(ctx context.Context) (<-chan []platform.Platform, <-chan error) {
+	condition := extensions.NewSearch(nil, nil, nil, nil)
+	return s.BaseRepository.GetWithConditionAsync(ctx, condition)
+}
--- infr-project/platform/repository.go
+++ infr-project/platform/repository.go
@@ -11,3 +11,9 @@ type IPlatformRepository interface {
 	GetPlatformByName(ctx context.Context, name string) (*Platform, error)
 	GetAllPlatform(ctx context.Context) ([]Platform, error)
 }
+
+type IPlatformRepositoryAsync interface {
+	domain.IRepositoryAsync[Platform]
+	GetPlatformByNameAsync(ctx context.Context, name string) (<-chan *Platform, <-chan error)
+	GetAllPlatformAsync(ctx context.Context) (<-chan []Platform, <-chan error)
+}
