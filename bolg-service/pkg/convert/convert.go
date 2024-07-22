e3e2d49bbe12f97f6a9d8e9e128a8a39f828b359
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-07-22 05:48:11 +0000

    fix code scanning alerts

--- bolg-service/pkg/convert/convert.go
+++ bolg-service/pkg/convert/convert.go
@@ -1,6 +1,10 @@
 package convert
 
-import "strconv"
+import (
+	"errors"
+	"math"
+	"strconv"
+)
 
 type StrTo string
 
@@ -9,8 +13,25 @@ func (s StrTo) String() string {
 }
 
 func (s StrTo) Int() (int, error) {
-	v, err := strconv.Atoi(s.String())
-	return v, err
+	i64, err := strconv.ParseInt(s.String(), 10, 0) // 0 specifies that it will infer the size from int
+	if err != nil {
+		return 0, err
+	}
+
+	// Determine the range for int based on the system architecture
+	var intMin, intMax int64
+	if strconv.IntSize == 32 {
+		intMin = math.MinInt32
+		intMax = math.MaxInt32
+	} else {
+		intMin = math.MinInt64
+		intMax = math.MaxInt64
+	}
+
+	if i64 < intMin || i64 > intMax {
+		return 0, errors.New("value out of int range")
+	}
+	return int(i64), nil
 }
 
 func (s StrTo) MustInt() int {
@@ -19,9 +40,16 @@ func (s StrTo) MustInt() int {
 }
 
 func (s StrTo) UInt32() (uint32, error) {
-	v, err := strconv.Atoi(s.String())
-	return uint32(v), err
+	u64, err := strconv.ParseUint(s.String(), 10, 32)
+	if err != nil {
+		return 0, err
+	}
+	if u64 > uint64(math.MaxUint32) {
+		return 0, errors.New("value out of uint32 range")
+	}
+	return uint32(u64), nil
 }
+
 func (s StrTo) MustUInt32() uint32 {
 	v, _ := s.UInt32()
 	return v
