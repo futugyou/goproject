21d2bcb7e65f0b2728bc93e55ee750828c6d6d2e
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-08-31 13:19:49 +0000

    add MaskString extension method

--- extensions/string.go
+++ extensions/string.go
@@ -13,3 +13,16 @@ func IndexWithOffset(s, substr string, offset int) int {
 
 	return -1
 }
+
+func MaskString(s string, minLength int, maskRatio float64) string {
+	length := len(s)
+	if length < minLength {
+		return strings.Repeat("*", length)
+	}
+
+	maskLength := int(float64(length) * maskRatio)
+	startIndex := (length - maskLength) / 2
+
+	maskedPart := strings.Repeat("*", maskLength)
+	return s[:startIndex] + maskedPart + s[startIndex+maskLength:]
+}
