a99f2a295eb13df1eb06d75e6565a8e67d1bdd38
futugyou <17871902+futugyou@users.noreply.github.com>
2024-09-20 14:38:33 +0800

    vault repository interface

---
+++ infr-project/vault/repository.go
@@ -0,0 +1,16 @@
+package vault
+
+import (
+	"context"
+
+	"github.com/futugyou/infr-project/domain"
+)
+
+type IVaultRepositoryAsync interface {
+	domain.IRepositoryAsync[Vault]
+	InsertMultipleVault(ctx context.Context, vaults []Vault) <-chan error
+	GetAllVaultAsync(ctx context.Context, page *int, size *int) (<-chan []Vault, <-chan error)
+	GetAllVaultByStorageMediaAsync(ctx context.Context, media StorageMedia) (<-chan []Vault, <-chan error)
+	GetAllVaultByVaultTypeAsync(ctx context.Context, vType VaultType, identities ...string) (<-chan []Vault, <-chan error)
+	GetAllVaultByTagsAsync(ctx context.Context, tags []string) (<-chan []Vault, <-chan error)
+}
