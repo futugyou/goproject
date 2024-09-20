41663d172f4452d70f2f02108c35a442e1770f70
futugyou <17871902+futugyou@users.noreply.github.com>
2024-09-20 14:53:40 +0800

    add vault application

---
+++ infr-project/application/vault.go
@@ -0,0 +1,49 @@
+package application
+
+import (
+	"context"
+	"fmt"
+
+	domain "github.com/futugyou/infr-project/domain"
+	vault "github.com/futugyou/infr-project/vault"
+	models "github.com/futugyou/infr-project/view_models"
+)
+
+type VaultService struct {
+	innerService *AppService
+	repository   vault.IVaultRepositoryAsync
+}
+
+func NewVaultService(
+	unitOfWork domain.IUnitOfWork,
+	repository vault.IVaultRepositoryAsync,
+) *VaultService {
+	return &VaultService{
+		innerService: NewAppService(unitOfWork),
+		repository:   repository,
+	}
+}
+
+func (s *VaultService) GetAllVault(ctx context.Context, page *int, size *int) ([]models.VaultView, error) {
+	src, err := s.repository.GetAllVaultAsync(ctx, page, size)
+	select {
+	case datas := <-src:
+		result := make([]models.VaultView, len(datas))
+		for i := 0; i < len(datas); i++ {
+			result[i] = models.VaultView{
+				Id:           datas[i].Id,
+				Key:          datas[i].Key,
+				Value:        datas[i].Value,
+				StorageMedia: datas[i].StorageMedia.String(),
+				VaultType:    datas[i].VaultType.String(),
+				TypeIdentity: datas[i].TypeIdentity,
+				Tags:         datas[i].Tags,
+			}
+		}
+		return result, nil
+	case errM := <-err:
+		return nil, errM
+	case <-ctx.Done():
+		return nil, fmt.Errorf("GetAllVault timeout")
+	}
+}
---
+++ infr-project/view_models/vault.go
@@ -0,0 +1,11 @@
+package viewmodels
+
+type VaultView struct {
+	Id           string   `json:"id"`
+	Key          string   `json:"key"`
+	Value        string   `json:"value"`
+	StorageMedia string   `json:"storage_media"`
+	VaultType    string   `json:"vault_type"`
+	TypeIdentity string   `json:"type_identity"`
+	Tags         []string `json:"tags"`
+}
