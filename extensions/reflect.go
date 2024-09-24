7646f59dcb06bfc3646ce71efa83ba344c088b51
futugyou <17871902+futugyou@users.noreply.github.com>
2024-09-24 18:36:17 +0800

    determines whether all fields are nil

--- extensions/reflect.go
+++ extensions/reflect.go
@@ -42,3 +42,46 @@ func GetStringFieldStruct(obj interface{}, fields ...string) string {
 	}
 	return *v
 }
+
+// Determines whether all fields are nil. This is only valid for simple objects.
+func IsAllFieldsNil(v interface{}) bool {
+	val := reflect.ValueOf(v)
+
+	if val.Kind() == reflect.Ptr {
+		if val.IsNil() {
+			return true
+		}
+		val = val.Elem()
+	}
+
+	if val.Kind() != reflect.Struct {
+		return false
+	}
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fieldType := field.Type()
+
+		switch field.Kind() {
+		case reflect.Ptr, reflect.Slice, reflect.Map, reflect.Interface, reflect.Chan:
+			if !field.IsNil() {
+				return false
+			}
+		case reflect.Struct:
+			if !IsAllFieldsNil(field.Interface()) {
+				return false
+			}
+		case reflect.Array:
+			for j := 0; j < field.Len(); j++ {
+				if !reflect.DeepEqual(field.Index(j).Interface(), reflect.Zero(fieldType.Elem()).Interface()) {
+					return false
+				}
+			}
+		default:
+			if !reflect.DeepEqual(field.Interface(), reflect.Zero(fieldType).Interface()) {
+				return false
+			}
+		}
+	}
+	return true
+}
