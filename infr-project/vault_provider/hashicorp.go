18976299eed16895f298bd0b4d032f9d5f590046
futugyou <17871902+futugyou@users.noreply.github.com>
2024-09-29 21:03:52 +0800

    implement hashicorp search method

--- infr-project/vault_provider/hashicorp.go
+++ infr-project/vault_provider/hashicorp.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	vault "github.com/hashicorp/hcp-sdk-go/clients/cloud-vault-secrets/stable/2023-06-13/client/secret_service"
@@ -48,7 +49,7 @@ func (s *VaultClient) Get(ctx context.Context, key string) (*ProviderVault, erro
 		LocationOrganizationID: os.Getenv("HCP_ORGANIZATION_ID"),
 		LocationProjectID:      os.Getenv("HCP_PROJECT_ID"),
 		SecretName:             key,
-		Context:                context.Background(),
+		Context:                ctx,
 	}
 
 	var result *vault.OpenAppSecretOK
@@ -70,7 +71,43 @@ func (s *VaultClient) Get(ctx context.Context, key string) (*ProviderVault, erro
 }
 
 func (s *VaultClient) Search(ctx context.Context, key string) ([]ProviderVault, error) {
-	return nil, nil
+	params := &vault.OpenAppSecretsParams{
+		AppName:                os.Getenv("HCP_APP_NAME"),
+		LocationOrganizationID: os.Getenv("HCP_ORGANIZATION_ID"),
+		LocationProjectID:      os.Getenv("HCP_PROJECT_ID"),
+		Context:                ctx,
+	}
+	var result *vault.OpenAppSecretsOK
+	var err error
+	if result, err = s.http.OpenAppSecrets(params, nil); err != nil {
+		return nil, err
+	}
+
+	var providerVaults = []ProviderVault{}
+	if result.Payload != nil || len(result.Payload.Secrets) == 0 {
+		for _, v := range result.Payload.Secrets {
+			if v != nil && v.Version != nil {
+				if len(key) > 0 {
+					if strings.HasPrefix(v.Version.Value, key) {
+						providerVaults = append(providerVaults, ProviderVault{
+							Key:       key,
+							Value:     v.Version.Value,
+							CreatedAt: time.Time(v.Version.CreatedAt),
+						})
+					}
+				} else {
+					providerVaults = append(providerVaults, ProviderVault{
+						Key:       key,
+						Value:     v.Version.Value,
+						CreatedAt: time.Time(v.Version.CreatedAt),
+					})
+				}
+			}
+		}
+		return providerVaults, nil
+	}
+
+	return nil, fmt.Errorf("can not found secret with name %s in hashicorp", key)
 }
 
 func (s *VaultClient) Upinsert(ctx context.Context, key string, value string) (*ProviderVault, error) {
