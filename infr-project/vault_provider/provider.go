d324f4b84078b09239e0e0897cfdd802ec6393b3
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-10-04 12:57:39 +0000

    use AsyncWrapper to generate async method

--- infr-project/vault_provider/provider.go
+++ infr-project/vault_provider/provider.go
@@ -25,4 +25,86 @@ type IVaultProviderAsync interface {
 	BatchSearchAsync(ctx context.Context, keys []string) (<-chan map[string]ProviderVault, <-chan error)
 	UpsertAsync(ctx context.Context, key string, value string) (<-chan *ProviderVault, <-chan error)
 	DeleteAsync(ctx context.Context, key string) <-chan error
+	IVaultProvider
+}
+
+type AsyncWrapper struct {
+	IVaultProvider
+}
+
+func NewAsyncWrapper(provider IVaultProvider) *AsyncWrapper {
+	return &AsyncWrapper{provider}
+}
+
+func (s *AsyncWrapper) SearchAsync(ctx context.Context, key string) (<-chan *ProviderVault, <-chan error) {
+	resultChan := make(chan *ProviderVault, 1)
+	errorChan := make(chan error, 1)
+	go func() {
+		defer close(resultChan)
+		defer close(errorChan)
+		result, err := s.Search(ctx, key)
+		if err != nil {
+			errorChan <- err
+			return
+		}
+		resultChan <- result
+	}()
+	return resultChan, errorChan
+}
+
+func (s *AsyncWrapper) PrefixSearchAsync(ctx context.Context, prefix string) (<-chan map[string]ProviderVault, <-chan error) {
+	resultChan := make(chan map[string]ProviderVault, 1)
+	errorChan := make(chan error, 1)
+	go func() {
+		defer close(resultChan)
+		defer close(errorChan)
+		result, err := s.PrefixSearch(ctx, prefix)
+		if err != nil {
+			errorChan <- err
+			return
+		}
+		resultChan <- result
+	}()
+	return resultChan, errorChan
+}
+
+func (s *AsyncWrapper) BatchSearchAsync(ctx context.Context, keys []string) (<-chan map[string]ProviderVault, <-chan error) {
+	resultChan := make(chan map[string]ProviderVault, 1)
+	errorChan := make(chan error, 1)
+	go func() {
+		defer close(resultChan)
+		defer close(errorChan)
+		result, err := s.BatchSearch(ctx, keys)
+		if err != nil {
+			errorChan <- err
+			return
+		}
+		resultChan <- result
+	}()
+	return resultChan, errorChan
+}
+
+func (s *AsyncWrapper) UpsertAsync(ctx context.Context, key string, value string) (<-chan *ProviderVault, <-chan error) {
+	resultChan := make(chan *ProviderVault, 1)
+	errorChan := make(chan error, 1)
+	go func() {
+		defer close(resultChan)
+		defer close(errorChan)
+		result, err := s.Upsert(ctx, key, value)
+		if err != nil {
+			errorChan <- err
+			return
+		}
+		resultChan <- result
+	}()
+	return resultChan, errorChan
+}
+
+func (s *AsyncWrapper) DeleteAsync(ctx context.Context, key string) <-chan error {
+	errorChan := make(chan error, 1)
+	go func() {
+		defer close(errorChan)
+		errorChan <- s.Delete(ctx, key)
+	}()
+	return errorChan
 }
