f866ea975756f438409f60a318e3c8049fe4b16c
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-10-05 09:18:49 +0000

    update aws ssm search

--- aws-web/services/ssm.go
+++ aws-web/services/ssm.go
@@ -26,18 +26,31 @@ func (s *SSMService) SearchSSMData(ctx context.Context, filter model.SSMDataFilt
 	}
 	awsenv.CfgWithProfileAndRegion(account.AccessKeyId, account.SecretAccessKey, account.Region)
 	svc := ssm.NewFromConfig(awsenv.Cfg)
+	var result = []model.SSMData{}
+	if len(filter.Name) > 0 {
+		return s.getParameters(ctx, []string{filter.Name}, svc)
+	} else {
+		names, err := s.getParameterDescribes(ctx, svc)
+		if err != nil {
+			return s.getParameters(ctx, names, svc)
+		}
+	}
 
+	return result, nil
+}
+
+func (*SSMService) getParameters(ctx context.Context, names []string, svc *ssm.Client) ([]model.SSMData, error) {
+	var result = []model.SSMData{}
 	input := &ssm.GetParametersInput{
-		Names:          []string{filter.Name}, // max count 10, but we have only one
+		Names:          names,
 		WithDecryption: aws.Bool(true),
 	}
 
-	parameters, err := svc.GetParameters(awsenv.EmptyContext, input)
+	parameters, err := svc.GetParameters(ctx, input)
 	if err != nil {
 		return nil, err
 	}
 
-	var result = []model.SSMData{}
 	for _, p := range parameters.Parameters {
 		result = append(result, model.SSMData{
 			Key:        *p.Name,
@@ -45,6 +58,23 @@ func (s *SSMService) SearchSSMData(ctx context.Context, filter model.SSMDataFilt
 			CreateDate: *p.LastModifiedDate,
 		})
 	}
+	return result, nil
+}
 
+func (*SSMService) getParameterDescribes(ctx context.Context, svc *ssm.Client) ([]string, error) {
+	var result = []string{}
+	input := &ssm.DescribeParametersInput{
+		// max value 50
+		MaxResults: aws.Int32(50),
+	}
+	output, err := svc.DescribeParameters(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+	for _, p := range output.Parameters {
+		if p.Name != nil {
+			result = append(result, *p.Name)
+		}
+	}
 	return result, nil
 }
