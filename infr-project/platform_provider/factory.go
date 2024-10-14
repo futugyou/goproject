f7545589fc119d5b3b549ff1d886a5abee407839
futugyou <17871902+futugyou@users.noreply.github.com>
2024-10-14 20:16:00 +0800

    platform provider factory

---
+++ infr-project/platform_provider/factory.go
@@ -0,0 +1,19 @@
+package platform_provider
+
+import "fmt"
+
+func PlatformProviderFatory(provider string, token string) (IPlatformProviderAsync, error) {
+	if provider == "circleci" {
+		client, err := NewCircleClient(token)
+		return client, err
+	}
+	if provider == "github" {
+		client, err := NewGithubClient(token)
+		return client, err
+	}
+	if provider == "vercel" {
+		client, err := NewVercelClient(token)
+		return client, err
+	}
+	return nil, fmt.Errorf("provider type %s is not support", provider)
+}
