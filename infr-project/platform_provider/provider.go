206c6e4cb52c061768627e03c60343d746597770
futugyou <17871902+futugyou@users.noreply.github.com>
2024-10-14 19:31:55 +0800

    platform provider interface

---
+++ infr-project/platform_provider/provider.go
@@ -0,0 +1,39 @@
+package platform_provider
+
+import "context"
+
+//TODO: fill field
+
+type CreateProjectRequest struct {
+	PlatformId string
+	Name       string
+}
+
+type Project struct {
+	Name  string
+	Hooks []WebHook
+}
+
+type WebHook struct {
+	Name string
+	Url  string
+}
+
+type ProjectFilter struct {
+	Name string
+}
+
+type CreateWebHookRequest struct {
+	PlatformId string
+	ProjectId  string
+	WebHook    WebHook
+}
+
+type IPlatformProviderAsync interface {
+	CreateProjectAsync(ctx context.Context, request CreateProjectRequest) (<-chan *Project, <-chan error)
+	// no webhook info
+	ListProjectAsync(ctx context.Context, filter ProjectFilter) (<-chan []Project, <-chan error)
+	// include webhook info
+	GetProjectAsync(ctx context.Context, filter ProjectFilter) (<-chan *Project, <-chan error)
+	CreateWebHookAsync(ctx context.Context, request CreateWebHookRequest) (<-chan *WebHook, <-chan error)
+}
