8ce26922b779d9ca1a9ce80202d264b58b004bc1
futugyou <17871902+futugyou@users.noreply.github.com>
2024-10-16 20:10:54 +0800

    create project in circle ci platform

--- infr-project/platform_provider/circleci.go
+++ infr-project/platform_provider/circleci.go
@@ -2,6 +2,7 @@ package platform_provider
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/futugyou/circleci"
 )
@@ -18,7 +19,36 @@ func NewCircleClient(token string) (*CircleClient, error) {
 }
 
 func (g *CircleClient) CreateProjectAsync(ctx context.Context, request CreateProjectRequest) (<-chan *Project, <-chan error) {
-	return nil, nil
+	resultChan := make(chan *Project, 1)
+	errorChan := make(chan error, 1)
+	go func() {
+		defer close(resultChan)
+		defer close(errorChan)
+		org_slug := ""
+		if org, ok := request.Parameters["org_slug"]; ok {
+			org_slug = org
+		} else {
+			errorChan <- fmt.Errorf("create project request need 'org_slug' in parameters")
+			return
+		}
+
+		if _, err := g.client.Project.CreateProject(org_slug, request.Name); err != nil {
+			errorChan <- err
+			return
+		}
+		project, err := g.client.Project.GetProject(org_slug, request.Name)
+		if err != nil {
+			errorChan <- err
+			return
+		}
+		resultChan <- &Project{
+			ID:   project.ID,
+			Name: project.Name,
+			Url:  "", // url may set in applcation layer
+		}
+	}()
+
+	return resultChan, errorChan
 }
 
 func (g *CircleClient) ListProjectAsync(ctx context.Context, filter ProjectFilter) (<-chan []Project, <-chan error) {
