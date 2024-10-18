939e37aa8d96c2714268b3b9283fa23b7658c5b0
futugyou <17871902+futugyou@users.noreply.github.com>
2024-10-18 14:13:06 +0800

    Implement vercel project create method

--- infr-project/platform_provider/vercel.go
+++ infr-project/platform_provider/vercel.go
@@ -2,6 +2,7 @@ package platform_provider
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/futugyou/vercel"
 )
@@ -17,8 +18,50 @@ func NewVercelClient(token string) (*VercelClient, error) {
 	}, nil
 }
 
+const VercelProjectUrl = "https://vercel.com/%s/%s"
+
+// Optional. TEAM_SLUG TEAM_ID in CreateProjectRequest 'Parameters',
+// default value ” ”.
 func (g *VercelClient) CreateProjectAsync(ctx context.Context, request CreateProjectRequest) (<-chan *Project, <-chan error) {
-	return nil, nil
+	resultChan := make(chan *Project, 1)
+	errorChan := make(chan error, 1)
+
+	go func() {
+		defer close(resultChan)
+		defer close(errorChan)
+
+		req := vercel.CreateProjectRequest{
+			Name: request.Name,
+		}
+
+		team_slug := ""
+		team_id := ""
+		if value, ok := request.Parameters["TEAM_SLUG"]; ok {
+			team_slug = value
+		}
+		if value, ok := request.Parameters["TEAM_ID"]; ok {
+			team_id = value
+		}
+
+		vercelProject, err := g.client.Project.CreateProject(team_slug, team_id, req)
+		if err != nil {
+			errorChan <- err
+			return
+		}
+
+		url := ""
+		if len(team_slug) > 0 {
+			url = fmt.Sprintf(VercelProjectUrl, team_slug, vercelProject.Name)
+		}
+
+		resultChan <- &Project{
+			ID:   vercelProject.Id,
+			Name: vercelProject.Name,
+			Url:  url,
+		}
+	}()
+
+	return resultChan, errorChan
 }
 
 func (g *VercelClient) ListProjectAsync(ctx context.Context, filter ProjectFilter) (<-chan []Project, <-chan error) {
