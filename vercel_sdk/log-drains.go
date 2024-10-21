25f67342a28f220499e3a763609995e6660acc27
futugyou <17871902+futugyou@users.noreply.github.com>
2024-10-22 04:02:05 +0800

    Updated vercel logDrains api

--- vercel_sdk/log-drains.go
+++ vercel_sdk/log-drains.go
@@ -8,163 +8,149 @@ import (
 
 type LogDrainService service
 
-func (v *LogDrainService) CreateLogDrain(ctx context.Context, slug string, teamId string, req CreateLogDrainRequest) (*LogDrainInfo, error) {
-	path := "/v1/log-drains"
-	queryParams := url.Values{}
-	if len(slug) > 0 {
-		queryParams.Add("slug", slug)
-	}
-	if len(teamId) > 0 {
-		queryParams.Add("teamId", teamId)
-	}
-	if len(queryParams) > 0 {
-		path += "?" + queryParams.Encode()
-	}
-	result := &LogDrainInfo{}
-	err := v.client.http.Post(ctx, path, req, result)
-
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-func (v *LogDrainService) CreateIntegrationLogDrain(ctx context.Context, slug string, teamId string, req CreateLogDrainRequest) (*LogDrainInfo, error) {
-	path := "/v2/integrations/log-drains"
-	queryParams := url.Values{}
-	if len(slug) > 0 {
-		queryParams.Add("slug", slug)
-	}
-	if len(teamId) > 0 {
-		queryParams.Add("teamId", teamId)
-	}
-	if len(queryParams) > 0 {
-		path += "?" + queryParams.Encode()
-	}
-	result := &LogDrainInfo{}
-	err := v.client.http.Post(ctx, path, req, result)
-
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-func (v *LogDrainService) DeletesConfigurableLogDrain(ctx context.Context, id string, slug string, teamId string) (*string, error) {
-	path := fmt.Sprintf("/v1/log-drains/%s", id)
-	queryParams := url.Values{}
-	if len(slug) > 0 {
-		queryParams.Add("slug", slug)
-	}
-	if len(teamId) > 0 {
-		queryParams.Add("teamId", teamId)
-	}
-	if len(queryParams) > 0 {
-		path += "?" + queryParams.Encode()
-	}
-	result := ""
-	err := v.client.http.Delete(ctx, path, &result)
-
-	if err != nil {
-		return nil, err
-	}
-	return &result, nil
-}
-
-func (v *LogDrainService) DeletesIntegrationConfigurableLogDrain(ctx context.Context, id string, slug string, teamId string) (*string, error) {
-	path := fmt.Sprintf("/v1/integrations/log-drains/%s", id)
-	queryParams := url.Values{}
-	if len(slug) > 0 {
-		queryParams.Add("slug", slug)
-	}
-	if len(teamId) > 0 {
-		queryParams.Add("teamId", teamId)
-	}
-	if len(queryParams) > 0 {
-		path += "?" + queryParams.Encode()
-	}
-	result := ""
-	err := v.client.http.Delete(ctx, path, &result)
-
-	if err != nil {
-		return nil, err
-	}
-	return &result, nil
-}
-
-func (v *LogDrainService) RetrievesListLogDrain(ctx context.Context, projectId string, slug string, teamId string) ([]LogDrainInfo, error) {
-	path := "/v1/log-drains"
-	queryParams := url.Values{}
-	if len(slug) > 0 {
-		queryParams.Add("slug", slug)
-	}
-	if len(teamId) > 0 {
-		queryParams.Add("teamId", teamId)
-	}
-	if len(queryParams) > 0 {
-		path += "?" + queryParams.Encode()
-	}
-	result := []LogDrainInfo{}
-	err := v.client.http.Delete(ctx, path, &result)
-
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-func (v *LogDrainService) RetrievesConfigurableLogDrain(ctx context.Context, id string, slug string, teamId string) (*LogDrainInfo, error) {
-	path := fmt.Sprintf("/v1/log-drains/%s", id)
-	queryParams := url.Values{}
-	if len(slug) > 0 {
-		queryParams.Add("slug", slug)
-	}
-	if len(teamId) > 0 {
-		queryParams.Add("teamId", teamId)
-	}
-	if len(queryParams) > 0 {
-		path += "?" + queryParams.Encode()
-	}
-	result := &LogDrainInfo{}
-	err := v.client.http.Delete(ctx, path, result)
-
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-func (v *LogDrainService) RetrievesIntegrationLogDrain(ctx context.Context, slug string, teamId string) ([]LogDrainInfo, error) {
-	path := "/v2/integrations/log-drains"
-	queryParams := url.Values{}
-	if len(slug) > 0 {
-		queryParams.Add("slug", slug)
-	}
-	if len(teamId) > 0 {
-		queryParams.Add("teamId", teamId)
-	}
-	if len(queryParams) > 0 {
-		path += "?" + queryParams.Encode()
-	}
-	result := []LogDrainInfo{}
-	err := v.client.http.Delete(ctx, path, &result)
-
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
 type CreateLogDrainRequest struct {
-	DeliveryFormat string      `json:"deliveryFormat,omitempty"`
-	Sources        []string    `json:"sources,omitempty"`
-	Url            string      `json:"url,omitempty"`
-	Environments   []string    `json:"environments,omitempty"`
-	Headers        interface{} `json:"headers,omitempty"`
-	ProjectIds     []string    `json:"projectIds,omitempty"`
-	SamplingRate   int         `json:"samplingRate,omitempty"`
-	Secret         string      `json:"secret,omitempty"`
-	Name           string      `json:"name,omitempty"`
+	DeliveryFormat   string      `json:"deliveryFormat,omitempty"`
+	Sources          []string    `json:"sources,omitempty"`
+	Url              string      `json:"url,omitempty"`
+	Environments     []string    `json:"environments,omitempty"`
+	Headers          interface{} `json:"headers,omitempty"`
+	ProjectIds       []string    `json:"projectIds,omitempty"`
+	SamplingRate     int         `json:"samplingRate,omitempty"`
+	Secret           string      `json:"secret,omitempty"`
+	Name             string      `json:"name,omitempty"`
+	BaseUrlParameter `json:"-"`
+}
+
+func (v *LogDrainService) CreateLogDrain(ctx context.Context, request CreateLogDrainRequest) (*LogDrainInfo, error) {
+	u := &url.URL{
+		Path: "/v1/log-drains",
+	}
+	params := request.GetUrlValues()
+	u.RawQuery = params.Encode()
+	path := u.String()
+
+	response := &LogDrainInfo{}
+	if err := v.client.http.Post(ctx, path, request, response); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func (v *LogDrainService) CreateIntegrationLogDrain(ctx context.Context, request CreateLogDrainRequest) (*LogDrainInfo, error) {
+	u := &url.URL{
+		Path: "/v2/integrations/log-drains",
+	}
+	params := request.GetUrlValues()
+	u.RawQuery = params.Encode()
+	path := u.String()
+
+	response := &LogDrainInfo{}
+	if err := v.client.http.Post(ctx, path, request, response); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+type DeletesConfigurableLogDrainRequest struct {
+	Id               string
+	BaseUrlParameter `json:"-"`
+}
+
+func (v *LogDrainService) DeletesConfigurableLogDrain(ctx context.Context, request DeletesConfigurableLogDrainRequest) (*string, error) {
+	u := &url.URL{
+		Path: fmt.Sprintf("/v1/log-drains/%s", request.Id),
+	}
+	params := request.GetUrlValues()
+	u.RawQuery = params.Encode()
+	path := u.String()
+
+	response := ""
+	if err := v.client.http.Delete(ctx, path, &response); err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
+
+type DeletesIntegrationConfigurableLogDrainRequest struct {
+	Id               string
+	BaseUrlParameter `json:"-"`
+}
+
+func (v *LogDrainService) DeletesIntegrationConfigurableLogDrain(ctx context.Context, request DeletesIntegrationConfigurableLogDrainRequest) (*string, error) {
+	u := &url.URL{
+		Path: fmt.Sprintf("/v1/integrations/log-drains/%s", request.Id),
+	}
+	params := request.GetUrlValues()
+	u.RawQuery = params.Encode()
+	path := u.String()
+
+	response := ""
+	if err := v.client.http.Delete(ctx, path, &response); err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
+
+type RetrievesListLogDrainParameter struct {
+	ProjectId        *string
+	BaseUrlParameter `json:"-"`
+}
+
+func (v *LogDrainService) RetrievesListLogDrain(ctx context.Context, request RetrievesListLogDrainParameter) ([]LogDrainInfo, error) {
+	u := &url.URL{
+		Path: "/v1/log-drains",
+	}
+	params := request.GetUrlValues()
+	if request.ProjectId != nil {
+		params.Add("projectId", *request.ProjectId)
+	}
+	u.RawQuery = params.Encode()
+	path := u.String()
+
+	response := []LogDrainInfo{}
+	if err := v.client.http.Delete(ctx, path, &response); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+type RetrievesConfigurableLogDrainParameter struct {
+	Id               string
+	BaseUrlParameter `json:"-"`
+}
+
+func (v *LogDrainService) RetrievesConfigurableLogDrain(ctx context.Context, request RetrievesConfigurableLogDrainParameter) (*LogDrainInfo, error) {
+	u := &url.URL{
+		Path: fmt.Sprintf("/v1/log-drains/%s", request.Id),
+	}
+	params := request.GetUrlValues()
+	u.RawQuery = params.Encode()
+	path := u.String()
+
+	response := &LogDrainInfo{}
+	if err := v.client.http.Delete(ctx, path, response); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+type RetrievesIntegrationLogDrainParameter struct {
+	BaseUrlParameter `json:"-"`
+}
+
+func (v *LogDrainService) RetrievesIntegrationLogDrain(ctx context.Context, request RetrievesIntegrationLogDrainParameter) ([]LogDrainInfo, error) {
+	u := &url.URL{
+		Path: "/v2/integrations/log-drains",
+	}
+	params := request.GetUrlValues()
+	u.RawQuery = params.Encode()
+	path := u.String()
+
+	response := []LogDrainInfo{}
+	if err := v.client.http.Delete(ctx, path, &response); err != nil {
+		return nil, err
+	}
+	return response, nil
 }
 
 type LogDrainInfo struct {
