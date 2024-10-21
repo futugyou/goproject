79a9ff69e1bfb2bd5dc53f4b013c1357e31294c4
futugyou <17871902+futugyou@users.noreply.github.com>
2024-10-21 20:29:59 +0800

    Updated vercel checks sdk

--- vercel_sdk/checks.go
+++ vercel_sdk/checks.go
@@ -8,146 +8,174 @@ import (
 
 type CheckService service
 
-func (v *CheckService) CreatesCheck(ctx context.Context, deploymentId string, slug string, teamId string, req CreateCheckRequest) (*CheckInfo, error) {
-	path := fmt.Sprintf("/v1/deployments/%s/checks", deploymentId)
-	queryParams := url.Values{}
-	if len(slug) > 0 {
-		queryParams.Add("slug", slug)
-	}
-	if len(teamId) > 0 {
-		queryParams.Add("teamId", teamId)
-	}
-	if len(queryParams) > 0 {
-		path += "?" + queryParams.Encode()
-	}
-	result := &CheckInfo{}
-	err := v.client.http.Post(ctx, path, req, result)
-
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-func (v *CheckService) ListAllChecks(ctx context.Context, deploymentId string, slug string, teamId string) ([]CheckInfo, error) {
-	path := fmt.Sprintf("/v1/deployments/%s/checks", deploymentId)
-	queryParams := url.Values{}
-	if len(slug) > 0 {
-		queryParams.Add("slug", slug)
-	}
-	if len(teamId) > 0 {
-		queryParams.Add("teamId", teamId)
-	}
-	if len(queryParams) > 0 {
-		path += "?" + queryParams.Encode()
-	}
-	result := []CheckInfo{}
-	err := v.client.http.Get(ctx, path, &result)
-
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-func (v *CheckService) GetSingleCheck(ctx context.Context, deploymentId string, checkId string, slug string, teamId string) (*CheckInfo, error) {
-	path := fmt.Sprintf("/v1/deployments/%s/checks/%s", deploymentId, checkId)
-	queryParams := url.Values{}
-	if len(slug) > 0 {
-		queryParams.Add("slug", slug)
-	}
-	if len(teamId) > 0 {
-		queryParams.Add("teamId", teamId)
-	}
-	if len(queryParams) > 0 {
-		path += "?" + queryParams.Encode()
-	}
-	result := &CheckInfo{}
-	err := v.client.http.Get(ctx, path, result)
-
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-func (v *CheckService) RerequestCheck(ctx context.Context, deploymentId string, checkId string, slug string, teamId string) (*CheckInfo, error) {
-	path := fmt.Sprintf("/v1/deployments/%s/checks/%s/rerequest", deploymentId, checkId)
-	queryParams := url.Values{}
-	if len(slug) > 0 {
-		queryParams.Add("slug", slug)
-	}
-	if len(teamId) > 0 {
-		queryParams.Add("teamId", teamId)
-	}
-	if len(queryParams) > 0 {
-		path += "?" + queryParams.Encode()
-	}
-	result := &CheckInfo{}
-	err := v.client.http.Post(ctx, path, nil, result)
-
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-func (v *CheckService) UpdateCheck(ctx context.Context, deploymentId string, checkId string, slug string, teamId string, req CheckInfo) (*CheckInfo, error) {
-	path := fmt.Sprintf("/v1/deployments/%s/checks/%s", deploymentId, checkId)
-	queryParams := url.Values{}
-	if len(slug) > 0 {
-		queryParams.Add("slug", slug)
-	}
-	if len(teamId) > 0 {
-		queryParams.Add("teamId", teamId)
-	}
-	if len(queryParams) > 0 {
-		path += "?" + queryParams.Encode()
-	}
-	result := &CheckInfo{}
-	err := v.client.http.Post(ctx, path, req, result)
-
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
 type CreateCheckRequest struct {
-	Blocking      bool   `json:"blocking"`
-	Name          string `json:"name"`
-	DetailsUrl    string `json:"detailsUrl,omitempty"`
-	ExternalId    string `json:"externalId,omitempty"`
-	Path          string `json:"path,omitempty"`
-	Rerequestable bool   `json:"rerequestable,omitempty"`
+	Blocking         bool   `json:"blocking"`
+	Name             string `json:"name"`
+	DetailsUrl       string `json:"detailsUrl,omitempty"`
+	ExternalId       string `json:"externalId,omitempty"`
+	Path             string `json:"path,omitempty"`
+	Rerequestable    bool   `json:"rerequestable,omitempty"`
+	DeploymentId     string `json:"-"`
+	BaseUrlParameter `json:"-"`
+}
+
+func (v *CheckService) CreatesCheck(ctx context.Context, request CreateCheckRequest) (*CheckInfo, error) {
+	u := &url.URL{
+		Path: fmt.Sprintf("/v1/deployments/%s/checks", request.DeploymentId),
+	}
+	params := url.Values{}
+	if request.TeamId != nil {
+		params.Add("teamId", *request.TeamId)
+	}
+	if request.TeamSlug != nil {
+		params.Add("slug", *request.TeamSlug)
+	}
+	u.RawQuery = params.Encode()
+	path := u.String()
+
+	response := &CheckInfo{}
+	if err := v.client.http.Post(ctx, path, request, response); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+type ListChecksParameter struct {
+	DeploymentId     string `json:"-"`
+	BaseUrlParameter `json:"-"`
+}
+
+func (v *CheckService) ListAllChecks(ctx context.Context, request ListChecksParameter) ([]CheckInfo, error) {
+	u := &url.URL{
+		Path: fmt.Sprintf("/v1/deployments/%s/checks", request.DeploymentId),
+	}
+	params := url.Values{}
+	if request.TeamId != nil {
+		params.Add("teamId", *request.TeamId)
+	}
+	if request.TeamSlug != nil {
+		params.Add("slug", *request.TeamSlug)
+	}
+	u.RawQuery = params.Encode()
+	path := u.String()
+
+	response := []CheckInfo{}
+	if err := v.client.http.Get(ctx, path, &response); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+type SingleCheckParameter struct {
+	DeploymentId     string `json:"-"`
+	CheckId          string `json:"-"`
+	BaseUrlParameter `json:"-"`
+}
+
+func (v *CheckService) GetSingleCheck(ctx context.Context, request SingleCheckParameter) (*CheckInfo, error) {
+	u := &url.URL{
+		Path: fmt.Sprintf("/v1/deployments/%s/checks/%s", request.DeploymentId, request.CheckId),
+	}
+	params := url.Values{}
+	if request.TeamId != nil {
+		params.Add("teamId", *request.TeamId)
+	}
+	if request.TeamSlug != nil {
+		params.Add("slug", *request.TeamSlug)
+	}
+	u.RawQuery = params.Encode()
+	path := u.String()
+
+	response := &CheckInfo{}
+	if err := v.client.http.Get(ctx, path, response); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+type RerequestCheckRequest struct {
+	DeploymentId     string `json:"-"`
+	CheckId          string `json:"-"`
+	BaseUrlParameter `json:"-"`
+}
+
+func (v *CheckService) RerequestCheck(ctx context.Context, request RerequestCheckRequest) (*CheckInfo, error) {
+	u := &url.URL{
+		Path: fmt.Sprintf("/v1/deployments/%s/checks/%s/rerequest", request.DeploymentId, request.CheckId),
+	}
+	params := url.Values{}
+	if request.TeamId != nil {
+		params.Add("teamId", *request.TeamId)
+	}
+	if request.TeamSlug != nil {
+		params.Add("slug", *request.TeamSlug)
+	}
+	u.RawQuery = params.Encode()
+	path := u.String()
+
+	response := &CheckInfo{}
+	if err := v.client.http.Post(ctx, path, nil, response); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+type UpdateCheckRequest struct {
+	Conclusion       string      `json:"conclusion"`
+	DetailsUrl       string      `json:"detailsUrl"`
+	ExternalId       string      `json:"externalId"`
+	Name             string      `json:"name"`
+	Output           CheckOutput `json:"output"`
+	Path             string      `json:"path"`
+	Status           string      `json:"status"`
+	DeploymentId     string      `json:"-"`
+	CheckId          string      `json:"-"`
+	BaseUrlParameter `json:"-"`
+}
+
+func (v *CheckService) UpdateCheck(ctx context.Context, request UpdateCheckRequest) (*CheckInfo, error) {
+	u := &url.URL{
+		Path: fmt.Sprintf("/v1/deployments/%s/checks/%s", request.DeploymentId, request.CheckId),
+	}
+	params := url.Values{}
+	if request.TeamId != nil {
+		params.Add("teamId", *request.TeamId)
+	}
+	if request.TeamSlug != nil {
+		params.Add("slug", *request.TeamSlug)
+	}
+	u.RawQuery = params.Encode()
+	path := u.String()
+
+	response := &CheckInfo{}
+	if err := v.client.http.Post(ctx, path, request, response); err != nil {
+		return nil, err
+	}
+	return response, nil
 }
 
 type CheckInfo struct {
-	Blocking      bool         `json:"blocking"`
-	CompletedAt   int          `json:"completedAt"`
 	Conclusion    string       `json:"conclusion"`
-	CreatedAt     int          `json:"createdAt"`
-	DeploymentId  string       `json:"deploymentId"`
 	DetailsUrl    string       `json:"detailsUrl"`
 	ExternalId    string       `json:"externalId"`
+	Name          string       `json:"name"`
+	Output        CheckOutput  `json:"output"`
+	Path          string       `json:"path"`
+	Status        string       `json:"status"`
+	Blocking      bool         `json:"blocking"`
+	CompletedAt   int          `json:"completedAt"`
+	CreatedAt     int          `json:"createdAt"`
+	StartedAt     int          `json:"startedAt"`
+	UpdatedAt     int          `json:"updatedAt"`
+	DeploymentId  string       `json:"deploymentId"`
 	Id            string       `json:"id"`
 	IntegrationId string       `json:"integrationId"`
-	Name          string       `json:"name"`
-	Path          string       `json:"path"`
-	Output        CheckOutput  `json:"output"`
+	Rerequestable bool         `json:"rerequestable,omitempty"`
 	Error         *VercelError `json:"error"`
 }
 
 type CheckOutput struct {
-	Metrics CheckMetrics `json:"metrics"`
-}
-
-type CheckMetrics struct {
-	Fcp                    CheckCls `json:"FCP"`
-	Lcp                    CheckCls `json:"LCP"`
-	Cls                    CheckCls `json:"CLS"`
-	Tbt                    CheckCls `json:"TBT"`
-	VirtualExperienceScore CheckCls `json:"virtualExperienceScore"`
+	Metrics map[string]CheckCls `json:"metrics"`
 }
 
 type CheckCls struct {
