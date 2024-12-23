6706fe1bf768810724143a9f6295d869c2e36251
futugyou <17871902+futugyou@users.noreply.github.com>
2024-12-24 01:27:18 +0800

    Updated string sanitize extension

--- extensions/regexp.go
+++ extensions/regexp.go
@@ -1,6 +1,11 @@
 package extensions
 
-import "github.com/dlclark/regexp2"
+import (
+	"regexp"
+	"strings"
+
+	"github.com/dlclark/regexp2"
+)
 
 func Regexp2FindAllString(pattern, s string) []string {
 	re := regexp2.MustCompile(pattern, 0)
@@ -12,4 +17,30 @@ func Regexp2FindAllString(pattern, s string) []string {
 	}
 
 	return matches
+}
+
+func Sanitize2String(input string) string {
+	// Compile the regular expression to match non-alphanumeric characters
+	re := regexp2.MustCompile(`[^a-zA-Z0-9]+`, regexp2.None)
+
+	// Replace matches with a space
+	result, _ := re.Replace(input, " ", 0, -1)
+
+	// Trim and replace multiple spaces with a single space
+	result = strings.TrimSpace(result)
+	result = strings.Join(strings.Fields(result), " ")
+
+	return result
+}
+
+func SanitizeString(input string) string {
+	// Replace all non-alphanumeric characters with spaces
+	re := regexp.MustCompile(`[^a-zA-Z0-9]+`)
+	input = re.ReplaceAllString(input, " ")
+
+	// Trim and replace multiple spaces with a single space
+	input = strings.TrimSpace(input)
+	input = strings.Join(strings.Fields(input), " ")
+
+	return input
 }
