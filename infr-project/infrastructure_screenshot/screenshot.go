7fae6510f83d262fa4970de6bade5d530b591d9e
futugyou <17871902+futugyou@users.noreply.github.com>
2025-02-24 13:22:45 +0000

    Updated screenshot implement added screenshot factory

--- infr-project/infrastructure_screenshot/screenshot.go
+++ infr-project/infrastructure_screenshot/screenshot.go
@@ -12,16 +12,32 @@ import (
 )
 
 type Screenshot struct {
+	screenshotClient *screenshot.ScreenshotClient
+	folderID         string
+	server           string
+	fileClient       *gofile.GofileClient
+}
+
+func NewScreenshot() *Screenshot {
+	screenshotClient := screenshot.NewClient(os.Getenv("SCREENSHOT_API_KEY"))
+	folderID := os.Getenv("GOFILE_FOLDER")
+	server := os.Getenv("GOFILE_SERVER")
+	fileClient := gofile.NewClient(os.Getenv("GOFILE_TOKEN"))
+	return &Screenshot{
+		screenshotClient: screenshotClient,
+		folderID:         folderID,
+		server:           server,
+		fileClient:       fileClient,
+	}
 }
 
 func (s *Screenshot) Create(ctx context.Context, url string) (*string, error) {
-	sclient := screenshot.NewClient(os.Getenv("SCREENSHOT_API_KEY"))
 	var image_data []byte
 	var err error
 	if os.Getenv("SCREENSHOT_TYPE") == "Apiflash" {
-		image_data, err = sclient.Apiflash.GetScreenshot(ctx, url, map[string]string{"wait_until": "page_loaded", "format": "png"})
+		image_data, err = s.screenshotClient.Apiflash.GetScreenshot(ctx, url, map[string]string{"wait_until": "page_loaded", "format": "png"})
 	} else {
-		image_data, err = sclient.Screenshotmachine.GetScreenshot(ctx, url, map[string]string{"dimension": "320x240", "delay": "2000", "format": "png"})
+		image_data, err = s.screenshotClient.Screenshotmachine.GetScreenshot(ctx, url, map[string]string{"dimension": "320x240", "delay": "2000", "format": "png"})
 	}
 
 	if err != nil {
@@ -29,22 +45,19 @@ func (s *Screenshot) Create(ctx context.Context, url string) (*string, error) {
 	}
 
 	reader := bytes.NewReader(image_data)
-	folderID := os.Getenv("GOFILE_FOLDER")
-	server := os.Getenv("GOFILE_SERVER")
 	fileName := fmt.Sprintf("%d.png", time.Now().UnixMilli())
 	request := gofile.UploadFileRequest{
 		File:     reader,
-		FolderId: &folderID,
-		Server:   &server,
+		FolderId: &s.folderID,
+		Server:   &s.server,
 		FileName: fileName,
 	}
 
-	fileClient := gofile.NewClient(os.Getenv("GOFILE_TOKEN"))
-	resp, err := fileClient.Contents.UploadFile(ctx, request)
+	resp, err := s.fileClient.Contents.UploadFile(ctx, request)
 	if err != nil {
 		return nil, err
 	}
 
-	fileUrl := fmt.Sprintf("https://%s.gofile.io/download/web/%s/%s", server, resp.Data.ID, fileName)
+	fileUrl := fmt.Sprintf("https://%s.gofile.io/download/web/%s/%s", s.server, resp.Data.ID, fileName)
 	return &fileUrl, nil
 }
