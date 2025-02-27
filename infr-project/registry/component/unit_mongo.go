0c1984838217022fafa83705de888433db399fbb
futugyou <17871902+futugyou@users.noreply.github.com>
2025-02-27 13:07:01 +0000

    Added mongo unit of work registry

---
+++ infr-project/registry/component/unit_mongo.go
@@ -0,0 +1,31 @@
+package component
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	mongo_options "go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/futugyou/infr-project/domain"
+	"github.com/futugyou/infr-project/infrastructure_mongo"
+	"github.com/futugyou/infr-project/options"
+	"github.com/futugyou/infr-project/registry/unit"
+)
+
+func init() {
+	unit.DefaultRegistry.RegisterComponent(func(ctx context.Context, option options.Options) domain.IUnitOfWork {
+		config := infrastructure_mongo.DBConfig{
+			ConnectString: option.MongoDBURL,
+		}
+		mongoclient, _ := mongo.Connect(ctx, mongo_options.Client().ApplyURI(config.ConnectString))
+		u, _ := infrastructure_mongo.NewMongoUnitOfWork(mongoclient)
+		return u
+	}, func(ctx context.Context, option options.Options) domain.IUnitOfWorkAsync {
+		config := infrastructure_mongo.DBConfig{
+			ConnectString: option.MongoDBURL,
+		}
+		mongoclient, _ := mongo.Connect(ctx, mongo_options.Client().ApplyURI(config.ConnectString))
+		u, _ := infrastructure_mongo.NewMongoUnitOfWork(mongoclient)
+		return u
+	}, "mongo")
+}
