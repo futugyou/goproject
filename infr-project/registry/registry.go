7b8cd399d6e982c224b0e6c2995f64f1c790eebe
futugyou <17871902+futugyou@users.noreply.github.com>
2025-02-28 13:02:37 +0000

    Added registry added registry flied and factory method

--- infr-project/registry/registry.go
+++ infr-project/registry/registry.go
@@ -1,1 +1,27 @@
 package registry
+
+import (
+	"github.com/futugyou/infr-project/registry/event_store"
+	"github.com/futugyou/infr-project/registry/publisher"
+	"github.com/futugyou/infr-project/registry/screenshot"
+	"github.com/futugyou/infr-project/registry/snapshot_store"
+	"github.com/futugyou/infr-project/registry/unit"
+)
+
+type Registry struct {
+	UnitOfWork    *unit.Registry
+	SnapshotStore *snapshot_store.Registry
+	Screenshot    *screenshot.Registry
+	Publisher     *publisher.Registry
+	EventStore    *event_store.Registry
+}
+
+func New() *Registry {
+	return &Registry{
+		UnitOfWork:    unit.DefaultRegistry,
+		SnapshotStore: snapshot_store.DefaultRegistry,
+		Screenshot:    screenshot.DefaultRegistry,
+		Publisher:     publisher.DefaultRegistry,
+		EventStore:    event_store.DefaultRegistry,
+	}
+}
