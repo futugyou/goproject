d598fdf35accc2ce5dbba94a5efb82c35a982c6d
futugyou <17871902+futugyou@users.noreply.github.com>
2025-03-02 15:08:43 +0000

    Added DelegatingEmbeddingGenerator

--- ai-extension/abstractions/chatcompletion/delegatingchatclient.go
+++ ai-extension/abstractions/chatcompletion/delegatingchatclient.go
@@ -3,17 +3,17 @@ package chatcompletion
 import "context"
 
 type DelegatingChatClient struct {
-	delegate IChatClient
+	InnerClient IChatClient
 }
 
 func NewDelegatingChatClient(delegate IChatClient) *DelegatingChatClient {
-	return &DelegatingChatClient{delegate: delegate}
+	return &DelegatingChatClient{InnerClient: delegate}
 }
 
 func (c *DelegatingChatClient) GetResponse(ctx context.Context, chatMessages []ChatMessage, options *ChatOptions) (*ChatResponse, error) {
-	return c.delegate.GetResponse(ctx, chatMessages, options)
+	return c.InnerClient.GetResponse(ctx, chatMessages, options)
 }
 
 func (c *DelegatingChatClient) GetStreamingResponse(ctx context.Context, chatMessages []ChatMessage, options *ChatOptions) <-chan ChatStreamingResponse {
-	return c.delegate.GetStreamingResponse(ctx, chatMessages, options)
+	return c.InnerClient.GetStreamingResponse(ctx, chatMessages, options)
 }
---
+++ ai-extension/abstractions/embeddings/delegating.go
@@ -0,0 +1,17 @@
+package embeddings
+
+import "context"
+
+type DelegatingEmbeddingGenerator[TInput any, TEmbedding IEmbedding] struct {
+	InnerGenerator IEmbeddingGenerator[TInput, TEmbedding]
+}
+
+func NewDelegatingEmbeddingGenerator[TInput any, TEmbedding IEmbedding](innerGenerator IEmbeddingGenerator[TInput, TEmbedding]) *DelegatingEmbeddingGenerator[TInput, TEmbedding] {
+	return &DelegatingEmbeddingGenerator[TInput, TEmbedding]{
+		InnerGenerator: innerGenerator,
+	}
+}
+
+func (g *DelegatingEmbeddingGenerator[TInput, TEmbedding]) Generate(ctx context.Context, values []TInput, options *EmbeddingGenerationOptions) (GeneratedEmbeddings[TEmbedding], error) {
+	return g.InnerGenerator.Generate(ctx, values, options)
+}
