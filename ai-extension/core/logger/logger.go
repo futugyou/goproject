770cb4e0cbb273b90bf8fc67d2f6bcd4c0c0f045
futugyou <17871902+futugyou@users.noreply.github.com>
2025-03-09 15:53:42 +0000

    Added a new logger module with comprehensive logging functionalities and level settings

---
+++ ai-extension/core/logger/logger.go
@@ -0,0 +1,72 @@
+/*
+Based on  https://github.com/dapr/kit/blob/main/logger/logger.go
+*/
+
+package logger
+
+import (
+	"context"
+	"io"
+	"strings"
+)
+
+const (
+	LogTypeLog     = "log"
+	LogTypeRequest = "request"
+)
+
+type logContextKeyType struct{}
+
+var logContextKey = logContextKeyType{}
+
+type LogLevel string
+
+const (
+	DebugLevel     LogLevel = "debug"
+	InfoLevel      LogLevel = "info"
+	WarnLevel      LogLevel = "warn"
+	ErrorLevel     LogLevel = "error"
+	FatalLevel     LogLevel = "fatal"
+	UndefinedLevel LogLevel = "undefined"
+)
+
+type Logger interface {
+	EnableJSONOutput(enabled bool)
+	SetAppID(id string)
+	SetOutputLevel(outputLevel LogLevel)
+	SetOutput(dst io.Writer)
+	IsOutputLevelEnabled(level LogLevel) bool
+	WithLogType(logType string) Logger
+	WithFields(fields map[string]any) Logger
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+	Warn(args ...interface{})
+	Warnf(format string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
+}
+
+func ToLogLevel(level string) LogLevel {
+	switch strings.ToLower(level) {
+	case "debug":
+		return DebugLevel
+	case "info":
+		return InfoLevel
+	case "warn":
+		return WarnLevel
+	case "error":
+		return ErrorLevel
+	case "fatal":
+		return FatalLevel
+	}
+
+	return UndefinedLevel
+}
+
+func NewContext(ctx context.Context, logger Logger) context.Context {
+	return context.WithValue(ctx, logContextKey, logger)
+}
