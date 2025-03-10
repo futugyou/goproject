756ae6bfdd8e97e0e32b1a97fc8a9604687f15d2
futugyou <17871902+futugyou@users.noreply.github.com>
2025-03-10 15:29:52 +0000

    Added DistributedCachingEmbeddingGenerator for improved caching capabilities in embeddings module

---
+++ ai-extension/ai/embeddings/distributedcachingembedding.go
@@ -0,0 +1,52 @@
+package embeddings
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/futugyou/ai-extension/abstractions/embeddings"
+	"github.com/futugyou/ai-extension/abstractions/utilities"
+	"github.com/futugyou/ai-extension/core"
+)
+
+type DistributedCachingEmbeddingGenerator[TInput any, TEmbedding embeddings.IEmbedding] struct {
+	*CachingEmbeddingGenerator[TInput, TEmbedding]
+	storage core.IDistributedCache
+}
+
+func NewDistributedCachingEmbeddingGenerator[TInput any, TEmbedding embeddings.IEmbedding](
+	innerGenerator embeddings.IEmbeddingGenerator[TInput, TEmbedding],
+	storage core.IDistributedCache,
+) *DistributedCachingEmbeddingGenerator[TInput, TEmbedding] {
+	return &DistributedCachingEmbeddingGenerator[TInput, TEmbedding]{
+		CachingEmbeddingGenerator: NewCachingEmbeddingGenerator[TInput, TEmbedding](innerGenerator),
+		storage:                   storage,
+	}
+}
+
+func (client *DistributedCachingEmbeddingGenerator[TInput, TEmbedding]) ReadCache(ctx context.Context, key string) (*TEmbedding, error) {
+	cache, err := client.storage.Get(ctx, key)
+	if err != nil {
+		return nil, err
+	}
+
+	var result TEmbedding
+	if err := json.Unmarshal(cache, &result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+func (client *DistributedCachingEmbeddingGenerator[TInput, TEmbedding]) WriteCache(ctx context.Context, key string, value TEmbedding) error {
+	cache, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+
+	return client.storage.Set(ctx, key, cache)
+}
+
+func (client *DistributedCachingEmbeddingGenerator[TInput, TEmbedding]) GetCacheKey(values ...interface{}) string {
+	return utilities.HashDataToString(values...)
+}
