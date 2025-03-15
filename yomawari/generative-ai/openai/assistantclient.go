797ca62af496471add4c3a1eec790a1c2f008a25
futugyou <17871902+futugyou@users.noreply.github.com>
2025-03-15 16:35:22 +0000

    Added openai package

---
+++ yomawari/generative-ai/openai/assistantclient.go
@@ -0,0 +1,71 @@
+package openai
+
+import (
+	"context"
+
+	"github.com/futugyou/yomawari/generative-ai/abstractions/chatcompletion"
+	rawopenai "github.com/openai/openai-go"
+	"github.com/openai/openai-go/packages/ssestream"
+)
+
+type OpenAIAssistantClient struct {
+	metadata    chatcompletion.ChatClientMetadata
+	threads     *rawopenai.BetaThreadService
+	assistantId string
+	threadId    *string
+}
+
+func NewOpenAIAssistantClient(threads *rawopenai.BetaThreadService, assistantId string, threadId *string) *OpenAIAssistantClient {
+	name := "openai"
+	return &OpenAIAssistantClient{
+		metadata: chatcompletion.ChatClientMetadata{
+			ProviderName: &name,
+		},
+		threads:     threads,
+		assistantId: assistantId,
+		threadId:    threadId,
+	}
+}
+
+func (client *OpenAIAssistantClient) GetResponse(ctx context.Context, chatMessages []chatcompletion.ChatMessage, options *chatcompletion.ChatOptions) (*chatcompletion.ChatResponse, error) {
+
+	return nil, nil
+}
+
+func (client *OpenAIAssistantClient) GetStreamingResponse(ctx context.Context, chatMessages []chatcompletion.ChatMessage, options *chatcompletion.ChatOptions) <-chan chatcompletion.ChatStreamingResponse {
+	result := make(chan chatcompletion.ChatStreamingResponse)
+	threadId := ""
+	runId := ""
+	//runId = getRunId(....)
+	if options != nil && options.ChatThreadId != nil {
+		threadId = *options.ChatThreadId
+	}
+
+	var stream *ssestream.Stream[rawopenai.AssistantStreamEvent]
+	if len(runId) > 0 && len(threadId) > 0 {
+		// TODO
+		params := rawopenai.BetaThreadRunSubmitToolOutputsParams{
+			ToolOutputs: rawopenai.F([]rawopenai.BetaThreadRunSubmitToolOutputsParamsToolOutput{}),
+		}
+		stream = client.threads.Runs.SubmitToolOutputsStreaming(ctx, threadId, runId, params)
+	} else if len(threadId) == 0 {
+		// TODO
+		params := rawopenai.BetaThreadNewAndRunParams{}
+		stream = client.threads.NewAndRunStreaming(ctx, params)
+	} else {
+		// TODO
+		params := rawopenai.BetaThreadRunNewParams{}
+		stream = client.threads.Runs.NewStreaming(ctx, threadId, params)
+	}
+	for stream.Next() {
+		evt := stream.Current()
+		result<- chatcompletion.ChatStreamingResponse{
+			Update: ToChatResponseUpdateFromAssistantStreamEvent(evt),
+		}
+	}
+	return result
+}
+
+func ToChatResponseUpdateFromAssistantStreamEvent(evt rawopenai.AssistantStreamEvent) *chatcompletion.ChatResponseUpdate{
+	panic("unimplemented")
+}
--- yomawari/go.mod
+++ yomawari/go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/google/uuid v1.6.0
+	github.com/openai/openai-go v0.1.0-alpha.53
 	go.opentelemetry.io/otel v1.23.0
 	go.opentelemetry.io/otel/metric v1.23.0
 	go.opentelemetry.io/otel/trace v1.23.0
@@ -12,4 +13,8 @@ require (
 require (
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
 )
