7b2859c19f31775d7a80e89f713ea9f3edf1fa07
futugyou <17871902+futugyou@users.noreply.github.com>
2025-03-15 18:05:17 +0000

    Added empty OpenAIResponseChatClient

---
+++ yomawari/generative-ai/openai/responsechatclient.go
@@ -0,0 +1,35 @@
+package openai
+
+import (
+	"context"
+
+	"github.com/futugyou/yomawari/generative-ai/abstractions/chatcompletion"
+	rawopenai "github.com/openai/openai-go"
+)
+
+// TODO: OpenAI official golang sdk has not yet implemented the responses interface
+type OpenAIResponseChatClient struct {
+	metadata     chatcompletion.ChatClientMetadata
+	openAIClient *rawopenai.Client
+	modelId      *string
+}
+
+func NewOpenAIResponseChatClient(openAIClient *rawopenai.Client, modelId *string) *OpenAIResponseChatClient {
+	name := "openai"
+	return &OpenAIResponseChatClient{
+		metadata: chatcompletion.ChatClientMetadata{
+			ProviderName: &name,
+		},
+		openAIClient: openAIClient,
+		modelId:      modelId,
+	}
+}
+
+func (client *OpenAIResponseChatClient) GetResponse(ctx context.Context, chatMessages []chatcompletion.ChatMessage, options *chatcompletion.ChatOptions) (*chatcompletion.ChatResponse, error) {
+	return nil, nil
+}
+
+func (client *OpenAIResponseChatClient) GetStreamingResponse(ctx context.Context, chatMessages []chatcompletion.ChatMessage, options *chatcompletion.ChatOptions) <-chan chatcompletion.ChatStreamingResponse {
+	result := make(chan chatcompletion.ChatStreamingResponse)
+	return result
+}
