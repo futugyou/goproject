d78fa0bd385f6f4197c91657c73c4b35742e506d
futugyou <17871902+futugyou@users.noreply.github.com>
2025-03-21 15:24:35 +0000

    Added IContext interface with various argument management methods

---
+++ yomawari/kernel-memory/abstractions/context/icontext.go
@@ -0,0 +1,41 @@
+package context
+
+type IContext interface {
+	InitArgs(map[string]interface{}) IContext
+	SetArgs(map[string]interface{}) IContext
+	GetArgs() map[string]interface{}
+	ResetArgs() IContext
+	TryGetArg(ctx IContext, key string) (interface{}, bool)
+	GetCustomEmptyAnswerTextOrDefault(defaultValue string) string
+	GetCustomRagFactTemplateOrDefault(defaultValue string) string
+	GetCustomRagIncludeDuplicateFactsOrDefault(defaultValue bool) bool
+	GetCustomRagPromptOrDefault(defaultValue string) string
+	GetCustomRagMaxTokensOrDefault(defaultValue int64) int64
+	GetCustomRagMaxMatchesCountOrDefault(defaultValue int64) int64
+	GetCustomRagTemperatureOrDefault(defaultValue float64) float64
+	GetCustomRagNucleusSamplingOrDefault(defaultValue float64) float64
+	GetCustomSummaryPromptOrDefault(defaultValue string) string
+	GetCustomSummaryTargetTokenSizeOrDefault(defaultValue int64) int64
+	GetCustomSummaryOverlappingTokensOrDefault(defaultValue int64) int64
+	GetCustomPartitioningMaxTokensPerChunkOrDefault(defaultValue int64) int64
+	GetCustomPartitioningOverlappingTokensOrDefault(defaultValue int64) int64
+	GetCustomPartitioningChunkHeaderOrDefault(defaultValue *string) *string
+	GetCustomEmbeddingGenerationBatchSizeOrDefault(defaultValue int64) int64
+	GetCustomTextGenerationModelNameOrDefault(defaultValue string) string
+	GetCustomEmbeddingGenerationModelNameOrDefault(defaultValue string) string
+}
+
+func TryGetContextArg[T any](ctx IContext, key string) (*T, bool) {
+	if ctx == nil {
+		return new(T), false
+	}
+	if ctx.GetArgs() == nil {
+		return new(T), false
+	}
+	if v, ok := ctx.GetArgs()[key]; ok {
+		if condition, ok := v.(T); ok {
+			return &condition, true
+		}
+	}
+	return new(T), false
+}
