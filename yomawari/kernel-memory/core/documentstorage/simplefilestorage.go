08eae99c58687a3ef3ac395d08c7a8bd2ac0e4b4
futugyou <17871902+futugyou@users.noreply.github.com>
2025-03-25 01:57:04 +0000

    Implemented SimpleFileStorage with support for Volatile and Disk file systems

--- yomawari/kernel-memory/core/documentstorage/simplefilestorage.go
+++ yomawari/kernel-memory/core/documentstorage/simplefilestorage.go
@@ -4,47 +4,76 @@ import (
 	"context"
 	"io"
 
+	"github.com/futugyou/yomawari/kernel-memory/abstractions/constant"
 	"github.com/futugyou/yomawari/kernel-memory/abstractions/documentstorage"
 	"github.com/futugyou/yomawari/kernel-memory/abstractions/models"
+	"github.com/futugyou/yomawari/kernel-memory/abstractions/pipeline"
+	"github.com/futugyou/yomawari/kernel-memory/core/filesystem"
 )
 
 type SimpleFileStorage struct {
-	// TODO: implement
+	fileSystem filesystem.IFileSystem
+}
+
+func NewSimpleFileStorage(config SimpleFileStorageConfig, mimeTypeDetection pipeline.IMimeTypeDetection) *SimpleFileStorage {
+	var fileSystem filesystem.IFileSystem
+	if config.StorageType == filesystem.Volatile {
+		fileSystem = filesystem.NewVolatileFileSystem(mimeTypeDetection)
+	} else {
+		fileSystem = filesystem.NewDiskFileSystem(config.Directory, mimeTypeDetection)
+	}
+	return &SimpleFileStorage{
+		fileSystem: fileSystem,
+	}
 }
 
 // CreateDocumentDirectory implements documentstorage.IDocumentStorage.
 func (s *SimpleFileStorage) CreateDocumentDirectory(ctx context.Context, index string, documentId string) error {
-	panic("unimplemented")
+	return s.fileSystem.CreateDirectory(ctx, index, documentId)
 }
 
 // CreateIndexDirectory implements documentstorage.IDocumentStorage.
 func (s *SimpleFileStorage) CreateIndexDirectory(ctx context.Context, index string) error {
-	panic("unimplemented")
+	return s.fileSystem.CreateVolume(ctx, index)
 }
 
 // DeleteDocumentDirectory implements documentstorage.IDocumentStorage.
 func (s *SimpleFileStorage) DeleteDocumentDirectory(ctx context.Context, index string, documentId string) error {
-	panic("unimplemented")
+	return s.fileSystem.DeleteDirectory(ctx, index, documentId)
 }
 
 // DeleteIndexDirectory implements documentstorage.IDocumentStorage.
 func (s *SimpleFileStorage) DeleteIndexDirectory(ctx context.Context, index string) error {
-	panic("unimplemented")
+	return s.fileSystem.DeleteVolume(ctx, index)
 }
 
 // EmptyDocumentDirectory implements documentstorage.IDocumentStorage.
 func (s *SimpleFileStorage) EmptyDocumentDirectory(ctx context.Context, index string, documentId string) error {
-	panic("unimplemented")
+	files, err := s.fileSystem.GetAllFileNames(ctx, index, documentId)
+	if err != nil {
+		return err
+	}
+	for _, fileName := range files {
+		if fileName == constant.PipelineStatusFilename {
+			continue
+		}
+		s.fileSystem.DeleteFile(ctx, index, documentId, fileName)
+	}
+	return nil
 }
 
 // ReadFile implements documentstorage.IDocumentStorage.
 func (s *SimpleFileStorage) ReadFile(ctx context.Context, index string, documentId string, filename string, logErrIfNotFound bool) (*models.StreamableFileContent, error) {
-	panic("unimplemented")
+	return s.fileSystem.ReadFileInfo(ctx, index, documentId, filename)
 }
 
 // WriteFile implements documentstorage.IDocumentStorage.
 func (s *SimpleFileStorage) WriteFile(ctx context.Context, index string, documentId string, filename string, streamContent io.ReadCloser) error {
-	panic("unimplemented")
+	err := s.fileSystem.CreateDirectory(ctx, index, documentId)
+	if err != nil {
+		return err
+	}
+	return s.fileSystem.WriteFile(ctx, index, documentId, filename, streamContent)
 }
 
 var _ documentstorage.IDocumentStorage = (*SimpleFileStorage)(nil)
