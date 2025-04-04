9222b452388e38bcbbc6eef33f8704aef9b1a958
futugyou <17871902+futugyou@users.noreply.github.com>
2025-04-04 14:57:59 +0000

    Added StdioClientSessionTransport and StdioClientTransport

---
+++ yomawari/mcp/protocol/transport/stdioclientsessiontransport.go
@@ -0,0 +1,62 @@
+package transport
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"syscall"
+	"time"
+
+	"github.com/futugyou/yomawari/mcp/logging"
+	"github.com/futugyou/yomawari/mcp/protocol/messages"
+)
+
+type StdioClientSessionTransport struct {
+	*StreamClientSessionTransport
+	options *StdioClientTransportOptions
+	cmd     *exec.Cmd
+}
+
+func NewStdioClientSessionTransport(options *StdioClientTransportOptions, cmd *exec.Cmd, endpointName string, logger logging.Logger) *StdioClientSessionTransport {
+	return &StdioClientSessionTransport{
+		StreamClientSessionTransport: NewStreamClientSessionTransport(cmd.Stdout, cmd.Stdin, endpointName, logger),
+		options:                      options,
+		cmd:                          cmd,
+	}
+}
+
+func (t *StdioClientSessionTransport) SendMessage(ctx context.Context, message messages.IJsonRpcMessage) error {
+	if t.cmd.ProcessState != nil && t.cmd.ProcessState.Exited() {
+		t.logger.TransportNotConnected(t.EndpointName)
+		return fmt.Errorf("transport is not connected")
+	}
+
+	if err := t.cmd.Process.Signal(syscall.Signal(0)); err != nil {
+		t.logger.TransportNotConnected(t.EndpointName)
+		return fmt.Errorf("transport is not connected: %v", err)
+	}
+
+	return t.StreamClientSessionTransport.SendMessage(ctx, message)
+}
+
+func (t *StdioClientSessionTransport) Close() error {
+	if t.cmd == nil {
+		return nil
+	}
+
+	done := make(chan error, 1)
+	go func() {
+		done <- t.cmd.Wait()
+	}()
+
+	select {
+	case <-time.After(t.options.ShutdownTimeout):
+		killProcessTree(t.cmd.Process.Pid)
+	case err := <-done:
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+
+	return t.StreamClientSessionTransport.Close()
+}
