bee9a03d0a48f96dfd8fe9ebaa13cd378c943f7c
futugyou <17871902+futugyou@users.noreply.github.com>
2025-04-10 15:32:24 +0000

    Added DelegatingMcpServerTool

---
+++ yomawari/mcp/server/delegatingmcpservertool.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"context"
+
+	"github.com/futugyou/yomawari/mcp/protocol/types"
+)
+
+var _ IMcpServerTool = (*DelegatingMcpServerTool)(nil)
+
+type DelegatingMcpServerTool struct {
+	innerTool IMcpServerTool
+}
+
+func NewDelegatingMcpServerTool(innerTool IMcpServerTool) *DelegatingMcpServerTool {
+	return &DelegatingMcpServerTool{
+		innerTool: innerTool,
+	}
+}
+
+// GetName implements IMcpServerTool.
+func (d *DelegatingMcpServerTool) GetName() string {
+	return d.innerTool.GetName()
+}
+
+// GetProtocolTool implements IMcpServerTool.
+func (d *DelegatingMcpServerTool) GetProtocolTool() *types.Tool {
+	return d.innerTool.GetProtocolTool()
+}
+
+// Invoke implements IMcpServerTool.
+func (d *DelegatingMcpServerTool) Invoke(ctx context.Context, request RequestContext[*types.CallToolRequestParams]) (*types.CallToolResult, error) {
+	return d.innerTool.Invoke(ctx, request)
+}
