de61e6921fce2509ff0babc95f89d6f8f99ca423
futugyou <17871902+futugyou@users.noreply.github.com>
2025-04-11 16:24:08 +0000

    Updated  IMcpClient

--- yomawari/mcp/client/imcpclient.go
+++ yomawari/mcp/client/imcpclient.go
@@ -2,7 +2,9 @@ package client
 
 import (
 	"context"
+	"net/url"
 
+	"github.com/futugyou/yomawari/core/logger"
 	"github.com/futugyou/yomawari/mcp"
 	"github.com/futugyou/yomawari/mcp/protocol/types"
 	"github.com/futugyou/yomawari/mcp/server"
@@ -17,4 +19,20 @@ type IMcpClient interface {
 	ListTools(ctx context.Context) ([]McpClientTool, error)
 	CallTool(ctx context.Context, toolName string, arguments map[string]interface{}) (*types.CallToolResult, error)
 	GetPrompt(ctx context.Context, name string, arguments map[string]interface{}) (*types.GetPromptResult, error)
+	EnumerateTools(ctx context.Context) (<-chan McpClientTool, <-chan error)
+	ListPrompts(ctx context.Context, client IMcpClient) ([]McpClientPrompt, error)
+	EnumeratePrompts(ctx context.Context, client IMcpClient) (<-chan McpClientPrompt, <-chan error)
+	ListResourceTemplates(ctx context.Context, client IMcpClient) ([]types.ResourceTemplate, error)
+	EnumerateResourceTemplates(ctx context.Context, client IMcpClient) (<-chan types.ResourceTemplate, <-chan error)
+	ListResources(ctx context.Context, client IMcpClient) ([]types.Resource, error)
+	EnumerateResources(ctx context.Context, client IMcpClient) (<-chan types.Resource, <-chan error)
+	ReadResource(ctx context.Context, uri string) (*types.ReadResourceResult, error)
+	ReadResourceWithUri(ctx context.Context, uri url.URL) (*types.ReadResourceResult, error)
+	Complete(ctx context.Context, reference types.Reference, argumentName string, argumentValue string) (*types.CompleteResult, error)
+	SubscribeToResource(ctx context.Context, uri string) error
+	SubscribeToResourceWithUri(ctx context.Context, uri url.URL) error
+	UnsubscribeFromResource(ctx context.Context, uri string) error
+	UnsubscribeFromResourceWithUri(ctx context.Context, uri url.URL) error
+	SetLoggingLevel(ctx context.Context, level types.LoggingLevel) error
+	SetLoggingLevelWithLogLevel(ctx context.Context, level logger.LogLevel) error
 }
