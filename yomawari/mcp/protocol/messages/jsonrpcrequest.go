b2c2df3131ade85e9e31d1557ec8972b9fca88b5
futugyou <17871902+futugyou@users.noreply.github.com>
2025-04-14 09:18:20 +0000

    Updated JsonRpcRequest impletion TextMapCarrier interface

--- yomawari/mcp/protocol/messages/jsonrpcrequest.go
+++ yomawari/mcp/protocol/messages/jsonrpcrequest.go
@@ -1,5 +1,7 @@
 package messages
 
+import "fmt"
+
 type JsonRpcRequest struct {
 	JsonRpc string     `json:"jsonrpc"`
 	Method  string     `json:"method"`
@@ -15,6 +17,46 @@ func (j *JsonRpcRequest) GetId() *RequestId {
 // GetJsonRpc implements IJsonRpcMessageWithId.
 func (j *JsonRpcRequest) GetJsonRpc() string {
 	return "2.0"
+}
+
+func (m *JsonRpcRequest) Get(key string) string {
+	if m.Params == nil {
+		return ""
+	}
+
+	if mp, ok := m.Params.(map[string]interface{}); ok {
+		if val, exists := mp[key]; exists {
+			return fmt.Sprintf("%v", val)
+		}
+	}
+	return ""
+}
+
+func (m *JsonRpcRequest) Set(key, value string) {
+	var mp map[string]interface{}
+
+	if m.Params != nil {
+		if existingMap, ok := m.Params.(map[string]interface{}); ok {
+			mp = existingMap
+		}
+	}
+
+	if mp == nil {
+		mp = make(map[string]interface{})
+	}
+
+	mp[key] = value
+	m.Params = mp
+}
+
+func (m *JsonRpcRequest) Keys() []string {
+	var keys []string
+	if mp, ok := m.Params.(map[string]interface{}); ok {
+		for k := range mp {
+			keys = append(keys, k)
+		}
+	}
+	return keys
 }
 
 var _ IJsonRpcMessageWithId = (*JsonRpcRequest)(nil)
