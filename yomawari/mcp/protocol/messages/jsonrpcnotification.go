817fed3486410fa2c3f9e51a579a53b40c677a2d
futugyou <17871902+futugyou@users.noreply.github.com>
2025-04-14 09:18:11 +0000

    Updated JsonRpcNotification impletion TextMapCarrier interface

--- yomawari/mcp/protocol/messages/jsonrpcnotification.go
+++ yomawari/mcp/protocol/messages/jsonrpcnotification.go
@@ -1,5 +1,7 @@
 package messages
 
+import "fmt"
+
 type JsonRpcNotification struct {
 	JsonRpc string `json:"jsonrpc"`
 	Method  string `json:"method"`
@@ -12,6 +14,46 @@ func NewJsonRpcNotification(method string, params any) *JsonRpcNotification {
 		Method:  method,
 		Params:  params,
 	}
+}
+
+func (m *JsonRpcNotification) Get(key string) string {
+	if m.Params == nil {
+		return ""
+	}
+
+	if mp, ok := m.Params.(map[string]interface{}); ok {
+		if val, exists := mp[key]; exists {
+			return fmt.Sprintf("%v", val)
+		}
+	}
+	return ""
+}
+
+func (m *JsonRpcNotification) Set(key, value string) {
+	var mp map[string]interface{}
+
+	if m.Params != nil {
+		if existingMap, ok := m.Params.(map[string]interface{}); ok {
+			mp = existingMap
+		}
+	}
+
+	if mp == nil {
+		mp = make(map[string]interface{})
+	}
+
+	mp[key] = value
+	m.Params = mp
+}
+
+func (m *JsonRpcNotification) Keys() []string {
+	var keys []string
+	if mp, ok := m.Params.(map[string]interface{}); ok {
+		for k := range mp {
+			keys = append(keys, k)
+		}
+	}
+	return keys
 }
 
 // GetJsonRpc implements IJsonRpcMessage.
