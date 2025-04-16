884c8856d724932bdc2267c1ceddde2321ff7c70
futugyou <17871902+futugyou@users.noreply.github.com>
2025-04-16 13:53:11 +0000

    Added SamplingChatClient

---
+++ yomawari/mcp/server/samplingchatclient.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"context"
+
+	"github.com/futugyou/yomawari/extensions-ai/abstractions/chatcompletion"
+)
+
+var _ chatcompletion.IChatClient = (*SamplingChatClient)(nil)
+
+type SamplingChatClient struct {
+	server IMcpServer
+}
+
+func NewSamplingChatClient(server IMcpServer) *SamplingChatClient {
+	return &SamplingChatClient{server: server}
+}
+
+// GetResponse implements chatcompletion.IChatClient.
+func (s *SamplingChatClient) GetResponse(ctx context.Context, chatMessages []chatcompletion.ChatMessage, options *chatcompletion.ChatOptions) (*chatcompletion.ChatResponse, error) {
+	return s.server.RequestSamplingWithChatMessage(ctx, chatMessages, options)
+}
+
+// GetStreamingResponse implements chatcompletion.IChatClient.
+func (s *SamplingChatClient) GetStreamingResponse(ctx context.Context, chatMessages []chatcompletion.ChatMessage, options *chatcompletion.ChatOptions) <-chan chatcompletion.ChatStreamingResponse {
+	response, err := s.GetResponse(ctx, chatMessages, options)
+	streamResp := make(chan chatcompletion.ChatStreamingResponse)
+	if err != nil {
+		streamResp <- chatcompletion.ChatStreamingResponse{
+			Update: nil,
+			Err:    err,
+		}
+		close(streamResp)
+		return streamResp
+	}
+	updates := response.ToChatResponseUpdates()
+	go func() {
+		defer close(streamResp)
+		for _, item := range updates {
+			streamResp <- chatcompletion.ChatStreamingResponse{
+				Update: &item,
+				Err:    nil,
+			}
+		}
+	}()
+	return streamResp
+}
