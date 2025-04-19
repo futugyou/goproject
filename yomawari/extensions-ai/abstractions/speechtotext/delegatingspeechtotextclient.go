c9aa0cd4c2edc1b2fc729e4a8e80fb83d329981c
futugyou <17871902+futugyou@users.noreply.github.com>
2025-04-19 13:19:48 +0000

    Added DelegatingSpeechToTextClient

---
+++ yomawari/extensions-ai/abstractions/speechtotext/delegatingspeechtotextclient.go
@@ -0,0 +1,34 @@
+package speechtotext
+
+import (
+	"context"
+	"io"
+
+	"github.com/futugyou/yomawari/extensions-ai/abstractions/contents"
+)
+
+var _ ISpeechToTextClient = (*DelegatingSpeechToTextClient)(nil)
+
+type DelegatingSpeechToTextClient struct {
+	InnerClient ISpeechToTextClient
+}
+
+// GetStreamingText implements ISpeechToTextClient.
+func (d *DelegatingSpeechToTextClient) GetStreamingText(ctx context.Context, audioSpeechStream io.ReadCloser, options *SpeechToTextOptions) (<-chan SpeechToTextResponse, <-chan error) {
+	return d.InnerClient.GetStreamingText(ctx, audioSpeechStream, options)
+}
+
+// GetStreamingTextWithDataConten implements ISpeechToTextClient.
+func (d *DelegatingSpeechToTextClient) GetStreamingTextWithDataConten(ctx context.Context, audioSpeechContent contents.DataContent, options *SpeechToTextOptions) (<-chan SpeechToTextResponse, <-chan error) {
+	return d.InnerClient.GetStreamingTextWithDataConten(ctx, audioSpeechContent, options)
+}
+
+// GetText implements ISpeechToTextClient.
+func (d *DelegatingSpeechToTextClient) GetText(ctx context.Context, audioSpeechStream io.ReadCloser, options *SpeechToTextOptions) (*SpeechToTextResponse, error) {
+	return d.InnerClient.GetText(ctx, audioSpeechStream, options)
+}
+
+// GetTextWithDataContent implements ISpeechToTextClient.
+func (d *DelegatingSpeechToTextClient) GetTextWithDataContent(ctx context.Context, audioSpeechContent contents.DataContent, options *SpeechToTextOptions) (*SpeechToTextResponse, error) {
+	return d.InnerClient.GetTextWithDataContent(ctx, audioSpeechContent, options)
+}
