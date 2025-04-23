8de6299e4d315373bd51374d6f2b9f4a92c59302
futugyou <17871902+futugyou@users.noreply.github.com>
2025-04-23 15:20:03 +0000

    Updated StreamableHttpServerTransport added HandleGetRequest/HandlePostRequest

--- yomawari/mcp/protocol/transport/streamablehttpservertransport.go
+++ yomawari/mcp/protocol/transport/streamablehttpservertransport.go
@@ -2,6 +2,9 @@ package transport
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"sync/atomic"
 
 	"github.com/futugyou/yomawari/mcp/protocol/messages"
 )
@@ -13,16 +16,17 @@ type StreamableHttpServerTransport struct {
 	incomingChannel   chan messages.IJsonRpcMessage
 	ctx               context.Context
 	cancelFunc        context.CancelFunc
-	getRequestStarted int
+	getRequestStarted int32
 }
 
 func NewStreamableHttpServerTransport() *StreamableHttpServerTransport {
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	return &StreamableHttpServerTransport{
-		sseWriter:       NewSseWriter(""),
-		incomingChannel: make(chan messages.IJsonRpcMessage),
-		ctx:             ctx,
-		cancelFunc:      cancelFunc,
+		sseWriter:         NewSseWriter(""),
+		incomingChannel:   make(chan messages.IJsonRpcMessage),
+		ctx:               ctx,
+		cancelFunc:        cancelFunc,
+		getRequestStarted: 0,
 	}
 }
 
@@ -43,4 +47,41 @@ func (s *StreamableHttpServerTransport) MessageReader() <-chan messages.IJsonRpc
 // SendMessage implements ITransport.
 func (s *StreamableHttpServerTransport) SendMessage(ctx context.Context, message messages.IJsonRpcMessage) error {
 	return s.sseWriter.SendMessage(ctx, message)
+}
+
+func (s *StreamableHttpServerTransport) HandleGetRequest(ctx context.Context, sseResponseStream io.Writer) error {
+	if atomic.SwapInt32(&s.getRequestStarted, 1) != 0 {
+		return fmt.Errorf("session resumption is not yet supported. Please start a new session")
+	}
+	ctx, _ = mergeContexts(s.ctx, ctx)
+	resultCh := s.sseWriter.WriteAll(ctx, sseResponseStream)
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case err := <-resultCh:
+		return err
+	}
+}
+
+func (s *StreamableHttpServerTransport) HandlePostRequest(ctx context.Context, httpBodies *DuplexPipe) (bool, error) {
+	ctx, _ = mergeContexts(s.ctx, ctx)
+	postTransport := NewStreamableHttpPostTransport(s.incomingChannel, httpBodies)
+	return postTransport.Run(ctx)
+}
+
+func mergeContexts(ctx1, ctx2 context.Context) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		select {
+		case <-ctx1.Done():
+			cancel()
+		case <-ctx2.Done():
+			cancel()
+		case <-ctx.Done():
+		}
+	}()
+
+	return ctx, cancel
 }
