c85e475a22aa3f6380fa4f8dfbe99843179c5a3b
futugyou <17871902+futugyou@users.noreply.github.com>
2025-04-23 13:43:08 +0000

    Added DuplexPipe

---
+++ yomawari/mcp/protocol/transport/duplexpipe.go
@@ -0,0 +1,26 @@
+package transport
+
+import (
+	"io"
+	"net/http"
+)
+
+type DuplexPipe struct {
+	Input  io.Reader
+	Output io.Writer
+}
+
+func NewInMemoryDuplexPipe() (*DuplexPipe, io.Closer) {
+	pr, pw := io.Pipe()
+	return &DuplexPipe{
+		Input:  pr,
+		Output: pw,
+	}, pw
+}
+
+func NewHttpDuplexPipe(r *http.Request, w http.ResponseWriter) *DuplexPipe {
+	return &DuplexPipe{
+		Input:  r.Body,
+		Output: w,
+	}
+}
