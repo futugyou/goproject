d2b6b28aa55fff9b862528dec99b8e6dd49ee526
futugyou <17871902+futugyou@users.noreply.github.com>
2025-04-29 14:50:03 +0000

    Added MemoryQueryResult

---
+++ yomawari/semantic-kernel/abstractions/memory/memory_query_result.go
@@ -0,0 +1,15 @@
+package memory
+
+type MemoryQueryResult struct {
+	Metadata  MemoryRecordMetadata
+	Relevance float64
+	Embedding []float32
+}
+
+func FromMemoryRecord(record MemoryRecord, relevance float64) MemoryQueryResult {
+	return MemoryQueryResult{
+		Metadata:  record.Metadata,
+		Relevance: relevance,
+		Embedding: record.Embedding,
+	}
+}
