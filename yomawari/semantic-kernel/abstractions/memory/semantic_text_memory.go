fccbb68fe516447a82ccf8c444a4cad62ee2f3fb
futugyou <17871902+futugyou@users.noreply.github.com>
2025-04-30 15:10:35 +0000

    Changed return type of Get method in ISemanticTextMemory from *MemoryRecord to *MemoryQueryResult

--- yomawari/semantic-kernel/abstractions/memory/semantic_text_memory.go
+++ yomawari/semantic-kernel/abstractions/memory/semantic_text_memory.go
@@ -9,7 +9,7 @@ import (
 type ISemanticTextMemory interface {
 	SaveInformation(ctx context.Context, collection string, text string, id string, description *string, additionalMetadata *string, kernel *abstractions.Kernel) (string, error)
 	SaveReference(ctx context.Context, collection string, text string, externalId string, externalSourceName string, description *string, additionalMetadata *string, kernel *abstractions.Kernel) (string, error)
-	Get(ctx context.Context, collection string, key string, withEmbedding bool, kernel *abstractions.Kernel) (*MemoryRecord, error)
+	Get(ctx context.Context, collection string, key string, withEmbedding bool, kernel *abstractions.Kernel) (*MemoryQueryResult, error)
 	Remove(ctx context.Context, collection string, key string, kernel *abstractions.Kernel) error
 	// limit = 1, minRelevanceScore = 0.7,withEmbeddings = false,
 	Search(ctx context.Context, collection string, query string, limit int, minRelevanceScore float64, withEmbeddings bool, kernel *abstractions.Kernel) (<-chan MemoryQueryResult, <-chan error)
@@ -22,7 +22,7 @@ type NullMemory struct {
 }
 
 // Get implements ISemanticTextMemory.
-func (n *NullMemory) Get(ctx context.Context, collection string, key string, withEmbedding bool, kernel *abstractions.Kernel) (*MemoryRecord, error) {
+func (n *NullMemory) Get(ctx context.Context, collection string, key string, withEmbedding bool, kernel *abstractions.Kernel) (*MemoryQueryResult, error) {
 	return nil, nil
 }
 
