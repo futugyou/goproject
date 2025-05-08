209d96b3167e5a5f18c35e999e8e9af3ddf5e4f0
futugyou <17871902+futugyou@users.noreply.github.com>
2025-05-08 14:30:55 +0000

    Added FunctionResultContent

---
+++ yomawari/semantic-kernel/abstractions/contents/function_result_content.go
@@ -0,0 +1,27 @@
+package contents
+
+type FunctionResultContent struct {
+	MimeType     string         `json:"mimeType"`
+	ModelId      string         `json:"modelId"`
+	Metadata     map[string]any `json:"metadata"`
+	CallId       string         `json:"callId"`
+	PluginName   string         `json:"pluginName"`
+	FunctionName string         `json:"functionName"`
+	Result       any            `json:"result"`
+}
+
+func (FunctionResultContent) Type() string {
+	return "functionResult"
+}
+
+func (f FunctionResultContent) ToChatMessage() ChatMessageContent {
+	return ChatMessageContent{
+		MimeType: f.MimeType,
+		ModelId:  f.ModelId,
+		Metadata: f.Metadata,
+		Role:     AuthorRoleTool,
+		Items: ChatMessageContentItemCollection{
+			Items: []KernelContent{f},
+		},
+	}
+}
