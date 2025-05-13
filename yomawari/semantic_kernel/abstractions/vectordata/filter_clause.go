8e948795d7ce1a93c7c76e1d37f9aab0d3ac513c
futugyou <17871902+futugyou@users.noreply.github.com>
2025-05-13 13:34:15 +0000

    Added TextSearchFilter

---
+++ yomawari/semantic_kernel/abstractions/vectordata/filter_clause.go
@@ -0,0 +1,19 @@
+package vectordata
+
+type FilterClause interface {
+	IsFilterClause()
+}
+
+type AnyTagEqualToFilterClause struct {
+	FieldName string
+	Value     string
+}
+
+func (f AnyTagEqualToFilterClause) IsFilterClause() {}
+
+type EqualToFilterClause struct {
+	FieldName string
+	Value     any
+}
+
+func (f EqualToFilterClause) IsFilterClause() {}
