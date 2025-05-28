45379ee4ca6ecb124ee5a0d914be11dd7cdb565f
futugyou <17871902+futugyou@users.noreply.github.com>
2025-05-28 14:04:32 +0000

    Updated Kernel added some property motheds

--- yomawari/semantic_kernel/abstractions/kernel.go
+++ yomawari/semantic_kernel/abstractions/kernel.go
@@ -1,6 +1,10 @@
 package abstractions
 
-import "github.com/futugyou/yomawari/core"
+import (
+	"sync"
+
+	"github.com/futugyou/yomawari/core"
+)
 
 type Kernel struct {
 	data                          map[string]any
@@ -10,6 +14,8 @@ type Kernel struct {
 	autoFunctionInvocationFilters []IAutoFunctionInvocationFilter
 	Services                      core.IServiceProvider
 }
+
+var once sync.Once
 
 func NewKernel(services core.IServiceProvider, plugins *KernelPluginCollection) *Kernel {
 	if services == nil {
@@ -47,4 +53,49 @@ func NewKernel(services core.IServiceProvider, plugins *KernelPluginCollection) 
 		kernel.autoFunctionInvocationFilters = autoFunctionInvocationFilters
 	}
 	return kernel
+}
+
+func (k *Kernel) GetFunctionInvocationFilters() []IFunctionInvocationFilter {
+	once.Do(func() {
+		if k.functionInvocationFilters == nil {
+			k.functionInvocationFilters = []IFunctionInvocationFilter{}
+		}
+	})
+	return k.functionInvocationFilters
+}
+
+func (k *Kernel) GetPromptRenderFilters() []IPromptRenderFilter {
+	once.Do(func() {
+		if k.promptRenderFilters == nil {
+			k.promptRenderFilters = []IPromptRenderFilter{}
+		}
+	})
+	return k.promptRenderFilters
+}
+
+func (k *Kernel) GetAutoFunctionInvocationFilters() []IAutoFunctionInvocationFilter {
+	once.Do(func() {
+		if k.autoFunctionInvocationFilters == nil {
+			k.autoFunctionInvocationFilters = []IAutoFunctionInvocationFilter{}
+		}
+	})
+	return k.autoFunctionInvocationFilters
+}
+
+func (k *Kernel) GetPlugins() KernelPluginCollection {
+	once.Do(func() {
+		if k.plugins.Dictionary == nil {
+			k.plugins.Dictionary = core.NewDictionary[string, KernelPlugin]()
+		}
+	})
+	return k.plugins
+}
+
+func (k *Kernel) GetDatas() map[string]any {
+	once.Do(func() {
+		if k.data == nil {
+			k.data = map[string]any{}
+		}
+	})
+	return k.data
 }
