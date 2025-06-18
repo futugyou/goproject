605d10cfb9a3cb7fa719b6b3aefeaef2b669ee95
futugyou <17871902+futugyou@users.noreply.github.com>
2025-06-18 14:03:58 +0000

    Updated redis lua script

--- extensions/redis.go
+++ extensions/redis.go
@@ -2,10 +2,11 @@ package extensions
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"time"
 
-	"github.com/mitchellh/mapstructure"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -31,6 +32,7 @@ func RedisScanHashAll[T any](ctx context.Context, client *redis.Client, prefix s
 }
 
 var HashAllLueScript = redis.NewScript(`
+	local cjson = require "cjson"
 	local keys = redis.call("SCAN", 0, "MATCH", ARGV[1], "COUNT", ARGV[2])
 	local results = {}
 	for _, key in ipairs(keys[2]) do
@@ -39,12 +41,14 @@ var HashAllLueScript = redis.NewScript(`
 		for i = 1, #hash, 2 do
 			result[hash[i]] = hash[i+1]
 		end
-		table.insert(results, result)
+		table.insert(results, cjson.encode(result))  -- 👈 Key: Convert to JSON string
 	end
 	return results
 `)
 
 var HashAllLueScript2 = redis.NewScript(`
+	local cjson = require "cjson"
+
 	local results = {}
 	local missing_keys = {}
 
@@ -67,7 +71,7 @@ var HashAllLueScript2 = redis.NewScript(`
 		for i = 1, #hash, 2 do
 			result[hash[i]] = hash[i+1]
 		end
-		table.insert(results, result)
+		table.insert(results, cjson.encode(result)) -- 👈 Convert to JSON string
 	end
 
 	return {results, missing_keys}
@@ -80,13 +84,20 @@ func RedisListHashWithLua[T any](ctx context.Context, client *redis.Client, patt
 	if err != nil {
 		return nil, err
 	}
+	rawItems, ok := res.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("unexpected Redis result type: %T", res)
+	}
 
 	var datas []T
-	for _, item := range res.([]interface{}) {
-		data := item.(map[string]interface{})
+	for _, item := range rawItems {
+		jsonStr, ok := item.(string)
+		if !ok {
+			return nil, fmt.Errorf("unexpected item type: %T", item)
+		}
 
 		var rv T
-		err := mapstructure.Decode(data, &rv)
+		err := json.Unmarshal([]byte(jsonStr), &rv)
 		if err != nil {
 			return nil, err
 		}
@@ -107,25 +118,44 @@ func RedisListHashWithLua2[T any](ctx context.Context, client *redis.Client, pat
 		return nil, nil, err
 	}
 
-	rawResults := res.([]interface{})[0].([]interface{})
-	rawMissingKeys := res.([]interface{})[1].([]interface{})
+	// First assert that res is []interface{}
+	resList, ok := res.([]interface{})
+	if !ok || len(resList) != 2 {
+		return nil, nil, fmt.Errorf("unexpected result structure from Lua script: %T", res)
+	}
+
+	// Get the result part
+	rawResults, ok := resList[0].([]interface{})
+	if !ok {
+		return nil, nil, fmt.Errorf("unexpected result[0] type: %T", resList[0])
+	}
+
+	rawMissingKeys, ok := resList[1].([]interface{})
+	if !ok {
+		return nil, nil, fmt.Errorf("unexpected result[1] type: %T", resList[1])
+	}
 
 	var datas []T
 	for _, item := range rawResults {
-		data := item.(map[string]interface{})
-
-		var rv T
-		err := mapstructure.Decode(data, &rv)
-		if err != nil {
-			return nil, nil, err
+		jsonStr, ok := item.(string)
+		if !ok {
+			return nil, nil, fmt.Errorf("unexpected item type: %T", item)
 		}
 
+		var rv T
+		if err := json.Unmarshal([]byte(jsonStr), &rv); err != nil {
+			return nil, nil, err
+		}
 		datas = append(datas, rv)
 	}
 
 	var missingKeys []string
-	for _, key := range rawMissingKeys {
-		missingKeys = append(missingKeys, key.(string))
+	for i, key := range rawMissingKeys {
+		strKey, ok := key.(string)
+		if !ok {
+			return nil, nil, fmt.Errorf("unexpected key type at index %d: %T", i, key)
+		}
+		missingKeys = append(missingKeys, strKey)
 	}
 
 	return datas, missingKeys, nil
