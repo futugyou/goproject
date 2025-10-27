29d7bba3aa1774313cc2a7f100d3b86ca5c5ec36
futugyou <17871902+futugyou@users.noreply.github.com>
2025-10-27 17:10:36 +0000

    Updated repository in domaincore

--- domaincore/domain/repository.go
+++ domaincore/domain/repository.go
@@ -4,9 +4,35 @@ import (
 	"context"
 )
 
+const DATA_NOT_FOUND_MESSAGE string = "data not found"
+
 type Repository[Aggregate AggregateRoot] interface {
-	Get(ctx context.Context, id string) (*Aggregate, error)
+	FindByID(ctx context.Context, id string) (*Aggregate, error)
 	Delete(ctx context.Context, id string) error
 	Update(ctx context.Context, aggregate Aggregate) error
 	Insert(ctx context.Context, aggregate Aggregate) error
+	Find(ctx context.Context, options *QueryOptions) ([]Aggregate, error)
+}
+
+type QueryOptions struct {
+	OrderBy map[string]int
+	Page    int
+	Limit   int
+	Filters map[string]any
+}
+
+func NewQueryOptions(page *int, limit *int, orderBy map[string]int, filters map[string]any) *QueryOptions {
+	opts := &QueryOptions{
+		OrderBy: orderBy,
+		Page:    1,
+		Limit:   100,
+		Filters: filters,
+	}
+	if page != nil && *page > 1 {
+		opts.Page = *page
+	}
+	if limit != nil && *limit > 1 && *limit < 100 {
+		opts.Limit = *limit
+	}
+	return opts
 }
