760b78c16f2f8aba376b814b49c43569f98d71f5
futugyou <17871902+futugyou@users.noreply.github.com>
2025-11-05 03:27:52 +0000

    Added PlatformAssembler to convert domain.Platform to PlatformDetailView

---
+++ infr-project/microservices/platformservice/assembler/platform.go
@@ -0,0 +1,43 @@
+package assembler
+
+import (
+	"github.com/futugyou/platformservice/domain"
+	"github.com/futugyou/platformservice/viewmodel"
+)
+
+type PlatformAssembler struct{}
+
+func (a *PlatformAssembler) ToPlatformDetailView(d *domain.Platform) *viewmodel.PlatformDetailView {
+	return &viewmodel.PlatformDetailView{
+		ID:       d.ID,
+		Name:     d.Name,
+		Url:      d.Url,
+		Provider: d.Provider.String(),
+		Properties: func() []viewmodel.Property {
+			res := []viewmodel.Property{}
+			for k, v := range d.Properties {
+				res = append(res, viewmodel.Property{
+					Key:   k,
+					Value: v.Value,
+				})
+			}
+			return res
+		}(),
+		Tags: d.Tags,
+		Secrets: func() []viewmodel.Secret {
+			res := []viewmodel.Secret{}
+			for _, v := range d.Secrets {
+				res = append(res, viewmodel.Secret{
+					Key:       v.Key,
+					VaultID:   v.Value,
+					VaultKey:  v.VaultKey,
+					MaskValue: v.VaultMaskValue,
+				})
+			}
+			return res
+		}(),
+		Activate:  d.Activate,
+		IsDeleted: d.IsDeleted,
+		Projects:  []viewmodel.PlatformProject{},
+	}
+}
