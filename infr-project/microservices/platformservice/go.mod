da98d145c1b2d642c251e21bcbce37c57d12aefd
futugyou <17871902+futugyou@users.noreply.github.com>
2025-11-17 07:32:59 +0000

    Added gin server to platfrom service

--- infr-project/microservices/platformservice/go.mod
+++ infr-project/microservices/platformservice/go.mod
@@ -6,53 +6,96 @@ require (
 	github.com/futugyou/circleci v1.0.0
 	github.com/futugyou/domaincore v1.0.0
 	github.com/futugyou/domaincore/mongoimpl v1.0.0
+	github.com/futugyou/domaincore/qstashdispatcherimpl v1.0.0
 	github.com/futugyou/extensions v1.0.0
 	github.com/futugyou/gofile v1.0.0
 	github.com/futugyou/screenshot v1.0.0
 	github.com/futugyou/vercel v1.0.0
+	github.com/gin-gonic/gin v1.11.0
 	github.com/google/go-github/v66 v66.0.0
 	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+	github.com/swaggo/files v1.0.1
+	github.com/swaggo/gin-swagger v1.6.1
+	github.com/swaggo/swag v1.16.6
 	go.mongodb.org/mongo-driver v1.17.6
 	golang.org/x/oauth2 v0.32.0
 )
 
 require (
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/purell v1.2.1 // indirect
+	github.com/bytedance/gopkg v0.1.3 // indirect
+	github.com/bytedance/sonic v1.14.2 // indirect
+	github.com/bytedance/sonic/loader v0.4.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/chidiwilliams/flatbson v0.3.0 // indirect
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/cloudwego/base64x v0.1.6 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.4.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/dlclark/regexp2 v1.11.2 // indirect
-	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/golang/snappy v0.0.4 // indirect
-	github.com/google/go-cmp v0.7.0 // indirect
+	github.com/dlclark/regexp2 v1.11.5 // indirect
+	github.com/futugyou/qstash v1.0.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.11 // indirect
+	github.com/gin-contrib/sse v1.1.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/spec v0.20.4 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.27.0 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/goccy/go-yaml v1.18.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/klauspost/compress v1.16.7 // indirect
-	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.18.1 // indirect
+	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.4 // indirect
 	github.com/lestrrat-go/httpcc v1.0.1 // indirect
-	github.com/lestrrat-go/httprc v1.0.4 // indirect
+	github.com/lestrrat-go/httprc v1.0.6 // indirect
 	github.com/lestrrat-go/iter v1.0.2 // indirect
 	github.com/lestrrat-go/jwx/v2 v2.0.11 // indirect
 	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/mailru/easyjson v0.9.1 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
-	github.com/redis/go-redis/v9 v9.7.0 // indirect
-	github.com/segmentio/asm v1.2.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/quic-go/qpack v0.5.1 // indirect
+	github.com/quic-go/quic-go v0.56.0 // indirect
+	github.com/redis/go-redis/v9 v9.16.0 // indirect
+	github.com/segmentio/asm v1.2.1 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.3.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
-	golang.org/x/mod v0.19.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.23.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/arch v0.20.0 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
+	golang.org/x/mod v0.27.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/tools v0.36.0 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
 replace (
 	github.com/futugyou/circleci v1.0.0 => ../../../circleci_sdk
 	github.com/futugyou/domaincore v1.0.0 => ../../../domaincore
 	github.com/futugyou/domaincore/mongoimpl v1.0.0 => ../../../domaincore/mongoimpl
+	github.com/futugyou/domaincore/qstashdispatcherimpl v1.0.0 => ../../../domaincore/qstashdispatcherimpl
 	github.com/futugyou/extensions v1.0.0 => ../../../extensions
 	github.com/futugyou/gofile v1.0.0 => ../../../gofile_sdk
+	github.com/futugyou/qstash v1.0.0 => ../../../qstash_sdk
 	github.com/futugyou/screenshot v1.0.0 => ../../../screenshot_sdk
 	github.com/futugyou/vercel v1.0.0 => ../../../vercel_sdk
 )
---
+++ infr-project/microservices/platformservice/routes/v1/route.go
@@ -0,0 +1,480 @@
+package v1
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/futugyou/domaincore/mongoimpl"
+	"github.com/futugyou/domaincore/qstashdispatcherimpl"
+
+	"github.com/futugyou/platformservice/application"
+	"github.com/futugyou/platformservice/infrastructure"
+	"github.com/futugyou/platformservice/options"
+	"github.com/futugyou/platformservice/viewmodel"
+)
+
+type Response struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func ConfigPlatformRoutes(v1 *gin.RouterGroup) {
+	v1.GET("/platform", searchPlatforms)
+	v1.GET("/platform/:id", getPlatform)
+	v1.GET("/platform/:id/provider_projects", getProviderProjectList)
+	v1.POST("/platform/:id/import", importProjectsFromProvider)
+
+	v1.POST("/platform", createPlatform)
+	v1.PUT("/platform/:id", updatePlatform)
+	v1.DELETE("/platform/:id", deletePlatform)
+	v1.POST("/platform/:id/recovery", recoveryPlatform)
+	// platform project
+	v1.POST("/platform/:id/project", createPlatformProject)
+	v1.GET("/platform/:id/project/:project_id", getPlatformProject)
+	v1.PUT("/platform/:id/project/:project_id", updatePlatformProject)
+	v1.DELETE("/platform/:id/project/:project_id", deletePlatformProject)
+}
+
+// @Summary delete platform project
+// @Description delete platform project
+// @Tags Platform
+// @Accept json
+// @Produce json
+// @Param id path string true "Platform ID or Name"
+// @Param project_id path string true "Platform Project ID"
+// @Success 200 {object} Response
+// @Failure 400 {object} Response "Incorrect request parameters"
+// @Failure 500 {object} Response "Internal server error"
+// @Router /v1/platform/{id}/project/{project_id} [delete]
+func deletePlatformProject(c *gin.Context) {
+	ctx := c.Request.Context()
+	service, err := createPlatformService(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	err = service.DeleteProject(ctx, c.Param("id"), c.Param("project_id"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, Response{})
+}
+
+// @Summary update platform project
+// @Description update platform project
+// @Tags Platform
+// @Accept json
+// @Produce json
+// @Param id path string true "Platform ID or Name"
+// @Param project_id path string true "Platform Project ID"
+// @Param request body viewmodel.UpdatePlatformProjectRequest true "Request body"
+// @Success 200 {object} Response
+// @Failure 400 {object} Response "Incorrect request parameters"
+// @Failure 500 {object} Response "Internal server error"
+// @Router /v1/platform/{id}/project/{project_id} [put]
+func updatePlatformProject(c *gin.Context) {
+	ctx := c.Request.Context()
+	service, err := createPlatformService(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	aux := viewmodel.UpdatePlatformProjectRequest{}
+	if err := c.ShouldBind(&aux); err != nil {
+		c.JSON(http.StatusBadRequest, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	err = service.UpsertProject(ctx, c.Param("id"), c.Param("project_id"), aux)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, Response{})
+}
+
+// @Summary create platform project
+// @Description create platform project
+// @Tags Platform
+// @Accept json
+// @Produce json
+// @Param id path string true "Platform ID or Name"
+// @Param request body viewmodel.UpdatePlatformProjectRequest true "Request body"
+// @Success 200 {object} Response
+// @Failure 400 {object} Response "Incorrect request parameters"
+// @Failure 500 {object} Response "Internal server error"
+// @Router /v1/platform/{id}/project [post]
+func createPlatformProject(c *gin.Context) {
+	ctx := c.Request.Context()
+	service, err := createPlatformService(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	aux := viewmodel.UpdatePlatformProjectRequest{}
+	if err := c.ShouldBind(&aux); err != nil {
+		c.JSON(http.StatusBadRequest, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	err = service.UpsertProject(ctx, c.Param("id"), "", aux)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, Response{})
+}
+
+// @Summary recovery platform
+// @Description recovery platform
+// @Tags Platform
+// @Accept json
+// @Produce json
+// @Param id path string true "Platform ID or Name"
+// @Success 200 {object} Response
+// @Failure 400 {object} Response "Incorrect request parameters"
+// @Failure 500 {object} Response "Internal server error"
+// @Router /v1/platform/{id}/recovery [post]
+func recoveryPlatform(c *gin.Context) {
+	ctx := c.Request.Context()
+	service, err := createPlatformService(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	err = service.RecoveryPlatform(ctx, c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, Response{})
+}
+
+// @Summary delete platform
+// @Description delete platform
+// @Tags Platform
+// @Accept json
+// @Produce json
+// @Param id path string true "Platform ID or Name"
+// @Success 200 {object} Response
+// @Failure 400 {object} Response "Incorrect request parameters"
+// @Failure 500 {object} Response "Internal server error"
+// @Router /v1/platform/{id} [delete]
+func deletePlatform(c *gin.Context) {
+	ctx := c.Request.Context()
+	service, err := createPlatformService(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	err = service.DeletePlatform(ctx, c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, Response{})
+}
+
+// @Summary update platform
+// @Description update platform
+// @Tags Platform
+// @Accept json
+// @Produce json
+// @Param id path string true "Platform ID or Name"
+// @Param request body viewmodel.UpdatePlatformRequest true "Request body"
+// @Success 200 {object} Response
+// @Failure 400 {object} Response "Incorrect request parameters"
+// @Failure 500 {object} Response "Internal server error"
+// @Router /v1/platform/{id} [put]
+func updatePlatform(c *gin.Context) {
+	ctx := c.Request.Context()
+	service, err := createPlatformService(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	aux := viewmodel.UpdatePlatformRequest{}
+	if err := c.ShouldBind(&aux); err != nil {
+		c.JSON(http.StatusBadRequest, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	err = service.UpdatePlatform(ctx, c.Param("id"), aux)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, Response{})
+}
+
+// @Summary create platform
+// @Description create platform
+// @Tags Platform
+// @Accept json
+// @Produce json
+// @Param request body viewmodel.CreatePlatformRequest true "Request body"
+// @Success 200 {object} Response
+// @Failure 400 {object} Response "Incorrect request parameters"
+// @Failure 500 {object} Response "Internal server error"
+// @Router /v1/platform [post]
+func createPlatform(c *gin.Context) {
+	ctx := c.Request.Context()
+	service, err := createPlatformService(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	aux := viewmodel.CreatePlatformRequest{}
+	if err := c.ShouldBind(&aux); err != nil {
+		c.JSON(http.StatusBadRequest, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	err = service.CreatePlatform(ctx, aux)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, Response{})
+}
+
+// @Summary search platforms
+// @Description search platforms
+// @Tags Platform
+// @Accept json
+// @Produce json
+// @Param name query string false "name - Fuzzy Search"
+// @Param activate query boolean false "activate"
+// @Param tags query []string false "Tags" collectionFormat(csv)
+// @Param page query int false "Page number" default(1)
+// @Param size query int false "Page size" default(100)
+// @Success 200 {array} viewmodel.PlatformView
+// @Failure 400 {object} Response "Incorrect request parameters"
+// @Failure 500 {object} Response "Internal server error"
+// @Router /v1/platform [get]
+func searchPlatforms(c *gin.Context) {
+	ctx := c.Request.Context()
+	service, err := createPlatformService(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	aux := viewmodel.SearchPlatformsRequest{}
+	if err := c.ShouldBindQuery(&aux); err != nil {
+		c.JSON(http.StatusBadRequest, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	datas, err := service.SearchPlatforms(ctx, aux)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, datas)
+}
+
+// @Summary get platform by id or name
+// @Description get platform by id or name
+// @Tags Platform
+// @Accept json
+// @Produce json
+// @Param id path string true "Platform ID or Name"
+// @Success 200 {object} viewmodel.PlatformDetailView
+// @Failure 400 {object} Response "Incorrect request parameters"
+// @Failure 500 {object} Response "Internal server error"
+// @Router /v1/platform/{id} [get]
+func getPlatform(c *gin.Context) {
+	ctx := c.Request.Context()
+	service, err := createPlatformService(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	datas, err := service.GetPlatform(ctx, c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, datas)
+}
+
+// @Summary get platform provider project list by id or name
+// @Description get platform provider project list by id or name
+// @Tags Platform
+// @Accept json
+// @Produce json
+// @Param id path string true "Platform ID or Name"
+// @Success 200 {array} viewmodel.PlatformProviderProject
+// @Failure 400 {object} Response "Incorrect request parameters"
+// @Failure 500 {object} Response "Internal server error"
+// @Router /v1/platform/{id}/provider_projects [get]
+func getProviderProjectList(c *gin.Context) {
+	ctx := c.Request.Context()
+	service, err := createPlatformService(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	datas, err := service.GetProviderProjectList(ctx, c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, datas)
+}
+
+// @Summary get platform project by id
+// @Description get platform project by id
+// @Tags Platform
+// @Accept json
+// @Produce json
+// @Param id path string true "Platform ID or Name"
+// @Param project_id path string true "Platform Project ID"
+// @Success 200 {object} viewmodel.PlatformProject
+// @Failure 400 {object} Response "Incorrect request parameters"
+// @Failure 500 {object} Response "Internal server error"
+// @Router /v1/platform/{id}/project/{project_id} [get]
+func getPlatformProject(c *gin.Context) {
+	ctx := c.Request.Context()
+	service, err := createPlatformService(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	datas, err := service.GetPlatformProject(ctx, c.Param("id"), c.Param("project_id"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, datas)
+}
+
+// @Summary import platform provider projects by id or name
+// @Description import platform provider projects by id or name
+// @Tags Platform
+// @Accept json
+// @Produce json
+// @Param request body viewmodel.ImportProjectsRequest true "Request body"
+// @Success 200 {object} Response
+// @Failure 400 {object} Response "Incorrect request parameters"
+// @Failure 500 {object} Response "Internal server error"
+// @Router /v1/platform/{id}/import [post]
+func importProjectsFromProvider(c *gin.Context) {
+	ctx := c.Request.Context()
+	service, err := createPlatformService(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	aux := viewmodel.ImportProjectsRequest{}
+	if err := c.ShouldBind(&aux); err != nil {
+		c.JSON(http.StatusBadRequest, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	err = service.ImportProjectsFromProvider(ctx, aux.PlatformID, aux.ProjectIDs)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, Response{})
+}
+
+func createPlatformService(ctx context.Context) (*application.PlatformService, error) {
+	option := options.New()
+	mongoclient, err := mongoimpl.CreateMongoDBClient(ctx, option.MongoDBURL)
+	config := mongoimpl.DBConfig{
+		DBName: option.DBName,
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	repo := infrastructure.NewPlatformRepository(mongoclient, config)
+	unitOfWork, err := mongoimpl.NewMongoUnitOfWork(mongoclient)
+	if err != nil {
+		return nil, err
+	}
+
+	eventPublisher := qstashdispatcherimpl.NewQStashEventDispatcher(option.QstashToken, option.QstashDestination)
+	vaultService := infrastructure.NewVaultService(option)
+	screenshot := infrastructure.NewScreenshot(option)
+
+	return application.NewPlatformService(unitOfWork, repo, eventPublisher, vaultService, screenshot, option), nil
+}
