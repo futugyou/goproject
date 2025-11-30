36874016e71b178705c1158f5932e0fcabb9b930
futugyou <17871902+futugyou@users.noreply.github.com>
2025-11-30 17:13:52 +0000

    Updated auth user

--- identity-server/pkg/domain/user.go
+++ identity-server/pkg/domain/user.go
@@ -12,6 +12,8 @@ type User struct {
 	Password      string
 	Email         string
 	EmailVerified bool
+	Roles         []string
+	Scopes        []string
 }
 
 func NewUser(name, password, email string) *User {
@@ -24,4 +26,70 @@ func NewUser(name, password, email string) *User {
 		Email:         email,
 		EmailVerified: false,
 	}
+}
+
+func (u *User) GrantRole(roles []string) {
+	u.Roles = mergeDeduplication(u.Roles, roles)
+}
+
+func mergeDeduplication[T comparable](a, b []T) []T {
+	seen := make(map[T]struct{})
+	var result []T
+	for _, item := range append(a, b...) {
+		if _, exists := seen[item]; !exists {
+			seen[item] = struct{}{}
+			result = append(result, item)
+		}
+	}
+	return result
+}
+
+func (u *User) RevokeRole(role string) {
+	if role == "" {
+		return
+	}
+
+	for i, s := range u.Roles {
+		if s == role {
+			copy(u.Roles[i:], u.Roles[i+1:])
+			u.Roles = u.Roles[:len(u.Roles)-1]
+			return
+		}
+	}
+}
+
+func (u *User) GrantAuthorization(scopes []string) {
+	u.Scopes = mergeDeduplication(u.Scopes, scopes)
+}
+
+func (u *User) RevokeAuthorization(scope string) {
+	if scope == "" {
+		return
+	}
+
+	for i, s := range u.Scopes {
+		if s == scope {
+			copy(u.Scopes[i:], u.Scopes[i+1:])
+			u.Scopes = u.Scopes[:len(u.Scopes)-1]
+			return
+		}
+	}
+}
+
+func (u *User) HasRole(role string) bool {
+	for _, r := range u.Roles {
+		if r == role {
+			return true
+		}
+	}
+	return false
+}
+
+func (u *User) HasScope(scope string) bool {
+	for _, s := range u.Scopes {
+		if s == scope {
+			return true
+		}
+	}
+	return false
 }
