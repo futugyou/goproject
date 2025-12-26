5cbf6bf1a9c751c734d4209e5d799b7d9cacaa8f
futugyou <17871902+futugyou@users.noreply.github.com>
2025-12-26 23:48:04 +0000

    Use the separate console launcher

---
+++ openai-web/cmd/msdoc/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	_ "github.com/joho/godotenv/autoload"
+
+	"google.golang.org/adk/agent"
+	"google.golang.org/adk/cmd/launcher"
+	"google.golang.org/adk/cmd/launcher/console"
+
+	"github.com/futugyousuzu/go-openai-web/agentic/agents"
+)
+
+// go run ./cmd/msdoc/main.go
+func main() {
+	ctx := context.Background()
+
+	a, err := agents.MsDocAgent(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create agent: %v", err)
+	}
+
+	config := &launcher.Config{
+		AgentLoader: agent.NewSingleLoader(a),
+	}
+
+	l := console.NewLauncher()
+	if err = l.Run(ctx, config); err != nil {
+		log.Fatalf("Run failed: %v\n\n%s", err, l.CommandLineSyntax())
+	}
+}
