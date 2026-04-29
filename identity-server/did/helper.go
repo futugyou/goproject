309b912d6ad8381a9aaf9ad062825826bde8db45
futugyou <17871902+futugyou@users.noreply.github.com>
2026-04-29 19:56:17 +0800

    Added JWK to []byte

--- identity-server/did/helper.go
+++ identity-server/did/helper.go
@@ -1,8 +1,14 @@
 package did
 
 import (
+	"crypto"
+	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
 	"strings"
+
+	"github.com/lestrrat-go/jwx/v4/jwk"
 )
 
 func ToHex(value []byte, prefix bool) string {
@@ -21,4 +27,43 @@ func HexToByteArray(value string) ([]byte, error) {
 	}
 
 	return hex.DecodeString(value)
+}
+
+func ComputeJWKThumbprint(key jwk.Key) ([]byte, error) {
+	return key.Thumbprint(crypto.SHA256)
+}
+
+type OKPThumbprint struct {
+	Crv string `json:"crv"`
+	Kty string `json:"kty"`
+	X   string `json:"x"`
+}
+
+func ComputeOKPThumbprint(key jwk.Key) ([]byte, error) {
+	var crv, kty, x any
+	var ok bool
+
+	if crv, ok = key.Field("crv"); !ok {
+		return nil, fmt.Errorf("crv can not be null")
+	}
+	if kty, ok = key.Field("kty"); !ok {
+		return nil, fmt.Errorf("kty can not be null")
+	}
+	if x, ok = key.Field("x"); !ok {
+		return nil, fmt.Errorf("x can not be null")
+	}
+
+	thumb := OKPThumbprint{
+		Crv: crv.(string),
+		Kty: kty.(string),
+		X:   x.(string),
+	}
+
+	canonicalJwk, err := json.Marshal(thumb)
+	if err != nil {
+		return nil, err
+	}
+
+	h := sha256.Sum256(canonicalJwk)
+	return h[:], nil
 }
